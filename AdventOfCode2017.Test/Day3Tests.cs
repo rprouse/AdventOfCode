fdb0f5df1a75638eb9960d60fd5701292b0e2b22
Rob Prouse <rob.prouse@bentley.com>
2017-12-07 16:46:33 -0500

    Rather ugly solution to day 3 part 1

---
+++ AdventOfCode2017.Test/Day3Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2017.Test
+{
+    public class Day3Tests
+    {
+        [TestCase(1, 0)]
+        [TestCase(12, 3)]
+        [TestCase(23, 2)]
+        [TestCase(1024, 31)]
+        [TestCase(265149, 438)]
+        public void TestCalculateDistance(int square, int expected)
+        {
+            Assert.That(Day3.CalculateDistance(square), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ AdventOfCode2017/Day3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public static class Day3
+    {
+        public static int CalculateDistance(int location)
+        {
+            var square = location.CalculateSquare();
+            return Math.Abs(square.x) + Math.Abs(square.y) - 1;
+        }
+
+        static (int x, int y) CalculateSquare(this int location)
+        {
+            int x = 0;
+            int y = 0;
+            int maxX = 0;
+            int maxY = 0;
+            int minX = 0;
+            int minY = 0;
+            int xDir = 1;
+            int yDir = 0;
+
+            for(int i = 0; i < location; i++)
+            {
+                x += xDir;
+                y += yDir;
+                if(x > maxX)
+                {
+                    maxX = x;
+                    xDir = 0;
+                    yDir = -1;
+                }
+                else if(y > maxY)
+                {
+                    maxY = y;
+                    xDir = 1;
+                    yDir = 0;
+                }
+                else if(x < minX)
+                {
+                    minX = x;
+                    xDir = 0;
+                    yDir = 1;
+                }
+                else if(y < minY)
+                {
+                    minY = y;
+                    xDir = -1;
+                    yDir = 0;
+                }
+            }
+            return (x, y);
+        }
+    }
+}
