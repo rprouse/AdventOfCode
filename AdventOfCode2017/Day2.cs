050c8ac0649577e43b20657571693f05cd417e36
Rob Prouse <rob.prouse@bentley.com>
2017-12-07 15:44:36 -0500

    Refactored and solved day2 part 1

--- AdventOfCode2017/Day2.cs
+++ AdventOfCode2017/Day2.cs
@@ -4,14 +4,7 @@ namespace AdventOfCode2017
 {
     public static class Day2
     {
-        public static int Checksum(int[][] spreadsheet)
-        {
-            int sum = 0;
-            foreach(var row in spreadsheet)
-            {
-                sum += row.Max() - row.Min();
-            }
-            return sum;
-        }
+        public static int Checksum(int[][] spreadsheet) =>
+            spreadsheet.Sum(row => row.Max() - row.Min());
     }
 }
