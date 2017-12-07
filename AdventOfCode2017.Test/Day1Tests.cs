60ab3f01189b984d7632b09c636c50099ec38484
Rob Prouse <rob.prouse@bentley.com>
2017-12-07 15:00:02 -0500

    Created a C# solution for solving the Advent of Code

---
+++ AdventOfCode2017.Test/Day1Tests.cs
@@ -0,0 +1,19 @@
+using AdventOfCode2017;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Day1Tests
+    {
+        [TestCase("1112", 3)]
+        [TestCase("1111", 4)]
+        [TestCase("1234", 0)]
+        [TestCase("91212129", 9)]
+        public void TestCaptcha(string captcha, int expected)
+        {
+            var sum = Day1.CalculateCaptcha(captcha);
+            Assert.That(sum, Is.EqualTo(expected));
+            TestContext.WriteLine(sum);
+        }
+    }
+}
---
+++ AdventOfCode2017/Day1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2017
+{
+    public static class Day1
+    {
+        public static int CalculateCaptcha(string captcha)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
