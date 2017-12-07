f7369dc356a95244af7e3dae6dca0eaf3a03451a
Rob Prouse <rob.prouse@bentley.com>
2017-12-07 17:37:53 -0500

    Extremely unproud of the day 3 solutions. Hangs head in shame

--- AdventOfCode2017.Test/Day3Tests.cs
+++ AdventOfCode2017.Test/Day3Tests.cs
@@ -13,5 +13,40 @@ namespace AdventOfCode2017.Test
         {
             Assert.That(Day3.CalculateDistance(square), Is.EqualTo(expected));
         }
+
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 4)]
+        [TestCase(5, 5)]
+        [TestCase(6, 10)]
+        [TestCase(7, 11)]
+        [TestCase(8, 23)]
+        [TestCase(9, 25)]
+        [TestCase(10, 26)]
+        [TestCase(11, 54)]
+        [TestCase(12, 57)]
+        [TestCase(13, 59)]
+        [TestCase(14, 122)]
+        [TestCase(15, 133)]
+        [TestCase(16, 142)]
+        [TestCase(17, 147)]
+        [TestCase(18, 304)]
+        [TestCase(19, 330)]
+        [TestCase(20, 351)]
+        [TestCase(21, 362)]
+        [TestCase(22, 747)]
+        [TestCase(23, 806)]
+        [TestCase(265149, 4671229017281475194)]
+        public void TestCalculateSums(int square, long expected)
+        {
+            Assert.That(Day3.CalculateSums(square), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestNextLarger()
+        {
+            Assert.Pass(Day3.NextSum(265149).ToString());
+        }
     }
 }
--- AdventOfCode2017/Day3.cs
+++ AdventOfCode2017/Day3.cs
@@ -8,12 +8,6 @@ namespace AdventOfCode2017
     {
         public static int CalculateDistance(int location)
         {
-            var square = location.CalculateSquare();
-            return Math.Abs(square.x) + Math.Abs(square.y) - 1;
-        }
-
-        static (int x, int y) CalculateSquare(this int location)
-        {
             int x = 0;
             int y = 0;
             int maxX = 0;
@@ -52,7 +46,123 @@ namespace AdventOfCode2017
                     yDir = 0;
                 }
             }
-            return (x, y);
+            return Math.Abs(x) + Math.Abs(y) - 1;
         }
+
+        public static long CalculateSums(int location)
+        {
+            var dict = new Dictionary<string, long>();
+            int x = 0;
+            int y = 0;
+            int maxX = 0;
+            int maxY = 0;
+            int minX = 0;
+            int minY = 0;
+            int xDir = 1;
+            int yDir = 0;
+            long lastValue = 1;
+            dict.Add("0,0", lastValue);
+
+            for (int i = 1; i < location; i++)
+            {
+                x += xDir;
+                y += yDir;
+                lastValue = NewValue(x, y, dict);
+                dict.Add(Key(x, y), lastValue);
+                if (x > maxX)
+                {
+                    maxX = x;
+                    xDir = 0;
+                    yDir = -1;
+                }
+                else if (y > maxY)
+                {
+                    maxY = y;
+                    xDir = 1;
+                    yDir = 0;
+                }
+                else if (x < minX)
+                {
+                    minX = x;
+                    xDir = 0;
+                    yDir = 1;
+                }
+                else if (y < minY)
+                {
+                    minY = y;
+                    xDir = -1;
+                    yDir = 0;
+                }
+            }
+            return lastValue;
+        }
+
+        public static long NextSum(int location)
+        {
+            var dict = new Dictionary<string, long>();
+            int x = 0;
+            int y = 0;
+            int maxX = 0;
+            int maxY = 0;
+            int minX = 0;
+            int minY = 0;
+            int xDir = 1;
+            int yDir = 0;
+            long lastValue = 1;
+            dict.Add("0,0", lastValue);
+
+            for (int i = 1; i < location; i++)
+            {
+                x += xDir;
+                y += yDir;
+                lastValue = NewValue(x, y, dict);
+                dict.Add(Key(x, y), lastValue);
+                if (lastValue > location) return lastValue;
+                if (x > maxX)
+                {
+                    maxX = x;
+                    xDir = 0;
+                    yDir = -1;
+                }
+                else if (y > maxY)
+                {
+                    maxY = y;
+                    xDir = 1;
+                    yDir = 0;
+                }
+                else if (x < minX)
+                {
+                    minX = x;
+                    xDir = 0;
+                    yDir = 1;
+                }
+                else if (y < minY)
+                {
+                    minY = y;
+                    xDir = -1;
+                    yDir = 0;
+                }
+            }
+            return lastValue;
+        }
+
+        static long NewValue(int x, int y, Dictionary<string, long> dict)
+        {
+            long sum = 0;
+            for (int x1 = x - 1; x1 <= x + 1; x1++)
+            {
+                for (int y1 = y - 1; y1 <= y + 1; y1++)
+                {
+                    string key = Key(x1, y1);
+                    if ((x1 != x || y1 != y) && dict.ContainsKey(key))
+                    {
+                        sum += dict[key];
+                    }
+                }
+            }
+            return sum;
+        }
+
+        static string Key(int x, int y) => $"{x},{y}";
     }
 }
