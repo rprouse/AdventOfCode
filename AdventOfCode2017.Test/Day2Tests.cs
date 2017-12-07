9e3e40030ab2097504947bc54d0b7720c1f039fa
Rob Prouse <rob.prouse@bentley.com>
2017-12-07 15:32:28 -0500

    Initial solution for Day 2 part 1

---
+++ AdventOfCode2017.Test/Day2Tests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace AdventOfCode2017.Test
+{
+    public class Day2Tests
+    {
+        [TestCaseSource(nameof(Day2Data))]
+        public void TestChecksum(int[][] spreadsheet, int expected)
+        {
+            Assert.That(Day2.Checksum(spreadsheet), Is.EqualTo(expected));
+        }
+
+        public static IEnumerable Day2Data()
+        {
+            yield return new TestCaseData(
+                new int[][]
+                {
+                    new int[] { 5,1,9,5 },
+                    new int[] { 7,5,3 },
+                    new int[] { 2,4,6,8 }
+                }
+                , 18
+                );
+        }
+    }
+}
---
+++ AdventOfCode2017/Day2.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public static class Day2
+    {
+        public static int Checksum(int[][] spreadsheet)
+        {
+            int sum = 0;
+            foreach(var row in spreadsheet)
+            {
+                sum += row.Max() - row.Min();
+            }
+            return sum;
+        }
+    }
+}
