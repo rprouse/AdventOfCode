d7f018e935f5098b308272f296a9ec8649988811
Rob Prouse <rob@prouse.org>
2017-12-07 21:34:35 -0500

    Solution for day 5 part 1

---
+++ AdventOfCode2017.Test/Day5Tests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AdventOfCode2017.Test
+{
+    public class Day5Tests
+    {
+        [Test]
+        public void TestCountJumps()
+        {
+            var instructions = new int[] { 0, 3, 0, 1, -3 };
+            Assert.That(instructions.CountJumps(), Is.EqualTo(5));
+        }
+
+        [Test]
+        public async Task TestCountJumpsSolution()
+        {
+            var instructions = await ReadInstructions();
+            Assert.That(instructions.CountJumps(), Is.EqualTo(358131));
+        }
+
+
+        async Task<int[]> ReadInstructions()
+        {
+            var instructions = new List<int>();
+            using (var reader = new StreamReader("Day5-1.txt"))
+            {
+                string instruction = await reader.ReadLineAsync();
+                while (!string.IsNullOrWhiteSpace(instruction))
+                {
+                    if (int.TryParse(instruction, out int i))
+                        instructions.Add(i);
+                    instruction = await reader.ReadLineAsync();
+                }
+            }
+            return instructions.ToArray();
+        }
+    }
+}
---
+++ AdventOfCode2017/Day5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public static class Day5
+    {
+        public static int CountJumps(this int[] instuctions)
+        {
+            int jumps = 0;
+            int pointer = 0;
+            while(pointer >= 0 && pointer < instuctions.Length)
+            {
+                int jump = instuctions[pointer];
+                instuctions[pointer]++;
+                pointer += jump;
+                jumps++;
+            }
+            return jumps;
+        }
+    }
+}
