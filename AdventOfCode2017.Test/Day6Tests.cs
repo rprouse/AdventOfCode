0c5b16c28167c312190d55e40af8a225e00940ff
Rob Prouse <rob@prouse.org>
2017-12-07 22:31:34 -0500

    Solved Day 6 part 1

---
+++ AdventOfCode2017.Test/Day6Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace AdventOfCode2017.Test
+{
+    public class Day6Tests
+    {
+        [TestCaseSource(nameof(TestData))]
+        public void TestReallocation(int[] blocks, int expected)
+        {
+            Assert.That(blocks.CountReallocations(), Is.EqualTo(expected));
+        }
+
+        public static IEnumerable TestData()
+        {
+            yield return new TestCaseData(
+                new int[] { 0, 2, 7, 0 },
+                5);
+
+            yield return new TestCaseData(
+                new int[] { 10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 3, 6 },
+                14029);
+
+        }
+    }
+}
---
+++ AdventOfCode2017/Day6.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public static class Day6
+    {
+        public static int CountReallocations(this int[] blocks)
+        {
+            var comparer = new IntArrayComparer();
+            int count = 0;
+            var previous = new List<int[]>();
+            previous.Add(blocks);
+            do
+            {
+                count++;
+                blocks = blocks.Reallocate();
+                if (previous.Contains(blocks, comparer)) break;
+                previous.Add(blocks);
+
+            } while (true);
+
+            return count;
+        }
+
+        class IntArrayComparer : IEqualityComparer<int[]>
+        {
+            public bool Equals(int[] x, int[] y)
+            {
+                if (x == null && y == null) return true;
+                if (x == null || y == null) return false;
+                if (x.Length != y.Length) return false;
+                for (int i = 0; i < x.Length; i++)
+                    if (x[i] != y[i]) return false;
+
+                return true;
+            }
+
+            public int GetHashCode(int[] obj) =>
+                obj.Aggregate((working, next) => working ^ next.GetHashCode());
+        }
+
+        static int[] Reallocate(this int[] blocks)
+        {
+            int[] copy = new int[blocks.Length];
+            blocks.CopyTo(copy, 0);
+
+            int pos = copy.IndexOfMax();
+            int block = copy[pos];
+            copy[pos++] = 0;
+            while(block > 0)
+            {
+                copy[pos++ % blocks.Length]++;
+                block--;
+            }
+            return copy;
+        }
+
+        static int IndexOfMax(this int[] blocks)
+        {
+            int max = blocks.Max();
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                if (blocks[i] == max) return i;
+            }
+            return -1;
+        }
+    }
+}
