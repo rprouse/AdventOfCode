f40315252b5ad270ec17c5b60cd7b396da767901
Rob Prouse <rob@prouse.org>
2017-12-07 21:11:44 -0500

    Day 4 solution

---
+++ AdventOfCode2017.Test/Day4Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AdventOfCode2017.Test
+{
+    public class Day4Tests
+    {
+        [TestCase("aa bb cc dd ee", true)]
+        [TestCase("aa bb cc dd aa", false)]
+        [TestCase("aa bb cc dd aaa", true)]
+        public void TestIsValid(string passphrase, bool expected)
+        {
+            Assert.That(passphrase.IsValid(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public async Task TestNumberOfValidPassphrases()
+        {
+            int valid = await NumberOfValidPasswords(s => s.IsValid());
+            Assert.That(valid, Is.EqualTo(466));
+        }
+
+        [TestCase("abcde fghij", true)]
+        [TestCase("abcde xyz ecdab", false)]
+        [TestCase("a ab abc abd abf abj", true)]
+        [TestCase("iiii oiii ooii oooi oooo", true)]
+        [TestCase("oiii ioii iioi iiio", false)]
+        public void TestIsAnagramValid(string passphrase, bool expected)
+        {
+            Assert.That(passphrase.IsAnagramValid(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public async Task TestNumberOfAnagramValidPassphrases()
+        {
+            int valid = await NumberOfValidPasswords(s => s.IsAnagramValid());
+            Assert.That(valid, Is.EqualTo(251));
+        }
+
+        async Task<int> NumberOfValidPasswords(Func<string, bool> test)
+        {
+            int valid = 0;
+            using (var reader = new StreamReader("Day4-1.txt"))
+            {
+                string passphrase = await reader.ReadLineAsync();
+                while (!string.IsNullOrWhiteSpace(passphrase))
+                {
+                    if (test.Invoke(passphrase)) valid++;
+                    passphrase = await reader.ReadLineAsync();
+                }
+            }
+            return valid;
+        }
+    }
+}
---
+++ AdventOfCode2017/Day4.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public static class Day4
+    {
+        public static bool IsValid(this string passphrase)
+        {
+            var split = passphrase.Split(' ');
+            return split.Distinct().Count() == split.Length;
+        }
+        public static bool IsAnagramValid(this string passphrase)
+        {
+            var split = passphrase
+                          .Split(' ')
+                          .Select(w => new string(w.ToCharArray().OrderBy(c => c).ToArray()));
+            return split.Distinct().Count() == split.Count();
+        }
+    }
+}
