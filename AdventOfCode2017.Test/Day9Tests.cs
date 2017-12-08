875f30f1ab5c7ec2482b0381d84c087fad7418b8
Rob Prouse <rob.prouse@bentley.com>
2017-12-08 16:39:01 -0500

    Getting code ready for Day 9

---
+++ AdventOfCode2017.Test/Day9Tests.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace AdventOfCode2017.Test
+{
+    public class Day9Tests
+    {
+        [TestCase("Day9.txt", 0)]
+        [TestCase("Day9Test.txt", 0)]
+        public void TestSomething(string filename, int expected)
+        {
+            var lines = Day9.ReadSomething(filename);
+            Assert.That(lines.Count(), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ AdventOfCode2017/Day9.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public static class Day9
+    {
+        public static IEnumerable<string> ReadSomething(string filename) =>
+            LineReader.ReadLines(filename);
+    }
+}
