026386150ca20462c2e7fea165a4c556badf6f33
Rob Prouse <rob.prouse@bentley.com>
2017-12-08 13:13:38 -0500

    Solved day 7 part 1

---
+++ AdventOfCode2017.Test/Day7Tests.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace AdventOfCode2017.Test
+{
+    public class Day7Tests
+    {
+        [Test]
+        public void CanParseWithChildren()
+        {
+            var program = new Program("fwft (72) -> ktlj, cntj, xhth");
+            Assert.That(program.Code, Is.EqualTo("fwft"));
+            Assert.That(program.Weight, Is.EqualTo("72"));
+            Assert.That(program.ChildCodes, Has.Count.EqualTo(3));
+        }
+
+        [Test]
+        public void CanParseWithoutChildren()
+        {
+            var program = new Program("ktlj (57)");
+            Assert.That(program.Code, Is.EqualTo("ktlj"));
+            Assert.That(program.Weight, Is.EqualTo("57"));
+            Assert.That(program.ChildCodes, Has.Count.EqualTo(0));
+        }
+
+        [TestCase("ktlj", true)]
+        [TestCase("vvvv", false)]
+        public void CanFindChild(string child, bool expected)
+        {
+            var program = new Program("fwft (72) -> ktlj, cntj, xhth");
+            Assert.That(program.ContainsChild(child), Is.EqualTo(expected));
+        }
+
+        [TestCase("Day7Test.txt", 13)]
+        [TestCase("Day7.txt", 1083)]
+        public void CanReadPrograms(string filename, int expected)
+        {
+            var count = Day7.ReadPrograms(filename).Count();
+            Assert.That(count, Is.EqualTo(expected));
+        }
+
+        [TestCase("Day7Test.txt", "tknk")]
+        [TestCase("Day7.txt", "azqje")]
+        public void CanFindRootProgram(string filename, string expected)
+        {
+            var programs = Day7.ReadPrograms(filename);
+            var root = programs.FindRootProgram();
+            Assert.That(root, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ AdventOfCode2017/Day7.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2017
+{
+    public static class Day7
+    {
+        public static IEnumerable<Program> ReadPrograms(string filename)
+        {
+            using (var reader = new StreamReader(filename))
+            {
+                string program = reader.ReadLine();
+                while (!string.IsNullOrWhiteSpace(program))
+                {
+                    yield return new Program(program);
+                    program = reader.ReadLine();
+                }
+            }
+        }
+
+        public static string FindRootProgram(this IEnumerable<Program> programs)
+        {
+            var candidates = programs.Where(p => p.HasChildren).ToList();
+            return candidates.Single(p1 => !candidates.Any(p2 => p2.ContainsChild(p1.Code))).Code;
+        }
+    }
+
+    public class Program
+    {
+        static Regex regex = new Regex(
+            "^(?<Code>[a-zA-Z]+)\\s\\((?<Weight>\\d+)\\)(?<Children>.*)$",
+            RegexOptions.CultureInvariant
+            | RegexOptions.Compiled );
+
+        public string Code { get; }
+        public string Weight { get; }
+        public IList<string> ChildCodes { get; } = new List<string>();
+        public IList<Program> Children { get; } = new List<Program>();
+
+        public Program(string info)
+        {
+            Match m = regex.Match(info);
+            if(m.Success)
+            {
+                Code = m.Groups["Code"].Value;
+                Weight = m.Groups["Weight"].Value;
+                string children = m.Groups["Children"].Value;
+                if(children.Length > 3)
+                {
+                    foreach (var code in children.Substring(4).Split(", "))
+                    {
+                        ChildCodes.Add(code);
+                    }
+                }
+            }
+        }
+
+        public bool HasChildren => ChildCodes.Count > 0;
+
+        public bool ContainsChild(string code) => ChildCodes.Contains(code);
+    }
+}
