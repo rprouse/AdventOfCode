d5a693be7a169ac2618213b457a937017f880f53
Rob Prouse <rob@prouse.org>
2017-12-09 10:43:13 -0500

    Solution to day 9 part 1

--- AdventOfCode2017.Test/Day9Tests.cs
+++ AdventOfCode2017.Test/Day9Tests.cs
@@ -5,12 +5,36 @@ namespace AdventOfCode2017.Test
 {
     public class Day9Tests
     {
-        [TestCase("Day9.txt", 0)]
-        [TestCase("Day9Test.txt", 0)]
-        public void TestSomething(string filename, int expected)
+        [TestCase("Day9.txt", 12803)]
+        public void FindSolution(string filename, int expected)
         {
-            var lines = Day9.ReadSomething(filename);
-            Assert.That(lines.Count(), Is.EqualTo(expected));
+            var str = Day9.ReadSomething(filename).First();
+            Assert.That(str.ScoreGroups(), Is.EqualTo(expected));
+        }
+
+        [TestCase("<>")]
+        [TestCase("<random characters>")]
+        [TestCase("<<<<>")]
+        [TestCase("<{!>}>")]
+        [TestCase("<!!>")]
+        [TestCase("<!!!>>")]
+        [TestCase("<{o\"i!a,<{i<a>")]
+        public void CanStripGarbage(string line)
+        {
+            Assert.That(line.StripGarbage(), Is.EqualTo(""));
+        }
+
+        [TestCase("{}", 1)]
+        [TestCase("{{{}}}", 6)]
+        [TestCase("{{},{}}", 5)]
+        [TestCase("{{{},{},{{}}}}", 16)]
+        [TestCase("{<a>,<a>,<a>,<a>}", 1)]
+        [TestCase("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9)]
+        [TestCase("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9)]
+        [TestCase("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3)]
+        public void CanScoreGroups(string line, int expected)
+        {
+            Assert.That(line.ScoreGroups(), Is.EqualTo(expected));
         }
     }
 }
--- AdventOfCode2017/Day9.cs
+++ AdventOfCode2017/Day9.cs
@@ -8,5 +8,56 @@ namespace AdventOfCode2017
     {
         public static IEnumerable<string> ReadSomething(string filename) =>
             LineReader.ReadLines(filename);
+
+        public static int ScoreGroups(this string str)
+        {
+            var clean = str.StripGarbage();
+            int sum = 0;
+            int depth = 0;
+            foreach(char c in clean)
+            {
+                switch(c)
+                {
+                    case '{':
+                        depth++;
+                        sum += depth;
+                        break;
+                    case '}':
+                        depth--;
+                        break;
+                }
+            }
+            return sum;
+        }
+
+        public static string StripGarbage(this string str)
+        {
+            var sb = new StringBuilder();
+            bool garbage = false;
+            bool skip = false;
+            foreach(char c in str)
+            {
+                if(skip)
+                {
+                    skip = false;
+                    continue;
+                }
+                switch (c)
+                {
+                    case '<':
+                        garbage = true;
+                        continue;
+                    case '!':
+                        skip = true;
+                        continue;
+                    case '>':
+                        garbage = false;
+                        continue;
+                }
+                if(!garbage)
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
     }
 }
