3f0fe91aeeb13d60e7dee31ae8f317e4bc8205f5
Rob Prouse <rob@prouse.org>
2017-12-11 00:25:13 -0500

    Solved day 11 part 1

--- AdventOfCode2017/Day11/Day11.cs
+++ AdventOfCode2017/Day11/Day11.cs
@@ -6,12 +6,40 @@ namespace AdventOfCode2017.Day11
 {
     public static class Day11
     {
-        public static int PartOne(string line)
+        public static int PartOne(string[] steps)
         {
-            return 0;
+            int x = 0;
+            int y = 0;
+            foreach(var step in steps)
+            {
+                switch(step)
+                {
+                    case "n":
+                        y++;
+                        break;
+                    case "ne":
+                        x++;
+                        y++;
+                        break;
+                    case "nw":
+                        x--;
+                        break;
+                    case "s":
+                        y--;
+                        break;
+                    case "se":
+                        x++;
+                        break;
+                    case "sw":
+                        x--;
+                        y--;
+                        break;
+                }
+            }
+            return Math.Max(Math.Max(Math.Abs(x), Math.Abs(y)), Math.Abs((x - y) * -1));
         }
 
-        public static int PartTwo(string line)
+        public static int PartTwo(string[] steps)
         {
             return 0;
         }
--- AdventOfCode2017/Day11/Day11Tests.cs
+++ AdventOfCode2017/Day11/Day11Tests.cs
@@ -4,18 +4,30 @@ namespace AdventOfCode2017.Day11
 {
     public class Day11Tests
     {
-        [Test]
+        [Test] // 794
         public void TestPartOne()
         {
             string line = LineReader.ReadLine("Day11\\Data.txt");
-            Assert.Pass(Day11.PartOne(line).ToString());
+            string[] steps = line.Split(',');
+            Assert.Pass(Day11.PartOne(steps).ToString());
         }
 
         [Test]
         public void TestPartTwo()
         {
             string line = LineReader.ReadLine("Day11\\Data.txt");
-            Assert.Pass(Day11.PartTwo(line).ToString());
+            string[] steps = line.Split(',');
+            Assert.Pass(Day11.PartTwo(steps).ToString());
+        }
+
+        [TestCase("ne,ne,ne", 3)]
+        [TestCase("ne,ne,sw,sw", 0)]
+        [TestCase("ne,ne,s,s", 2)]
+        [TestCase("se,sw,se,sw,sw", 3)]
+        public void CanCountSteps(string line, int expected)
+        {
+            string[] steps = line.Split(',');
+            Assert.That(Day11.PartOne(steps), Is.EqualTo(expected));
         }
     }
 }
