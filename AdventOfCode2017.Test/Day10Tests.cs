dfce421dc501aef41ec5fe726fc7d261256d0de2
Rob Prouse <rob@prouse.org>
2017-12-10 22:57:28 -0500

    Day 10 part 1

---
+++ AdventOfCode2017.Test/Day10Tests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace AdventOfCode2017.Test
+{
+    public class Day10Tests
+    {
+        [TestCaseSource(nameof(SolutionData))]
+        public void CanMultiplyFirstTwo(IEnumerable<int> twists, int length, int expected)
+        {
+            int result = Day10.TwistMultiplier(length, twists);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CanDoTheTwist()
+        {
+            int[] array = Day10.Twist(5, new [] { 3, 4, 1, 5 });
+            Assert.That(array, Is.EqualTo(new[] { 3, 4, 2, 1, 0 }));
+        }
+
+        [TestCaseSource(nameof(ReverseSectionData))]
+        public void CanReverseSection(int[] array, int start, int length, int[] expected)
+        {
+            array.ReverseSection(start, length);
+            Assert.That(array, Is.EqualTo(expected));
+        }
+
+        public static IEnumerable ReverseSectionData()
+        {
+            yield return new TestCaseData(new[] { 0, 1, 2, 3, 4 }, 0, 3, new[] { 2, 1, 0, 3, 4 });
+            yield return new TestCaseData(new[] { 2, 1, 0, 3, 4 }, 3, 4, new[] { 4, 3, 0, 1, 2 });
+            yield return new TestCaseData(new[] { 4, 3, 0, 1, 2 }, 3, 1, new[] { 4, 3, 0, 1, 2 });
+            yield return new TestCaseData(new[] { 4, 3, 0, 1, 2 }, 1, 5, new[] { 3, 4, 2, 1, 0 });
+        }
+
+        public static IEnumerable SolutionData()
+        {
+            yield return new TestCaseData(new[] { 3, 4, 1, 5 }, 5, 12);
+            yield return new TestCaseData(new[] { 76, 1, 88, 148, 166, 217, 130, 0, 128, 254, 16, 2, 130, 71, 255, 229 }, 256, 29240);
+        }
+    }
+}
---
+++ AdventOfCode2017/Day10.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public static class Day10
+    {
+        public static int TwistMultiplier(int length, IEnumerable<int> twists)
+        {
+            int[] array = Twist(length, twists);
+            return array[0] * array[1];
+        }
+
+        public static int[] Twist(int length, IEnumerable<int> twists)
+        {
+            int[] array = Enumerable.Range(0, length).ToArray();
+            int pos = 0;
+            int skip = 0;
+            foreach(int twist in twists)
+            {
+                array.ReverseSection(pos, twist);
+                pos += twist + skip++;
+                pos %= length;
+            }
+            return array;
+        }
+
+        public static int[] ReverseSection(this int[] array, int start, int length)
+        {
+            int[] copy = new int[length];
+            for(int i = 0; i < length; i++)
+            {
+                copy[i] = array[(start + i) % array.Length];
+            }
+            copy = copy.Reverse().ToArray();
+            for (int i = 0; i < length; i++)
+            {
+                array[(start + i) % array.Length] = copy[i];
+            }
+            return array;
+        }
+    }
+}
