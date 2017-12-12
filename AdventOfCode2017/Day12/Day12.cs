0057eda2828fc59e4f483b3cac69ce4793d01c60
Rob Prouse <rob.prouse@bentley.com>
2017-12-12 00:27:15 -0500

    Day 12 part 1

--- AdventOfCode2017/Day12/Day12.cs
+++ AdventOfCode2017/Day12/Day12.cs
@@ -1,17 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2017
 {
     public static class Day12
     {
-        public static int PartOne(string str)
+        public static int PartOne(IEnumerable<string> lines)
+        {
+            var programs = lines.Where(s => !string.IsNullOrWhiteSpace(s))
+                            .Select(s => new Day12Program(s));
+
+            var dict = new Dictionary<int, Day12Program>();
+            foreach (var program in programs)
+                dict.Add(program.Id, program);
+
+            bool[] connected = new bool[dict.Count];
+            for (int i = 0; i < dict.Count; i++)
+            {
+                connected[i] = false;
+            }
+            var root = dict[0];
+            FindConnected(dict, connected, root);
+
+            return connected.Count(c => c);
+        }
+
+        private static void FindConnected(Dictionary<int, Day12Program> dict, bool[] connected, Day12Program root)
+        {
+            var children = root.Connected.ToArray();
+            root.Connected.Clear();
+            foreach (var id in children)
+            {
+                connected[id] = true;
+                var child = dict[id];
+                FindConnected(dict, connected, child);
+            }
+        }
+
+
+        public static int PartTwo(IEnumerable<string> lines)
         {
             return 0;
         }
+    }
 
-        public static int PartTwo(string str)
+    public class Day12Program
+    {
+        public int Id { get; }
+        public IList<int> Connected { get; } = new List<int>();
+
+        public Day12Program(string line)
         {
-            return 0;
+            var split = line.Split(" <-> ");
+            if (int.TryParse(split[0], out int id))
+                Id = id;
+
+            var split2 = split[1].Split(", ");
+            foreach(string con in split2)
+            {
+                if (int.TryParse(con, out int i))
+                    Connected.Add(i);
+            }
         }
     }
 }
--- AdventOfCode2017/Day12/Day12Tests.cs
+++ AdventOfCode2017/Day12/Day12Tests.cs
@@ -8,38 +8,38 @@ namespace AdventOfCode2017
     {
         const int DAY = 12;
 
-        [Test]
-        public void TestPartOne()
-        {
-            string str = File.ReadAllText(PuzzleFile(DAY));
-            Assert.That(Day12.PartOne(str), Is.EqualTo(0));
-        }
+        //[Test]
+        //public void TestPartOne()
+        //{
+        //    string str = File.ReadAllText(PuzzleFile(DAY));
+        //    Assert.That(Day12.PartOne(str), Is.EqualTo(0));
+        //}
 
-        [Test]
-        public void TestPartTwo()
-        {
-            string str = File.ReadAllText(PuzzleFile(DAY));
-            Assert.That(Day12.PartTwo(str), Is.EqualTo(0));
-        }
+        //[Test]
+        //public void TestPartTwo()
+        //{
+        //    string str = File.ReadAllText(PuzzleFile(DAY));
+        //    Assert.That(Day12.PartTwo(str), Is.EqualTo(0));
+        //}
 
         [TestCaseSource(nameof(TestDataOne))]
         public void TestPartOne(string filename, int expected)
         {
-            string str = File.ReadAllText(PuzzleFile(DAY));
-            Assert.That(Day12.PartOne(str), Is.EqualTo(expected));
+            var lines = File.ReadLines(filename);
+            Assert.That(Day12.PartOne(lines), Is.EqualTo(expected));
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
-            string str = File.ReadAllText(PuzzleFile(DAY));
-            Assert.That(Day12.PartTwo(str), Is.EqualTo(expected));
+            var lines = File.ReadLines(filename);
+            Assert.That(Day12.PartTwo(lines), Is.EqualTo(expected));
         }
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), 6);
+            yield return new TestCaseData(PuzzleFile(DAY), 175);
         }
 
         public static IEnumerable TestDataTwo()
