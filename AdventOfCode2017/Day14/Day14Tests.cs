e69c334c20d6f6d44348860a861b011896c2add7
Rob Prouse <rob.prouse@bentley.com>
2017-12-13 15:16:17 -0500

    Prep for Day 14

---
+++ AdventOfCode2017/Day14/Day14Tests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.IO;
+using NUnit.Framework;
+
+namespace AdventOfCode2017
+{
+    public class Day14Tests : TestBase
+    {
+        const int DAY = 14;
+
+        [Test]
+        public void TestPartOne()
+        {
+            string str = File.ReadAllText(PuzzleFile(DAY));
+            Assert.That(Day14.PartOne(str), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            string str = File.ReadAllText(PuzzleFile(DAY));
+            Assert.That(Day14.PartTwo(str), Is.EqualTo(0));
+        }
+
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, int expected)
+        {
+            string str = File.ReadAllText(filename);
+            Assert.That(Day14.PartOne(str), Is.EqualTo(expected));
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            string str = File.ReadAllText(filename);
+            Assert.That(Day14.PartTwo(str), Is.EqualTo(expected));
+        }
+
+        public static IEnumerable TestDataOne()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
