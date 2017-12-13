953cfa58d04654d6d59d678e9f7477d28e5c0cb9
Rob Prouse <rob.prouse@bentley.com>
2017-12-13 12:01:22 -0500

    Solution to day 13 part 1

--- AdventOfCode2017/Day13/Day13.cs
+++ AdventOfCode2017/Day13/Day13.cs
@@ -7,14 +7,82 @@ namespace AdventOfCode2017
 {
     public static class Day13
     {
-        public static int PartOne(string str)
+        public static int PartOne(string[] input)
+        {
+            var layers = ParseInput(input);
+
+            int cost = 0;
+            for(int i = 0; i <= layers.MaxLayer(); i++)
+            {
+                var layer = layers.GetLayer(i);
+                if(layer != null)
+                {
+                    cost += layer.Cost();
+                }
+                layers.Step();
+            }
+            return cost;
+        }
+
+        public static int PartTwo(string[] input)
         {
             return 0;
         }
 
-        public static int PartTwo(string str)
+        static Layer[] ParseInput(IEnumerable<string> input) =>
+            input.Where(s => !string.IsNullOrWhiteSpace(s))
+                 .Select(s => new Layer(s))
+                 .ToArray();
+
+        static int MaxLayer(this IEnumerable<Layer> layers) =>
+            layers.Max(l => l.Depth);
+
+        static void Step(this IEnumerable<Layer> layers)
         {
-            return 0;
+            foreach(var layer in layers)
+            {
+                layer.Step();
+            }
+        }
+
+        static Layer GetLayer(this IEnumerable<Layer> layers, int level) =>
+            layers.FirstOrDefault(l => l.Depth == level);
+
+        class Layer
+        {
+            public int Depth { get; }
+            public int Range { get; }
+            int CurrentRange { get; set; }
+            int Direction { get; set; }
+
+            public Layer(string line)
+            {
+                var split = line.Split(": ");
+                int.TryParse(split[0], out int index);
+                int.TryParse(split[1], out int value);
+                Depth = index;
+                Range = value;
+            }
+
+            public int Cost() =>
+                CurrentRange == 0 ? Depth * Range : 0;
+
+            public void Step()
+            {
+                if(Range == 1)
+                {
+                    Direction = 0;
+                }
+                else if(CurrentRange == 0)
+                {
+                    Direction = 1;
+                }
+                else if(CurrentRange == Range - 1)
+                {
+                    Direction = -1;
+                }
+                CurrentRange += Direction;
+            }
         }
     }
 }
--- AdventOfCode2017/Day13/Day13Tests.cs
+++ AdventOfCode2017/Day13/Day13Tests.cs
@@ -8,43 +8,29 @@ namespace AdventOfCode2017
     {
         const int DAY = 13;
 
-        [Test]
-        public void TestPartOne()
-        {
-            string str = File.ReadAllText(PuzzleFile(DAY));
-            Assert.That(Day00.PartOne(str), Is.EqualTo(0));
-        }
-
-        [Test]
-        public void TestPartTwo()
-        {
-            string str = File.ReadAllText(PuzzleFile(DAY));
-            Assert.That(Day00.PartTwo(str), Is.EqualTo(0));
-        }
-
         [TestCaseSource(nameof(TestDataOne))]
         public void TestPartOne(string filename, int expected)
         {
-            string str = File.ReadAllText(filename);
-            Assert.That(Day00.PartOne(str), Is.EqualTo(expected));
+            string[] lines = File.ReadAllLines(filename);
+            Assert.That(Day13.PartOne(lines), Is.EqualTo(expected));
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
-            string str = File.ReadAllText(filename);
-            Assert.That(Day00.PartTwo(str), Is.EqualTo(expected));
+            string[] lines = File.ReadAllLines(filename);
+            Assert.That(Day13.PartTwo(lines), Is.EqualTo(expected));
         }
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), 24);
+            yield return new TestCaseData(PuzzleFile(DAY), 1844);
         }
 
         public static IEnumerable TestDataTwo()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), 10);
             yield return new TestCaseData(PuzzleFile(DAY), 0);
         }
     }
