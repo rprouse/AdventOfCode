11bddda56c51c47d4c56ce4fd4f1b70cc92beef6
Rob Prouse <rob.prouse@bentley.com>
2017-12-15 10:59:39 -0500

    Solution to day 15 part 1 - Dueling Generators

---
+++ AdventOfCode2017/Day15/Day15.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public static class Day15
+    {
+        const int FACTOR_A = 16807;
+        const int FACTOR_B = 48271;
+        const long DIVISOR = 2147483647;
+        const int ITERATIONS = 40000000;
+
+        public static int PartOne(int seedA, int seedB, int iterations = ITERATIONS)
+        {
+            int count = 0;
+            for(int i = 0; i < iterations; i++)
+            {
+                seedA = Generate(seedA, FACTOR_A);
+                seedB = Generate(seedB, FACTOR_B);
+                if (LowerBitsMatch(seedA, seedB)) count++;
+            }
+            return count;
+        }
+
+        public static int PartTwo(int seedA, int seedB)
+        {
+            return 0;
+        }
+
+        public static int Generate(long seed, long multiplier) =>
+            (int)((seed * multiplier) % DIVISOR);
+
+        public static bool LowerBitsMatch(int a, int b) =>
+            (a & 0xFFFF) == (b & 0xFFFF);
+    }
+}
---
+++ AdventOfCode2017/Day15/Day15Tests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.IO;
+using NUnit.Framework;
+
+namespace AdventOfCode2017
+{
+    public class Day15Tests : TestBase
+    {
+        const int DAY = 15;
+
+        [TestCase(65, 8921, 588)]
+        [TestCase(873, 583, 631)]
+        public void TestPartOne(int seedA, int seedB, int expected)
+        {
+            Assert.That(Day15.PartOne(seedA, seedB), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestPartOneSmallRun()
+        {
+            Assert.That(Day15.PartOne(65, 8921, 5), Is.EqualTo(1));
+        }
+
+        [TestCase(65, 8921, 0)]
+        [TestCase(873, 583, 0)]
+        public void TestPartTwo(int seedA, int seedB, int expected)
+        {
+            Assert.That(Day15.PartTwo(seedA, seedB), Is.EqualTo(expected));
+        }
+
+        [TestCase(65, 16807, 1092455)]
+        [TestCase(1092455, 16807, 1181022009)]
+        [TestCase(1181022009, 16807, 245556042)]
+        [TestCase(245556042, 16807, 1744312007)]
+        [TestCase(1744312007, 16807, 1352636452)]
+        [TestCase(8921, 48271, 430625591)]
+        [TestCase(430625591, 48271, 1233683848)]
+        [TestCase(1233683848, 48271, 1431495498)]
+        [TestCase(1431495498, 48271, 137874439)]
+        [TestCase(137874439, 48271, 285222916)]
+        public void CanGenerate(int seed, int multiplier, int expected)
+        {
+            Assert.That(Day15.Generate(seed, multiplier), Is.EqualTo(expected));
+        }
+
+        [TestCase(1092455, 430625591, false)]
+        [TestCase(1181022009, 1233683848, false)]
+        [TestCase(245556042, 1431495498, true)]
+        [TestCase(1744312007, 137874439, false)]
+        [TestCase(1352636452, 285222916, false)]
+        public void LowerBitsMatch(int a, int b, bool expected)
+        {
+            Assert.That(Day15.LowerBitsMatch(a, b), Is.EqualTo(expected));
+        }
+    }
+}
