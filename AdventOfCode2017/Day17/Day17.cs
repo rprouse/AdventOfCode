46a8a0dd13861f4301366c45de661e0c5108658a
Rob Prouse <rob@prouse.org>
2017-12-17 07:59:22 -0500

    Solution to day 17 part 1

---
+++ AdventOfCode2017/Day17/Day17.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public static class Day17
+    {
+        public static int PartOne(int skip)
+        {
+            var buffer = new List<int> { 0 };
+            var pos = 0;
+            for(int i = 1; i <= 2017; i++)
+            {
+                pos = (pos + skip) % buffer.Count;
+                buffer.Insert(++pos, i);
+            }
+            return buffer[pos+1];
+        }
+
+        public static int PartTwo(int str)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2017/Day17/Day17Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.IO;
+using NUnit.Framework;
+
+namespace AdventOfCode2017
+{
+    public class Day17Tests : TestBase
+    {
+        const int DAY = 17;
+
+        [Test]
+        public void TestPartOne()
+        {
+            Assert.That(Day17.PartOne(343), Is.EqualTo(1914));
+        }
+
+        [Test]
+        public void TestPartOneExample()
+        {
+            Assert.That(Day17.PartOne(3), Is.EqualTo(638));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.That(Day17.PartTwo(343), Is.EqualTo(0));
+        }
+    }
+}
