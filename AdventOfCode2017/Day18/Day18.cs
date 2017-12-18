d0d89a67cf2c479d50c03306947c96b51462b551
Rob Prouse <rob.prouse@bentley.com>
2017-12-18 10:23:49 -0500

    Solution to day 18 part 1

--- AdventOfCode2017/Day18/Day18.cs
+++ AdventOfCode2017/Day18/Day18.cs
@@ -7,12 +7,68 @@ namespace AdventOfCode2017
 {
     public static class Day18
     {
-        public static int PartOne(string str)
+        public static long PartOne(string[] program)
         {
-            return 0;
+            int ptr = 0;
+            long frq = 0;
+            var registers = new Dictionary<char, long>();
+            while(true)
+            {
+                if (ptr < 0 || ptr >= program.Length || string.IsNullOrEmpty(program[ptr]))
+                    break;
+
+                string instr = program[ptr];
+                char reg = instr[4];
+                long val = 0;
+                if (instr.Length > 6)
+                {
+                    string valStr = instr.Substring(6);
+                    if (!long.TryParse(valStr, out val))
+                    {
+                        if (registers.ContainsKey(valStr[0]))
+                            val = registers[valStr[0]];
+                    }
+                }
+                if (!registers.ContainsKey(reg))
+                    registers.Add(reg, 0);
+
+                switch (instr.Substring(0, 3))
+                {
+                    case "snd":
+                        frq = registers[reg];
+                        ptr++;
+                        break;
+                    case "set":
+                        registers[reg] = val;
+                        ptr++;
+                        break;
+                    case "add":
+                        registers[reg] = registers[reg] + val;
+                        ptr++;
+                        break;
+                    case "mul":
+                        registers[reg] = registers[reg] * val;
+                        ptr++;
+                        break;
+                    case "mod":
+                        registers[reg] = registers[reg] % val;
+                        ptr++;
+                        break;
+                    case "rcv":
+                        ptr = -1;   // Jump out of the program
+                        break;
+                    case "jgz":
+                        if (registers[reg] > 0)
+                            ptr += (int)val;
+                        else
+                            ptr++;
+                        break;
+                }
+            }
+            return frq;
         }
 
-        public static int PartTwo(string str)
+        public static long PartTwo(string[] program)
         {
             return 0;
         }
--- AdventOfCode2017/Day18/Day18Tests.cs
+++ AdventOfCode2017/Day18/Day18Tests.cs
@@ -8,38 +8,24 @@ namespace AdventOfCode2017
     {
         const int DAY = 18;
 
-        [Test]
-        public void TestPartOne()
-        {
-            string str = File.ReadAllText(PuzzleFile(DAY));
-            Assert.That(Day18.PartOne(str), Is.EqualTo(0));
-        }
-
-        [Test]
-        public void TestPartTwo()
-        {
-            string str = File.ReadAllText(PuzzleFile(DAY));
-            Assert.That(Day18.PartTwo(str), Is.EqualTo(0));
-        }
-
         [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
+        public void TestPartOne(string filename, long expected)
         {
-            string str = File.ReadAllText(filename);
+            string[] str = File.ReadAllLines(filename);
             Assert.That(Day18.PartOne(str), Is.EqualTo(expected));
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
-        public void TestPartTwo(string filename, int expected)
+        public void TestPartTwo(string filename, long expected)
         {
-            string str = File.ReadAllText(filename);
+            string[] str = File.ReadAllLines(filename);
             Assert.That(Day18.PartTwo(str), Is.EqualTo(expected));
         }
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), 4);
+            yield return new TestCaseData(PuzzleFile(DAY), 7071);
         }
 
         public static IEnumerable TestDataTwo()
