b8a3c5e71117f8b177e28f477b17e586afc33def
Rob Prouse <rob.prouse@bentley.com>
2017-12-21 19:29:10 -0500

    checkpointing day 21, moving to differnt computer

--- AdventOfCode2017/Day21/Day21.cs
+++ AdventOfCode2017/Day21/Day21.cs
@@ -3,21 +3,121 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace AdventOfCode2017
 {
     public static class Day21
     {
-        public static int PartOne(string filename)
+        public const string START_IMAGE = ".#./..#/###";
+
+        public static int PartOne(string filename, int iterations)
         {
-            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
-            return 0;
+            var rules = GetRules(filename);
+            var matrix = START_IMAGE;
+            for(int i = 0; i < iterations; i++)
+            {
+                matrix = matrix.Transform(rules);
+            }
+            return matrix.Count(c => c == '#');
         }
 
         public static int PartTwo(string filename)
         {
-            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            var rules = GetRules(filename);
             return 0;
+        }
+
+        public static Dictionary<string,string> GetRules(string filename) =>
+            File.ReadAllLines(filename)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Split(" => "))
+                .ToDictionary(s => s[0], s => s[1]);
+
+        public static string Transform(this string matrix, Dictionary<string, string> rules)
+        {
+            return matrix;
+        }
+
+        public static IEnumerable<string> BreakupMatrix(this string matrix)
+        {
+            string[] rows = matrix.Split('/');
+            int divisor = rows.Length % 2 == 0 ? 2 : 3;
+            int numGroups = (int)Math.Pow(rows.Length / divisor, 2);
+            for(int g = 0; g < numGroups; g++ )
+            {
+                var sb = new StringBuilder();
+                for (int y = 0; y < divisor; y++)
+                {
+                    for (int x = 0; x < divisor; x++)
+                    {
+                        sb.Append(rows[(g/divisor)*divisor+y][(g%divisor)*divisor+x]);
+                    }
+                    if (y != divisor - 1) sb.Append('/');
+                }
+                yield return sb.ToString();
+            }
+        }
+
+        public static string JoinMatrixes(this string[] children)
+        {
+            string[][] groups = children.Select(s => s.Split('/')).ToArray();
+            var divisor = groups[0][0].Length;
+            var size = (int)Math.Sqrt(groups.Length * divisor * divisor);
+            var sb = new StringBuilder();
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    sb.Append(groups[(y / divisor) * divisor + x / divisor][y % divisor][x % divisor]);
+                }
+                if (y != size - 1)
+                    sb.Append('/');
+            }
+            return sb.ToString();
+        }
+
+        public static string FlipV(this string matrix)
+        {
+            string[] rows = matrix.Split('/');
+            string[] ret = new string[rows.Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                ret[rows.Length - i - 1] = rows[i];
+            }
+            return string.Join('/', ret);
+        }
+
+        public static string FlipH(this string matrix)
+        {
+            string[] rows = matrix.Split('/');
+            string[] ret = new string[rows.Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                var row = new StringBuilder(rows.Length);
+                for (int j = 0; j < rows.Length; j++)
+                {
+                    row.Append(rows[i][rows.Length - j - 1]);
+                }
+                ret[i] = row.ToString();
+            }
+            return string.Join('/', ret);
+        }
+
+        public static string Rotate(this string matrix)
+        {
+            string[] rows = matrix.Split('/');
+            string[] ret = new string[rows.Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                var row = new StringBuilder(rows.Length);
+                for (int j = 0; j < rows.Length; j++)
+                {
+                    row.Append(rows[rows.Length - j - 1][i]);
+                }
+                ret[i] = row.ToString();
+            }
+            return string.Join('/', ret);
         }
     }
 }
--- AdventOfCode2017/Day21/Day21Tests.cs
+++ AdventOfCode2017/Day21/Day21Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.IO;
+using System.Linq;
 using NUnit.Framework;
 
 namespace AdventOfCode2017
@@ -8,22 +9,10 @@ namespace AdventOfCode2017
     {
         const int DAY = 21;
 
-        [Test]
-        public void TestPartOne()
-        {
-            Assert.That(Day21.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
-        [Test]
-        public void TestPartTwo()
-        {
-            Assert.That(Day21.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
         [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
+        public void TestPartOne(string filename, int iterations, int expected)
         {
-            Assert.That(Day21.PartOne(filename), Is.EqualTo(expected));
+            Assert.That(Day21.PartOne(filename, iterations), Is.EqualTo(expected));
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
@@ -34,14 +23,53 @@ namespace AdventOfCode2017
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), 2, 12);
+            yield return new TestCaseData(PuzzleFile(DAY), 5, 0);
         }
 
         public static IEnumerable TestDataTwo()
         {
             yield return new TestCaseData(TestFile(DAY), 0);
             yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        [Test]
+        public void CanFlipVertically()
+        {
+            var actual = Day21.START_IMAGE.FlipV();
+            Assert.That(actual, Is.EqualTo("###/..#/.#."));
+        }
+
+        [Test]
+        public void CanFlipHorizontally()
+        {
+            var actual = Day21.START_IMAGE.FlipH();
+            Assert.That(actual, Is.EqualTo(".#./#../###"));
+        }
+
+        [Test]
+        public void CanRotate()
+        {
+            var actual = Day21.START_IMAGE.Rotate();
+            Assert.That(actual, Is.EqualTo("#../#.#/##."));
+        }
+
+        [TestCaseSource(nameof(MatrixChildren))]
+        public void CanBreakupMatrix(string matrix, string[] children)
+        {
+            Assert.That(matrix.BreakupMatrix().ToArray(), Is.EqualTo(children));
+        }
+
+        [TestCaseSource(nameof(MatrixChildren))]
+        public void CanJoinMatrixes(string matrix, string[] children)
+        {
+            Assert.That(children.JoinMatrixes(), Is.EqualTo(matrix));
+        }
+
+        public static IEnumerable MatrixChildren()
+        {
+            yield return new TestCaseData("###/..#/.#.", new[] { "###/..#/.#." });
+            yield return new TestCaseData("####/..##/.#.#/..##", new[] { "##/..", "##/##", ".#/..", ".#/##" });
         }
     }
 }
