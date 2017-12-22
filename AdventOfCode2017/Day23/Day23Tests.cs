b45dd73531a84330567f63ff3c21627f093d3eab
Rob Prouse <rob.prouse@bentley.com>
2017-12-22 12:58:11 -0500

    Getting ready for day 23

---
+++ AdventOfCode2017/Day23/Day23Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.IO;
+using NUnit.Framework;
+
+namespace AdventOfCode2017
+{
+    public class Day23Tests : TestBase
+    {
+        const int DAY = 23;
+
+        [Test]
+        public void TestPartOne()
+        {
+            Assert.That(Day23.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.That(Day23.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
+        }
+
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, int expected)
+        {
+            Assert.That(Day23.PartOne(filename), Is.EqualTo(expected));
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            Assert.That(Day23.PartTwo(filename), Is.EqualTo(expected));
+        }
+
+        public static IEnumerable TestDataOne()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
