65b70cbdbd21ccc839917cfdb10846b8770357e4
Rob Prouse <rob@prouse.org>
2017-12-23 11:51:27 -0500

    Getting ready for the last two days

---
+++ AdventOfCode2017/Day24/Day24Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.IO;
+using NUnit.Framework;
+
+namespace AdventOfCode2017
+{
+    public class Day24Tests : TestBase
+    {
+        const int DAY = 24;
+
+        [Test]
+        public void TestPartOne()
+        {
+            Assert.That(Day24.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.That(Day24.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
+        }
+
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, int expected)
+        {
+            Assert.That(Day24.PartOne(filename), Is.EqualTo(expected));
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            Assert.That(Day24.PartTwo(filename), Is.EqualTo(expected));
+        }
+
+        public static IEnumerable TestDataOne()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
---
+++ AdventOfCode2017/Day25/Day25.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public static class Day25
+    {
+        public static int PartOne(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            return 0;
+        }
+
+        public static int PartTwo(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            return 0;
+        }
+    }
+}
