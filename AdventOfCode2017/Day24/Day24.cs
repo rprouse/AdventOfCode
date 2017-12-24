71635f4d04600a645f80a7d9b33cd95519ecc53d
Rob Prouse <rob@prouse.org>
2017-12-24 18:37:15 -0500

    Solution to the first part of day 24

--- AdventOfCode2017/Day24/Day24.cs
+++ AdventOfCode2017/Day24/Day24.cs
@@ -10,14 +10,65 @@ namespace AdventOfCode2017
     {
         public static int PartOne(string filename)
         {
-            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
-            return 0;
+            var parts = GetParts(filename);
+            return BuildBridge(parts, 0, 0);
         }
 
         public static int PartTwo(string filename)
         {
-            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            var parts = GetParts(filename);
             return 0;
+        }
+
+        static int BuildBridge(List<Part> parts, int strength, int port)
+        {
+            int maxStrength = strength;
+            foreach (var part in parts.Where(p => p.Matches(port)))
+            {
+                var copy = new List<Part>(parts);
+                // Remove part
+                copy.Remove(part);
+                // Recurse
+                var newStrength = BuildBridge(copy, strength + part.P1 + part.P2, part.OtherPort(port));
+                if (newStrength > maxStrength)
+                    maxStrength = newStrength;
+            }
+            return maxStrength;
+        }
+
+        public static List<Part> GetParts(string filename) =>
+            File.ReadAllLines(filename)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => new Part(s))
+                .ToList();
+
+        public struct Part
+        {
+            public int P1 { get; }
+            public int P2 { get; }
+
+            public Part(string line)
+            {
+                string[] ports = line.Split('/');
+                int p1 = 0;
+                int.TryParse(ports[0], out p1);
+                P1 = p1;
+                int p2 = 0;
+                int.TryParse(ports[1], out p2);
+                P2 = p2;
+            }
+
+            public Part(Part other)
+            {
+                P1 = other.P1;
+                P2 = other.P2;
+            }
+
+            public bool Matches(int port) => P1 == port || P2 == port;
+
+            public int OtherPort(int port) => P1 == port ? P2 : P1;
+
+            public override string ToString() => $"{P1}/{P2}";
         }
     }
 }
--- AdventOfCode2017/Day24/Day24Tests.cs
+++ AdventOfCode2017/Day24/Day24Tests.cs
@@ -8,18 +8,6 @@ namespace AdventOfCode2017
     {
         const int DAY = 24;
 
-        [Test]
-        public void TestPartOne()
-        {
-            Assert.That(Day24.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
-        [Test]
-        public void TestPartTwo()
-        {
-            Assert.That(Day24.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
         [TestCaseSource(nameof(TestDataOne))]
         public void TestPartOne(string filename, int expected)
         {
@@ -34,8 +22,8 @@ namespace AdventOfCode2017
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), 31);
+            yield return new TestCaseData(PuzzleFile(DAY), 1695);
         }
 
         public static IEnumerable TestDataTwo()
