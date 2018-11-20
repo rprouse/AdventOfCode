9a384edefcd08578a44347c3c1aeb9127ca39858
Rob Prouse <rob.prouse@bentley.com>
2018-11-20 13:51:49 -0500

    Added a project to go back and work on the 2016 problems

---
+++ AdventOfCode2016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.ResetColor();
+            Console.ReadLine();
+
+            await Task.FromResult(false);
+        }
+    }
+}
