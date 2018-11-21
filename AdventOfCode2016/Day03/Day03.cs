1106bc1fda43e1a685f3e5d79c1d6e07020f3609
Rob Prouse <rob.prouse@bentley.com>
2018-11-21 12:38:44 -0500

    2016 day 3 part 1

---
+++ AdventOfCode2016/Day03/Day03.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Linq;
+using AdventOfCode.Core;
+
+namespace AdventOfCode2016
+{
+    public static class Day03
+    {
+        public static int PartOne(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            int valid = 0;
+            foreach(var line in lines)
+            {
+                (int a, int b, int c) = ParseTriangle(line);
+                if (IsValidTriangle(a, b, c))
+                    valid++;
+            }
+            return valid;
+        }
+
+        public static int PartTwo(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            return 0;
+        }
+
+        public static (int, int, int) ParseTriangle(string input)
+        {
+            int.TryParse(input.Substring(0, 5), out int a);
+            int.TryParse(input.Substring(5, 5), out int b);
+            int.TryParse(input.Substring(10, 5), out int c);
+
+            return (a, b, c);
+        }
+
+        public static bool IsValidTriangle(int a, int b, int c)
+        {
+            if (a + b <= c) return false;
+            if (a + c <= b) return false;
+            if (c + b <= a) return false;
+            return true;
+        }
+    }
+}
---
+++ AdventOfCode2016/Day03/Day03Tests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.IO;
+using AdventOfCode.Core;
+using NUnit.Framework;
+
+namespace AdventOfCode2016
+{
+    public class Day03Tests : TestBase
+    {
+        const int DAY = 03;
+
+        [TestCase("    5   10   25", 5, 10, 25)]
+        [TestCase("  541  588  421", 541, 588, 421)]
+        [TestCase("  827  272  126", 827, 272, 126)]
+        [TestCase("  660  514  367", 660, 514, 367)]
+        [TestCase("   39  703  839", 39, 703, 839)]
+        [TestCase("  229  871    3", 229, 871, 3)]
+        public void CanParseTriangle(string input, int a, int b, int c)
+        {
+            (int a1, int b1, int c1) = Day03.ParseTriangle(input);
+            Assert.That(a1, Is.EqualTo(a));
+            Assert.That(b1, Is.EqualTo(b));
+            Assert.That(c1, Is.EqualTo(c));
+        }
+
+        [TestCase(5, 10, 25, false)]
+        [TestCase(541, 588, 421, true)]
+        [TestCase(827, 272, 126, false)]
+        [TestCase(660, 514, 367, true)]
+        [TestCase(39, 703, 839, false)]
+        [TestCase(229, 871, 3, false)]
+        public void IsTriangleValid(int a, int b, int c, bool expected)
+        {
+            bool valid = Day03.IsValidTriangle(a, b, c);
+            Assert.That(valid, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestPartOne()
+        {
+            Assert.That(Day03.PartOne(PuzzleFile(DAY)), Is.EqualTo(993));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.That(Day03.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            Assert.That(Day03.PartTwo(filename), Is.EqualTo(expected));
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
