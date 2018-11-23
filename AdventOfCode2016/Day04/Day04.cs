f9d97074d764095b50914c6849604ee9d8c7a660
Rob Prouse <rob.prouse@bentley.com>
2018-11-23 15:33:06 -0500

    2016 day 4 part 1

--- AdventOfCode2016/Day04/Day04.cs
+++ AdventOfCode2016/Day04/Day04.cs
@@ -1,21 +1,72 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using AdventOfCode.Core;
 
 namespace AdventOfCode2016
 {
-    public static class Day04
+    public class Day04
     {
         public static int PartOne(string filename)
         {
             string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
-            return 0;
+            return lines.Select(l => new Day04(l)).Where(r => r.RealRoom).Sum(r => r.Sector);
         }
 
         public static int PartTwo(string filename)
         {
             string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
             return 0;
+        }
+
+        int _sector;
+
+        public string Name { get; }
+        public int Sector => _sector;
+        public string Checksum { get; }
+        public bool RealRoom { get; }
+
+        public Day04(string code)
+        {
+            Name = code.Substring(0, code.Length - 11);
+            Checksum = code.Substring(code.Length - 6, 5);
+            string sector = code.Substring(code.Length - 10, 3);
+            int.TryParse(sector, out _sector);
+
+            RealRoom = Checksum == CalculateChecksum(Name);
+        }
+
+        internal static string CalculateChecksum(string name)
+        {
+            var chars = name.Replace("-", "").ToCharArray();
+            Array.Sort(chars);
+            var counts = new Dictionary<char, int>();
+            foreach(char c in chars)
+            {
+                if (!counts.ContainsKey(c))
+                    counts[c] = 1;
+                else
+                    counts[c]++;
+            }
+            char[] r = new char[5];
+            for(int i = 0; i < 5; i++)
+            {
+                char maxc = '0';
+                int maxv = -1;
+                foreach(char c in counts.Keys)
+                {
+                    if(counts[c] > maxv)
+                    {
+                        maxc = c;
+                        maxv = counts[c];
+                    }
+                }
+                r[i] = maxc;
+                counts[maxc] = 0;
+            }
+            return new string(r);
         }
     }
 }
--- AdventOfCode2016/Day04/Day04Tests.cs
+++ AdventOfCode2016/Day04/Day04Tests.cs
@@ -7,12 +7,42 @@ namespace AdventOfCode2016
 {
     public class Day04Tests : TestBase
     {
-        const int DAY = 00;
+        const int DAY = 04;
+
+        [TestCase("aaaaa-bbb-z-y-x-123[abxyz]", "aaaaa-bbb-z-y-x", 123, "abxyz")]
+        [TestCase("a-b-c-d-e-f-g-h-987[abcde]", "a-b-c-d-e-f-g-h", 987, "abcde")]
+        [TestCase("not-a-real-room-404[oarel]", "not-a-real-room", 404, "oarel")]
+        [TestCase("totally-real-room-200[decoy]", "totally-real-room", 200, "decoy")]
+        public void CanParseRooms(string code, string name, int sector, string checksum)
+        {
+            var day = new Day04(code);
+            Assert.That(day.Name, Is.EqualTo(name));
+            Assert.That(day.Sector, Is.EqualTo(sector));
+            Assert.That(day.Checksum, Is.EqualTo(checksum));
+        }
+
+        [TestCase("aaaaa-bbb-z-y-x-123[abxyz]", true)]
+        [TestCase("a-b-c-d-e-f-g-h-987[abcde]", true)]
+        [TestCase("not-a-real-room-404[oarel]", true)]
+        [TestCase("totally-real-room-200[decoy]", false)]
+        public void CanFindRealRooms(string code, bool real)
+        {
+            var day = new Day04(code);
+            Assert.That(day.RealRoom, Is.EqualTo(real));
+        }
+
+        [TestCase("aaaaa-bbb-z-y-x", "abxyz")]
+        [TestCase("a-b-c-d-e-f-g-h", "abcde")]
+        [TestCase("not-a-real-room", "oarel")]
+        public void CanCalculateChecksum(string name, string checksum)
+        {
+            Assert.That(Day04.CalculateChecksum(name), Is.EqualTo(checksum));
+        }
 
         [Test]
         public void TestPartOne()
         {
-            Assert.That(Day04.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day04.PartOne(PuzzleFile(DAY)), Is.EqualTo(158835));
         }
 
         [Test]
@@ -21,22 +51,10 @@ namespace AdventOfCode2016
             Assert.That(Day04.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
         }
 
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
-        {
-            Assert.That(Day04.PartOne(filename), Is.EqualTo(expected));
-        }
-
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
             Assert.That(Day04.PartTwo(filename), Is.EqualTo(expected));
-        }
-
-        public static IEnumerable TestDataOne()
-        {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
         }
 
         public static IEnumerable TestDataTwo()
