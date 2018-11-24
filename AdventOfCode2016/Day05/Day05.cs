18b579a26d48d1b9edec32d654452ec4d9200897
Rob Prouse <rob@prouse.org>
2018-11-24 16:02:53 -0500

    Added tests for 2016 day 5

---
+++ AdventOfCode2016/Day05/Day05.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Linq;
+using AdventOfCode.Core;
+
+namespace AdventOfCode2016
+{
+    public static class Day05
+    {
+        public static int PartOne(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            return 0;
+        }
+
+        public static int PartTwo(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            return 0;
+        }
+    }
+}
