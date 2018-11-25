b896a9db3fd9e0539c0bad73b2bb8bfcfc7fcca1
Rob Prouse <rob@prouse.org>
2018-11-25 10:14:12 -0500

    Refactored 2016 day 6 to eliminate duplicate code

--- AdventOfCode2016/Day06/Day06.cs
+++ AdventOfCode2016/Day06/Day06.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Core;
 
@@ -7,34 +7,24 @@ namespace AdventOfCode2016
 {
     public static class Day06
     {
-        public static string PartOne(string filename)
+        public static string PartOne(string filename) =>
+            Decrypt(filename, MostCommonChar);
+
+        public static string PartTwo(string filename) =>
+            Decrypt(filename, LeastCommonChar);
+
+        internal static string Decrypt(string filename, Func<IEnumerable<string>, int, char> decryptor)
         {
             string[] lines = filename.ReadAllLines();
             char[] c = Enumerable.Range(0, lines[0].Length)
-                .Select(i => MostCommonChar(lines, i))
+                .Select(i => decryptor(lines, i))
                 .ToArray();
             return new string(c);
         }
 
-        public static string PartTwo(string filename)
+        internal static char MostCommonChar(IEnumerable<string> lines, int pos)
         {
-            string[] lines = filename.ReadAllLines();
-            char[] c = Enumerable.Range(0, lines[0].Length)
-                .Select(i => LeastCommonChar(lines, i))
-                .ToArray();
-            return new string(c);
-        }
-
-        public static char MostCommonChar(string[] lines, int pos)
-        {
-            var counts = new Dictionary<char, int>();
-            foreach(char c in lines.Select(l => l[pos]))
-            {
-                if (!counts.ContainsKey(c))
-                    counts[c] = 1;
-                else
-                    counts[c]++;
-            }
+            var counts = CountCharacters(lines, pos);
             char maxc = char.MinValue;
             int maxv = -1;
             foreach (char c in counts.Keys)
@@ -48,16 +38,9 @@ namespace AdventOfCode2016
             return maxc;
         }
 
-        public static char LeastCommonChar(string[] lines, int pos)
+        internal static char LeastCommonChar(IEnumerable<string> lines, int pos)
         {
-            var counts = new Dictionary<char, int>();
-            foreach (char c in lines.Select(l => l[pos]))
-            {
-                if (!counts.ContainsKey(c))
-                    counts[c] = 1;
-                else
-                    counts[c]++;
-            }
+            var counts = CountCharacters(lines, pos);
             char minc = char.MinValue;
             int minv = int.MaxValue;
             foreach (char c in counts.Keys)
@@ -69,6 +52,20 @@ namespace AdventOfCode2016
                 }
             }
             return minc;
+        }
+
+        private static Dictionary<char, int> CountCharacters(IEnumerable<string> lines, int pos)
+        {
+            var counts = new Dictionary<char, int>();
+            foreach (char c in lines.Select(l => l[pos]))
+            {
+                if (!counts.ContainsKey(c))
+                    counts[c] = 1;
+                else
+                    counts[c]++;
+            }
+
+            return counts;
         }
     }
 }
