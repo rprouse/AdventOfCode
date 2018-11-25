952dbcec7a87f36fa4a4f0d10d91c22b5f203b71
Rob Prouse <rob@prouse.org>
2018-11-25 08:41:11 -0500

    2016 day 5 part 1

--- AdventOfCode2016/Day05/Day05.cs
+++ AdventOfCode2016/Day05/Day05.cs
@@ -1,21 +1,45 @@
 ﻿using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using AdventOfCode.Core;
 
 namespace AdventOfCode2016
 {
     public static class Day05
     {
-        public static int PartOne(string filename)
+        public static string PartOne(string door)
         {
-            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
-            return 0;
+            var chars = Enumerable.Range(0, 1000000000)
+                .Select(i => CreateMD5($"{door}{i}"))
+                .Where(h => h.StartsWith("00000"))
+                .Select(h => h[5])
+                .Take(8)
+                .ToArray();
+            return new string(chars);
         }
 
-        public static int PartTwo(string filename)
+        public static string PartTwo(string door)
         {
-            string[] lines = File.ReadAllLines(filename).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
-            return 0;
+            return "";
+        }
+
+        static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
         }
     }
 }
--- AdventOfCode2016/Day05/Day05Tests.cs
+++ AdventOfCode2016/Day05/Day05Tests.cs
@@ -9,40 +9,17 @@ namespace AdventOfCode2016
     {
         const int DAY = 05;
 
-        [Test]
-        public void TestPartOne()
+        [TestCase("abc", "18f47a30")]
+        [TestCase("wtnhxymk", "")]
+        public void TestPartOne(string door, string password)
         {
-            Assert.That(Day05.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day05.PartOne(door), Is.EqualTo(password));
         }
 
         [Test]
         public void TestPartTwo()
         {
-            Assert.That(Day05.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
-        {
-            Assert.That(Day05.PartOne(filename), Is.EqualTo(expected));
-        }
-
-        [TestCaseSource(nameof(TestDataTwo))]
-        public void TestPartTwo(string filename, int expected)
-        {
-            Assert.That(Day05.PartTwo(filename), Is.EqualTo(expected));
-        }
-
-        public static IEnumerable TestDataOne()
-        {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
-        }
-
-        public static IEnumerable TestDataTwo()
-        {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            Assert.That(Day05.PartTwo(""), Is.EqualTo(""));
         }
     }
 }
