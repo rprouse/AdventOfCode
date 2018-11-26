cc38d121b049ccdb7320bcbbe264ce8b60f093de
Rob Prouse <rob.prouse@bentley.com>
2018-11-26 18:51:51 -0500

    2016 day 7 part 1

--- AdventOfCode2016/Day07/Day07.cs
+++ AdventOfCode2016/Day07/Day07.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using AdventOfCode.Core;
 
 namespace AdventOfCode2016
@@ -10,13 +13,57 @@ namespace AdventOfCode2016
         public static int PartOne(string filename)
         {
             string[] lines = filename.ReadAllLines();
-            return 0;
+            return lines.Where(l => SupportsTLS(l)).Count();
         }
 
         public static int PartTwo(string filename)
         {
             string[] lines = filename.ReadAllLines();
             return 0;
+        }
+
+        public static bool SupportsTLS(string ipv7)
+        {
+            var sequences = GetHypernetSequences(ipv7);
+            if (ContainsAbba(sequences))
+                return false;
+
+            var parts = StripHypernetSequences(ipv7);
+            return ContainsAbba(parts);
+        }
+
+        private static bool ContainsAbba(IEnumerable<string> parts)
+        {
+            foreach (var part in parts)
+            {
+                for (int i = 0; i <= part.Length - 4; i++)
+                {
+                    if (SupportsTLS(part, i))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        static Regex sequences = new Regex(@"\[(\w+)\]", RegexOptions.Compiled);
+        static Regex split = new Regex(@"\[\w+\]", RegexOptions.Compiled);
+
+        internal static bool SupportsTLS(string ipv7, int i) =>
+            ipv7[i] == ipv7[i + 3] &&
+            ipv7[i + 1] == ipv7[i + 2] &&
+            ipv7[i] != ipv7[i + 1];
+
+        internal static string[] StripHypernetSequences(string ipv7) =>
+            split.Split(ipv7);
+
+        internal static IEnumerable<string> GetHypernetSequences(string ipv7)
+        {
+            var matches = sequences.Matches(ipv7);
+            foreach(Match match in matches)
+            {
+                if (match.Success)
+                    yield return match.Groups[1].Value;
+            }
         }
     }
 }
--- AdventOfCode2016/Day07/Day07Tests.cs
+++ AdventOfCode2016/Day07/Day07Tests.cs
@@ -9,10 +9,38 @@ namespace AdventOfCode2016
     {
         const int DAY = 07;
 
+        [TestCase("abba[mnop]qrst", true)]
+        [TestCase("abcd[bddb]xyyx", false)]
+        [TestCase("aaaa[qwer]tyui", false)]
+        [TestCase("ioxxoj[asdfgh]zxcvbn", true)]
+        public void TestTLSSupport(string ipv7, bool supportsTls)
+        {
+            Assert.That(Day07.SupportsTLS(ipv7), Is.EqualTo(supportsTls));
+        }
+
+        [TestCase(0, true)]
+        [TestCase(1, false)]
+        public void TestTLSSupportAtIndex(int index, bool supportsTls)
+        {
+            Assert.That(Day07.SupportsTLS("abbaqrst", index), Is.EqualTo(supportsTls));
+        }
+
+        [TestCaseSource(nameof(TestOutsideBrackets))]
+        public void CanStripHypernetSequences(string ipv7, string[] stripped)
+        {
+            Assert.That(Day07.StripHypernetSequences(ipv7), Is.EqualTo(stripped));
+        }
+
+        [TestCaseSource(nameof(TestInsideBrackets))]
+        public void CanGetHypernetSequences(string ipv7, string[] sequences)
+        {
+            Assert.That(Day07.GetHypernetSequences(ipv7), Is.EqualTo(sequences));
+        }
+
         [Test]
         public void TestPartOne()
         {
-            Assert.That(Day07.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day07.PartOne(PuzzleFile(DAY)), Is.EqualTo(105));
         }
 
         [Test]
@@ -21,22 +49,30 @@ namespace AdventOfCode2016
             Assert.That(Day07.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
         }
 
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
-        {
-            Assert.That(Day07.PartOne(filename), Is.EqualTo(expected));
-        }
-
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
             Assert.That(Day07.PartTwo(filename), Is.EqualTo(expected));
         }
 
-        public static IEnumerable TestDataOne()
+        public static IEnumerable TestOutsideBrackets()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData("abbaqrst", new [] { "abbaqrst" });
+            yield return new TestCaseData("abba[mnop]qrst", new[] { "abba", "qrst" });
+            yield return new TestCaseData("abcd[bddb]xyyx", new[] { "abcd", "xyyx" });
+            yield return new TestCaseData("aaaa[qwer]tyui", new[] { "aaaa", "tyui" });
+            yield return new TestCaseData("ioxxoj[asdfgh]zxcvbn", new[] { "ioxxoj", "zxcvbn" });
+            yield return new TestCaseData("ioxxoj[asdfgh]zxcvbn[sdfas]sldfjk", new[] { "ioxxoj", "zxcvbn", "sldfjk" });
+        }
+
+        public static IEnumerable TestInsideBrackets()
+        {
+            yield return new TestCaseData("abbaqrst", new string[] { });
+            yield return new TestCaseData("abba[mnop]qrst", new[] { "mnop" });
+            yield return new TestCaseData("abcd[bddb]xyyx", new[] { "bddb" });
+            yield return new TestCaseData("aaaa[qwer]tyui", new[] { "qwer" });
+            yield return new TestCaseData("ioxxoj[asdfgh]zxcvbn", new[] { "asdfgh" });
+            yield return new TestCaseData("ioxxoj[asdfgh]zxcvbn[sdfas]sldfjk", new[] { "asdfgh", "sdfas" });
         }
 
         public static IEnumerable TestDataTwo()
