270340fdd415aa64967dce6bce1dd42edbd72697
Rob Prouse <rob.prouse@bentley.com>
2018-11-29 14:39:39 -0500

    2016 day 9 part 1

--- AdventOfCode2016/Day09/Day09.cs
+++ AdventOfCode2016/Day09/Day09.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using AdventOfCode.Core;
 
 namespace AdventOfCode2016
 {
     public static class Day09
     {
-        public static int PartOne(string filename)
+        static Regex regex = new Regex(@"\((\d+)x(\d+)\)", RegexOptions.Compiled);
+
+        public static string Decompress(string compressed)
         {
-            string[] lines = filename.ReadAllLines();
-            return 0;
+            var sb = new StringBuilder();
+            var match = regex.Match(compressed);
+            while (match.Success)
+            {
+                // Add any text before the match
+                sb.Append(compressed.Substring(0, match.Index));
+
+                // Get the number of characters (c) and the multiplier (n)
+                int.TryParse(match.Groups[1].Value, out int c);
+                int.TryParse(match.Groups[2].Value, out int n);
+
+                // Multiply it!
+                var str = compressed.Substring(match.Index + match.Length, c);
+                for (int i = 0; i < n; i++)
+                    sb.Append(str);
+
+                // Take what's left
+                compressed = compressed.Substring(match.Index + match.Length + c);
+                match = regex.Match(compressed);
+            }
+            sb.Append(compressed);
+            return sb.ToString();
         }
 
         public static int PartTwo(string filename)
--- AdventOfCode2016/Day09/Day09Tests.cs
+++ AdventOfCode2016/Day09/Day09Tests.cs
@@ -9,10 +9,16 @@ namespace AdventOfCode2016
     {
         const int DAY = 09;
 
-        [Test]
-        public void TestPartOne()
+        [TestCaseSource(nameof(DecompressedData))]
+        public void TestDecompression(string compressed, string decompressed)
         {
-            Assert.That(Day09.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day09.Decompress(compressed), Is.EqualTo(decompressed));
+        }
+
+        [TestCaseSource(nameof(CompressedLengthData))]
+        public void TestUncompressedLength(string compressed, int expected)
+        {
+            Assert.That(Day09.Decompress(compressed).Length, Is.EqualTo(expected));
         }
 
         [Test]
@@ -21,22 +27,31 @@ namespace AdventOfCode2016
             Assert.That(Day09.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
         }
 
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
-        {
-            Assert.That(Day09.PartOne(filename), Is.EqualTo(expected));
-        }
-
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
             Assert.That(Day09.PartTwo(filename), Is.EqualTo(expected));
         }
 
-        public static IEnumerable TestDataOne()
+        public static IEnumerable DecompressedData()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData("ADVENT", "ADVENT");
+            yield return new TestCaseData("A(1x5)BC", "ABBBBBC");
+            yield return new TestCaseData("(3x3)XYZ", "XYZXYZXYZ");
+            yield return new TestCaseData("A(2x2)BCD(2x2)EFG", "ABCBCDEFEFG");
+            yield return new TestCaseData("(6x1)(1x3)A", "(1x3)A");
+            yield return new TestCaseData("X(8x2)(3x3)ABCY", "X(3x3)ABC(3x3)ABCY");
+        }
+
+        public static IEnumerable CompressedLengthData()
+        {
+            yield return new TestCaseData("ADVENT", 6);
+            yield return new TestCaseData("A(1x5)BC", 7);
+            yield return new TestCaseData("(3x3)XYZ", 9);
+            yield return new TestCaseData("A(2x2)BCD(2x2)EFG", 11);
+            yield return new TestCaseData("(6x1)(1x3)A", 6);
+            yield return new TestCaseData("X(8x2)(3x3)ABCY", 18);
+            yield return new TestCaseData(PuzzleFile(DAY).ReadFirstLine(), 152851);
         }
 
         public static IEnumerable TestDataTwo()
