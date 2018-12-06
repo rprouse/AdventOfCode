b22e175f596047d135f66f20a8c8506f2f9716e4
Rob Prouse <rob.prouse@bentley.com>
2018-12-06 16:42:08 -0500

    2018 day 6 part 1

--- AdventOfCode2018/Day06/Day06.cs
+++ AdventOfCode2018/Day06/Day06.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
@@ -9,14 +10,73 @@ namespace AdventOfCode2018
     {
         public static int PartOne(string filename)
         {
-            string[] lines = filename.ReadAllLines();
-            return 0;
+            (int, int)[] coords = GetCoordinates(filename);
+            int maxX = coords.Select(c => c.Item1).Max() + 1;
+            int maxY = coords.Select(c => c.Item2).Max() + 1;
+            int[,] grid = new int[maxX, maxY];
+            for (int y = 0; y < maxY; y++)
+            {
+                for (int x = 0; x < maxX; x++)
+                {
+                    grid[x, y] = ClosestTo(coords, x, y);
+                }
+            }
+
+            // Zero out everything from the edges
+            var infinite = new List<int>();
+            for(int x = 0; x < maxX; x++)
+            {
+                if (grid[x, 0] != -1) infinite.Add(grid[x, 0]);
+                if (grid[x, maxY-1] != -1) infinite.Add(grid[x, maxY-1]);
+            }
+            for (int y = 0; y < maxY; y++)
+            {
+                if (grid[0, y] != -1) infinite.Add(grid[0, y]);
+                if (grid[maxX-1, y] != -1) infinite.Add(grid[maxX-1, y]);
+            }
+            infinite = infinite.Distinct().ToList();
+            for (int y = 0; y < maxY; y++)
+            {
+                for (int x = 0; x < maxX; x++)
+                {
+                    if (infinite.Any(i => i == grid[x, y]))
+                        grid[x, y] = -1;
+                }
+            }
+
+            // Now count the number of each that is left
+            var ga = grid.Cast<int>().ToArray();
+            int[] distinct = ga.Distinct().Where(i => i != -1).ToArray();
+            return distinct.Select(d => ga.Count(i => i == d)).Max();
         }
 
         public static int PartTwo(string filename)
         {
-            string[] lines = filename.ReadAllLines();
+            (int, int)[] coords = GetCoordinates(filename);
             return 0;
+        }
+
+        public static (int, int)[] GetCoordinates(string filename) =>
+            filename.ReadAllLines()
+                .Select(l => Parse(l))
+                .ToArray();
+
+        public static (int, int) Parse(string line)
+        {
+            var parts = line.Split(',');
+            return (parts[0].ToInt(), parts[1].ToInt());
+        }
+
+        public static int Distance(int x1, int y1, int x2, int y2) =>
+            Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+
+        public static int ClosestTo((int, int)[] coords, int x, int y)
+        {
+            int[] distances = coords.Select(c => Distance(c.Item1, c.Item2, x, y)).ToArray();
+            int min = distances.Min();
+            int i1 = Array.FindIndex(distances, i => i == min);
+            int i2 = Array.FindLastIndex(distances, i => i == min);
+            return i1 == i2 ? i1 : -1;
         }
     }
 }
--- AdventOfCode2018/Day06/Day06Tests.cs
+++ AdventOfCode2018/Day06/Day06Tests.cs
@@ -10,21 +10,36 @@ namespace AdventOfCode2018
         const int DAY = 06;
 
         [Test]
-        public void TestPartOne()
+        public void CanParse()
         {
-            Assert.That(Day06.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            (int x, int y) = Day06.Parse("266, 253");
+            Assert.That(x, Is.EqualTo(266));
+            Assert.That(y, Is.EqualTo(253));
         }
 
-        [Test]
-        public void TestPartTwo()
+        [TestCase(0, 0, 0, TestName = "MyTest")]
+        [TestCase(5, 0, -1)]
+        [TestCase(5, 2, 4)]
+        [TestCase(3, 5, 3)]
+        [TestCase(5, 8, 4)]
+        [TestCase(6, 8, 5)]
+        public void CanDetermineClosestTo(int x, int y, int expected)
         {
-            Assert.That(Day06.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
+            (int, int)[] coords = Day06.GetCoordinates(TestFile(DAY));
+            int closest = Day06.ClosestTo(coords, x, y);
+            Assert.That(closest, Is.EqualTo(expected));
         }
 
         [TestCaseSource(nameof(TestDataOne))]
         public void TestPartOne(string filename, int expected)
         {
             Assert.That(Day06.PartOne(filename), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.That(Day06.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
@@ -35,8 +50,8 @@ namespace AdventOfCode2018
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), 17);
+            yield return new TestCaseData(PuzzleFile(DAY), 5532);
         }
 
         public static IEnumerable TestDataTwo()
