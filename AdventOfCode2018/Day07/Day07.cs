e6c13485d3f34a0744a0713314d36f088e59f9c4
Rob Prouse <rob.prouse@bentley.com>
2018-12-07 13:12:02 -0500

    Solution to 2018 day 7 part 1

--- AdventOfCode2018/Day07/Day07.cs
+++ AdventOfCode2018/Day07/Day07.cs
@@ -7,10 +7,70 @@ namespace AdventOfCode2018
 {
     public static class Day07
     {
-        public static int PartOne(string filename)
+        public class Step
+        {
+            public Step(char name)
+            {
+                Name = name;
+            }
+
+            public char Name { get; set; }
+
+            public Dictionary<char, Step> Next { get; } = new Dictionary<char, Step>();
+            public Dictionary<char, Step> Prev { get; } = new Dictionary<char, Step>();
+        }
+
+        public static string PartOne(string filename)
         {
             string[] lines = filename.ReadAllLines();
-            return 0;
+
+            // Build the tree
+            var steps = BuildTree(lines);
+
+            // Find the first steps
+            return new string(WalkTree(steps).ToArray());
+        }
+
+        private static Dictionary<char, Step> BuildTree(string[] lines)
+        {
+            var steps = new Dictionary<char, Step>();
+            foreach (string line in lines)
+            {
+                char s = line[5];   // Step name
+                char n = line[36];  // Next step
+                if (!steps.ContainsKey(s))
+                    steps.Add(s, new Step(s));
+
+                if (!steps.ContainsKey(n))
+                    steps.Add(n, new Step(n));
+
+                var step = steps[s];
+                var next = steps[n];
+                step.Next.Add(n, next);
+                next.Prev.Add(s, step);
+            }
+            return steps;
+        }
+
+        private static IEnumerable<char> WalkTree(Dictionary<char, Step> steps)
+        {
+            while (steps.Count > 0)
+            {
+                // Find the steps that have no predecessors and take lowest alphabetically
+                char firstKey = steps.Values.Where(s => s.Prev.Count == 0)
+                    .Select(s => s.Name)
+                    .Min();
+
+                Step first = steps[firstKey];
+
+                // Remove the first from the tree
+                steps.Remove(firstKey);
+                foreach(var next in first.Next.Values)
+                {
+                    next.Prev.Remove(firstKey);
+                }
+                yield return firstKey;
+            }
         }
 
         public static int PartTwo(string filename)
--- AdventOfCode2018/Day07/Day07Tests.cs
+++ AdventOfCode2018/Day07/Day07Tests.cs
@@ -9,22 +9,16 @@ namespace AdventOfCode2018
     {
         const int DAY = 07;
 
-        [Test]
-        public void TestPartOne()
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, string expected)
         {
-            Assert.That(Day07.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day07.PartOne(filename), Is.EqualTo(expected));
         }
 
         [Test]
         public void TestPartTwo()
         {
             Assert.That(Day07.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
-        {
-            Assert.That(Day07.PartOne(filename), Is.EqualTo(expected));
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
@@ -35,8 +29,8 @@ namespace AdventOfCode2018
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), "CABDFE");
+            yield return new TestCaseData(PuzzleFile(DAY), "OUGLTKDJVBRMIXSACWYPEQNHZF");
         }
 
         public static IEnumerable TestDataTwo()
