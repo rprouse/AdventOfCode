c06d422ee77603eb3740ba65434339eded6a3bf7
Rob Prouse <rob@prouse.org>
2018-12-13 19:30:38 -0500

    Day 12 part 1

--- AdventOfCode2018/Day12/Day12.cs
+++ AdventOfCode2018/Day12/Day12.cs
@@ -9,10 +9,47 @@ namespace AdventOfCode2018
 {
     public static class Day12
     {
-        public static int PartOne(string filename)
+        public static int PartOne(string filename, string initialState)
         {
-            string[] lines = filename.ReadAllLines();
-            return 0;
+            var rules = ParseRules(filename);
+
+            var sb = new StringBuilder(".....");
+            sb.Append(initialState);
+            sb.Append(".....");
+            var pots = sb.ToString();
+
+            foreach (int gen in Enumerable.Range(0, 20))
+            {
+                sb = new StringBuilder(".....");
+                for (int i = 2; i < pots.Length - 2; i++)
+                {
+                    var compare = pots.Substring(i - 2, 5);
+                    sb.Append(rules.Any(r => r == compare) ? '#' : '.');
+                }
+                sb.Append(".....");
+                pots = sb.ToString();
+            }
+            int offset = (pots.Length - initialState.Length) / 2;
+            int sum = 0;
+            for(int i = 0; i < pots.Length; i++)
+            {
+                if (pots[i] == '#')
+                    sum += i - offset;
+            }
+            return sum;
+        }
+
+        private static string[] ParseRules(string filename) =>
+            filename.ReadAllLines()
+                .Where(l => l.EndsWith("#"))
+                .Select(l => l.Substring(0, 5))
+                .ToArray();
+
+        private static char[] EmptyPots()
+        {
+            var pots = new char[150];
+            Enumerable.Range(0, 150).ForEach(i => pots[i] = '.');
+            return pots;
         }
 
         public static int PartTwo(string filename)
