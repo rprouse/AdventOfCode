75eda0b028bcb45a7deca06fc718e96e1fb6e731
Rob Prouse <rob@prouse.org>
2019-12-02 12:30:55 -0500

    Setup for Advent of Code 2019

---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using AdventOfCode.Core;
+using NUnit.Framework;
+
+[assembly:Parallelizable(ParallelScope.Fixtures)]
+
+namespace AdventOfCode2019
+{
+    public static class ProgramEntry
+    {
+        public static async Task Main()
+        {
+            //Day15.PartOne(TestBase.TestFile(15, "Test1.txt"));
+            //Day15.PartOne(TestBase.PuzzleFile(15));
+            //Day17.PartOne(TestBase.PuzzleFile(17));
+            //Day17.PartOne(TestBase.TestFile(17));
+
+            Console.ResetColor();
+            Console.ReadLine();
+
+            await Task.FromResult(false);
+        }
+    }
+}
