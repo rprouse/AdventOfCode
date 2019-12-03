5bcca62178d6bf9bafa65648b3dde1543c61bc1f
Rob Prouse <rob@prouse.org>
2019-12-03 00:47:10 -0500

    inefficient solution to day 3 part 1

--- AdventOfCode2019/Day03/Day03.cs
+++ AdventOfCode2019/Day03/Day03.cs
@@ -9,10 +9,80 @@ namespace AdventOfCode2019
 {
     public static class Day03
     {
+        const int SIZE = 13000;
+
         public static int PartOne(string filename)
         {
             string[] lines = filename.ReadAllLines();
-            return 0;
+            return PartOne(lines[0], lines[1]);
+        }
+
+        public static int PartOne(string wire1, string wire2)
+        {
+            bool[,] path1 = FollowWire(wire1);
+            bool[,] path2 = FollowWire(wire2);
+            int minDist = int.MaxValue;
+
+            for(int x = 0; x < SIZE * 2; x++)
+            for(int y = 0; y < SIZE * 2; y++)
+            {
+                if(path1[x,y] && path2[x,y])
+                {
+                    int d = Distance(x, y);
+                    if (d < minDist)
+                        minDist = d;
+                }
+            }
+
+            return minDist;
+        }
+
+        static int Distance(int x, int y) =>
+            Math.Abs(SIZE - x) + Math.Abs(SIZE - y);
+
+        static bool[,] FollowWire(string wire)
+        {
+            bool[,] board = new bool[SIZE * 2, SIZE * 2];
+
+            int x = SIZE;
+            int y = SIZE;
+
+            string[] steps = wire.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string step in steps)
+            {
+                char dir = step[0];
+                int dist = step.Substring(1).ToInt();
+                switch(dir)
+                {
+                    case 'U':
+                        for(int c = 0; c < dist; c++)
+                        {
+                            board[x, --y] = true;
+                        }
+                        break;
+                    case 'D':
+                        for (int c = 0; c < dist; c++)
+                        {
+                            board[x, ++y] = true;
+                        }
+                        break;
+                    case 'L':
+                        for (int c = 0; c < dist; c++)
+                        {
+                            board[--x, y] = true;
+                        }
+                        break;
+                    case 'R':
+                        for (int c = 0; c < dist; c++)
+                        {
+                            board[++x, y] = true;
+                        }
+                        break;
+                }
+            }
+
+            return board;
         }
 
         public static int PartTwo(string filename)
--- AdventOfCode2019/Day03/Day03Tests.cs
+++ AdventOfCode2019/Day03/Day03Tests.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode2019
         [Test]
         public void TestPartOne()
         {
-            Assert.That(Day03.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day03.PartOne(PuzzleFile(DAY)), Is.EqualTo(1195));
         }
 
         [Test]
@@ -21,10 +21,12 @@ namespace AdventOfCode2019
             Assert.That(Day03.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
         }
 
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
+        [TestCase("R8,U5,L5,D3,", "U7,R6,D4,L4,", 6)]
+        [TestCase("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159)]
+        [TestCase("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)]
+        public void TestPartOne(string wire1, string wire2, int expected)
         {
-            Assert.That(Day03.PartOne(filename), Is.EqualTo(expected));
+            Assert.That(Day03.PartOne(wire1, wire2), Is.EqualTo(expected));
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
