4f722792dc237df1d11ea20751369903633fd614
Rob Prouse <rob@prouse.org>
2019-12-04 08:02:12 -0500

    Day 4 part 2

--- AdventOfCode2019/Day04/Day04.cs
+++ AdventOfCode2019/Day04/Day04.cs
@@ -34,6 +34,47 @@ namespace AdventOfCode2019
             return twoSame;
         }
 
+        public static int PartTwo(int start, int end)
+        {
+            int count = 0;
+            for (int pass = start; pass <= end; pass++)
+            {
+                if (IsValidPassword2(pass)) count++;
+            }
+            return count;
+        }
+
+        public static bool IsValidPassword2(int n)
+        {
+            bool twoSameWithoutDup = false;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int a = DigitAt(n, i);
+                int b = DigitAt(n, i + 1);
+
+                if (a > b) return false;
+                if (a == b)
+                {
+                    bool foundDup = false;
+                    for (int j = 0; j < 6; j++)
+                    {
+                        if (j == i || j == i + 1)
+                            continue;
+
+                        int c = DigitAt(n, j);
+                        if(a == c)
+                        {
+                            foundDup = true;
+                            break;
+                        }
+                    }
+                    if(!foundDup) twoSameWithoutDup = true;
+                }
+            }
+            return twoSameWithoutDup;
+        }
+
         public static int DigitAt(int pass, int d)
         {
             for (int i = 0; i < 5 - d; i++)
--- AdventOfCode2019/Day04/Day04Tests.cs
+++ AdventOfCode2019/Day04/Day04Tests.cs
@@ -18,7 +18,7 @@ namespace AdventOfCode2019
         [Test]
         public void TestPartTwo()
         {
-            //Assert.That(Day04.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day04.PartTwo(108457, 562041), Is.EqualTo(1972));
         }
 
         [TestCase(111111, true)]
@@ -30,6 +30,14 @@ namespace AdventOfCode2019
         public void CanFindValidPass(int pass, bool expected)
         {
             Assert.That(Day04.IsValidPassword(pass), Is.EqualTo(expected));
+        }
+
+        [TestCase(112233, true)]
+        [TestCase(123444, false)]
+        [TestCase(111122, true)]
+        public void CanFindValidPass2(int pass, bool expected)
+        {
+            Assert.That(Day04.IsValidPassword2(pass), Is.EqualTo(expected));
         }
 
         [Test]
