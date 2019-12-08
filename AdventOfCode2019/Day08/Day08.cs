8504bcd51ad19c3cbce78c6d0b4ea78f38f12b5f
Rob Prouse <rob@prouse.org>
2019-12-08 09:44:46 -0500

    Solution to day 8 part 2

--- AdventOfCode2019/Day08/Day08.cs
+++ AdventOfCode2019/Day08/Day08.cs
@@ -11,16 +11,7 @@ namespace AdventOfCode2019
     {
         public static int PartOne(string filename, int width, int height)
         {
-            string encoded = filename.ReadAll();
-            var layers = new List<string>();
-            int pos = 0;
-
-            // Get the layers
-            while(pos < encoded.Length)
-            {
-                layers.Add(encoded.Substring(pos, width * height));
-                pos += width * height;
-            }
+            List<string> layers = GetLayers(filename.ReadAll(), width, height);
 
             // Find layer with fewest zeros
             int min = layers.Min(l => CountNumber(l, '0'));
@@ -30,13 +21,50 @@ namespace AdventOfCode2019
             return CountNumber(minLayer, '1') * CountNumber(minLayer, '2');
         }
 
+        private static List<string> GetLayers(string encoded, int width, int height)
+        {
+            var layers = new List<string>();
+            int pos = 0;
+
+            // Get the layers
+            while (pos < encoded.Length)
+            {
+                layers.Add(encoded.Substring(pos, width * height));
+                pos += width * height;
+            }
+
+            return layers;
+        }
+
         public static int CountNumber(string layer, char n) =>
             layer.Count(c => c == n);
 
-        public static int PartTwo(string filename)
+        public static string PartTwo(string filename, int width, int height)
         {
-            string encoded = filename.ReadAll();
-            return 0;
+            List<string> layers = GetLayers(filename.ReadAll(), width, height);
+            char[] image = new char[width * height];
+            for(int pos = 0; pos < image.Length; pos++)
+            {
+                image[pos] = GetColorAtPos(layers, pos);
+            }
+
+            string final = DivideImageIntoRows(new string(image), width, height);
+            Console.WriteLine(final);
+            return final;
+        }
+
+        static char GetColorAtPos(IEnumerable<string> layers, int pos) =>
+            layers.Select(l => l[pos]).First(c => c != '2');
+
+        static string DivideImageIntoRows(string image, int width, int height)
+        {
+            var builder = new StringBuilder(width * height);
+
+            for(int pos = 0; pos < width * height; pos += width)
+            {
+                builder.AppendLine(image.Substring(pos, width));
+            }
+            return builder.ToString();
         }
     }
 }
--- AdventOfCode2019/Day08/Day08Tests.cs
+++ AdventOfCode2019/Day08/Day08Tests.cs
@@ -9,12 +9,6 @@ namespace AdventOfCode2019
     {
         const int DAY = 08;
 
-        [Test]
-        public void TestPartTwo()
-        {
-            Assert.That(Day08.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
         [TestCaseSource(nameof(TestDataOne))]
         public void TestPartOne(string filename, int width, int height, int expected)
         {
@@ -22,21 +16,21 @@ namespace AdventOfCode2019
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
-        public void TestPartTwo(string filename, int expected)
+        public void TestPartTwo(string filename, int width, int height, string expected)
         {
-            Assert.That(Day08.PartTwo(filename), Is.EqualTo(expected));
+            Assert.That(Day08.PartTwo(filename, width, height), Is.EqualTo(expected));
         }
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 3, 2, 1);
+            yield return new TestCaseData(TestFile(DAY, "Test1.txt"), 3, 2, 1);
             yield return new TestCaseData(PuzzleFile(DAY), 25, 6, 1360);
         }
 
         public static IEnumerable TestDataTwo()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY, "Test2.txt"), 2, 2, "01\r\n10\r\n");
+            yield return new TestCaseData(PuzzleFile(DAY), 25, 6, "1111011100100100110011100\r\n1000010010100101001010010\r\n1110010010100101001010010\r\n1000011100100101111011100\r\n1000010000100101001010100\r\n1000010000011001001010010\r\n");
         }
     }
 }
