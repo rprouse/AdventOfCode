cc4fc444ef156e1c2adf579ee949862a03d4991f
Rob Prouse <rob@prouse.org>
2019-12-10 16:33:06 -0500

    Prep for day 11

---
+++ AdventOfCode2019/Day11/Day00Tests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.IO;
+using AdventOfCode.Core;
+using NUnit.Framework;
+
+namespace AdventOfCode2019
+{
+    public class Day11Tests : TestBase
+    {
+        const int DAY = 11;
+
+        [Test]
+        public void TestPartOne()
+        {
+            Assert.That(Day11.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.That(Day11.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
+        }
+
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, int expected)
+        {
+            Assert.That(Day11.PartOne(filename), Is.EqualTo(expected));
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            Assert.That(Day11.PartTwo(filename), Is.EqualTo(expected));
+        }
+
+        public static IEnumerable TestDataOne()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
