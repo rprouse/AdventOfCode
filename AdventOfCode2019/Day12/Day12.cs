d99a7a128861acc4d6194416d097fa69f826f593
Rob Prouse <rob@prouse.org>
2019-12-12 09:26:04 -0500

    Day 12 part 1

--- AdventOfCode2019/Day12/Day12.cs
+++ AdventOfCode2019/Day12/Day12.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
 
@@ -9,10 +6,30 @@ namespace AdventOfCode2019
 {
     public static class Day12
     {
-        public static int PartOne(string filename)
+        public static int PartOne(Moon[] moons, int steps)
         {
-            string[] lines = filename.ReadAllLines();
-            return 0;
+            for(int i = 0; i < steps; i++)
+            {
+                moons[0].AdjustVelocity(moons[1]);
+                moons[0].AdjustVelocity(moons[2]);
+                moons[0].AdjustVelocity(moons[3]);
+
+                moons[1].AdjustVelocity(moons[0]);
+                moons[1].AdjustVelocity(moons[2]);
+                moons[1].AdjustVelocity(moons[3]);
+
+                moons[2].AdjustVelocity(moons[0]);
+                moons[2].AdjustVelocity(moons[1]);
+                moons[2].AdjustVelocity(moons[3]);
+
+                moons[3].AdjustVelocity(moons[0]);
+                moons[3].AdjustVelocity(moons[1]);
+                moons[3].AdjustVelocity(moons[2]);
+
+                foreach (var moon in moons)
+                    moon.Move();
+            }
+            return moons.Sum(m => m.TotalEnergy);
         }
 
         public static int PartTwo(string filename)
@@ -20,5 +37,56 @@ namespace AdventOfCode2019
             string[] lines = filename.ReadAllLines();
             return 0;
         }
+    }
+
+    public class Moon
+    {
+        public Moon(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public int VX { get; set; }
+        public int VY { get; set; }
+        public int VZ { get; set; }
+
+        public void AdjustVelocity(Moon moon)
+        {
+            if (moon.X < X)
+                VX--;
+            else if (moon.X > X)
+                VX++;
+
+            if (moon.Y < Y)
+                VY--;
+            else if (moon.Y > Y)
+                VY++;
+
+            if (moon.Z < Z)
+                VZ--;
+            else if (moon.Z > Z)
+                VZ++;
+        }
+
+        public void Move()
+        {
+            X += VX;
+            Y += VY;
+            Z += VZ;
+        }
+
+        public int PotentialEnergy =>
+            Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+
+        public int KineticEnergy =>
+            Math.Abs(VX) + Math.Abs(VY) + Math.Abs(VZ);
+
+        public int TotalEnergy =>
+            PotentialEnergy * KineticEnergy;
     }
 }
--- AdventOfCode2019/Day12/Day12Tests.cs
+++ AdventOfCode2019/Day12/Day12Tests.cs
@@ -9,40 +9,45 @@ namespace AdventOfCode2019
     {
         const int DAY = 12;
 
-        [Test]
-        public void TestPartOne()
-        {
-            Assert.That(Day12.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
-        [Test]
-        public void TestPartTwo()
-        {
-            Assert.That(Day12.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
-        }
-
         [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
+        public void TestPartOne(Moon[] moons, int steps, int expected)
         {
-            Assert.That(Day12.PartOne(filename), Is.EqualTo(expected));
+            Assert.That(Day12.PartOne(moons, steps), Is.EqualTo(expected));
         }
 
-        [TestCaseSource(nameof(TestDataTwo))]
-        public void TestPartTwo(string filename, int expected)
-        {
-            Assert.That(Day12.PartTwo(filename), Is.EqualTo(expected));
-        }
+        //[TestCaseSource(nameof(TestDataTwo))]
+        //public void TestPartTwo()
+        //{
+        //    Assert.That(Day12.PartTwo(filename), Is.EqualTo(expected));
+        //}
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(new Moon[] {
+                new Moon(-1, 0, 2),
+                new Moon(2, -10, -7),
+                new Moon(4, -8, 8),
+                new Moon(3, 5, -1)
+            }, 10, 179);
+
+            yield return new TestCaseData(new Moon[] {
+                new Moon(-8, -10, 0),
+                new Moon(5, 5, 10),
+                new Moon(2, -7, 3),
+                new Moon(9, -8, -3)
+            }, 100, 1940);
+
+            yield return new TestCaseData(new Moon[] {
+                new Moon(-16, 15, -9),
+                new Moon(-14, 5, 4),
+                new Moon(2, 0, 6),
+                new Moon(-3, 18, 9)
+            }, 1000, 10664);
         }
 
         public static IEnumerable TestDataTwo()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData();
         }
     }
 }
