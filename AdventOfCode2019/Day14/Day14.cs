1949f6e510336b1355726c9eb4aa0df7ad8b3995
Rob Prouse <rob@prouse.org>
2019-12-13 21:44:53 -0500

    Prep for day 14

---
+++ AdventOfCode2019/Day14/Day14.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Core;
+
+namespace AdventOfCode2019
+{
+    public static class Day14
+    {
+        public static int PartOne(string filename)
+        {
+            string[] lines = filename.ReadAllLines();
+            return 0;
+        }
+
+        public static int PartTwo(string filename)
+        {
+            string[] lines = filename.ReadAllLines();
+            return 0;
+        }
+    }
+}
