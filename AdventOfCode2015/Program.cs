0adf7cae9bc51ca41809daf8ec8b0601236fe726
Rob Prouse <rob@prouse.org>
2019-12-15 19:02:39 -0500

    Let's do AOC 2015!

---
+++ AdventOfCode2015/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using AdventOfCode.Core;
+using NUnit.Framework;
+
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
+namespace AdventOfCode2015
+{
+    public static class ProgramEntry
+    {
+        public static async Task Main()
+        {
+            Console.ResetColor();
+            Console.ReadLine();
+
+            await Task.FromResult(false);
+        }
+    }
+}
