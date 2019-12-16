e428a54184e95eccb57fd26eab7cbda204b3bb31
Rob Prouse <rob@prouse.org>
2019-12-15 19:17:08 -0500

    Solution to 2015 day 1

--- AdventOfCode2015/Day01/Day01.cs
+++ AdventOfCode2015/Day01/Day01.cs
@@ -16,10 +16,22 @@ namespace AdventOfCode2015
         public static int PartTwo(string filename)
         {
             string directions = filename.ReadAll();
-            return 0;
+            return FirstBasementDirection(directions);
         }
 
         public static int CalculateFloor(string directions) =>
             directions.Count(u => u == '(') - directions.Count(d => d == ')');
+
+        public static int FirstBasementDirection(string directions)
+        {
+            int floor = 0;
+            for(int i = 0; i < directions.Length; i++)
+            {
+                floor += directions[i] == '(' ? 1 : -1;
+                if (floor < 0)
+                    return i + 1;
+            }
+            return 0;
+        }
     }
 }
--- AdventOfCode2015/Day01/Day01Tests.cs
+++ AdventOfCode2015/Day01/Day01Tests.cs
@@ -12,13 +12,13 @@ namespace AdventOfCode2015
         [Test]
         public void TestPartOne()
         {
-            Assert.That(Day01.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day01.PartOne(PuzzleFile(DAY)), Is.EqualTo(138));
         }
 
         [Test]
         public void TestPartTwo()
         {
-            Assert.That(Day01.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day01.PartTwo(PuzzleFile(DAY)), Is.EqualTo(1771));
         }
 
         [TestCase("(())", 0)]
@@ -33,6 +33,13 @@ namespace AdventOfCode2015
         public void TestPartOne(string directions, int expected)
         {
             Assert.That(Day01.CalculateFloor(directions), Is.EqualTo(expected));
+        }
+
+        [TestCase(")", 1)]
+        [TestCase("()())", 5)]
+        public void TestPartTwo(string directions, int expected)
+        {
+            Assert.That(Day01.FirstBasementDirection(directions), Is.EqualTo(expected));
         }
     }
 }
