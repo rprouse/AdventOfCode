59e7cd7f5361b0d4866c4f49ddcde37eee23ba38
Rob Prouse <rob@prouse.org>
2019-12-17 20:24:53 -0500

    Optimization gets part 2 tests down to 30 sec to 2 min. Still not fast enough though.

--- AdventOfCode2019/Day16/Day16.cs
+++ AdventOfCode2019/Day16/Day16.cs
@@ -34,14 +34,13 @@ namespace AdventOfCode2019
                 {
                     int i = n;
                     long newValue = 0;
-                    string debug = string.Empty;
-                    while (i < ints.Length)
+                    int max = ints.Length;
+                    while (i < max)
                     {
-                        for (int j = 0; j < basePattern.Length; j++)
+                        for (int j = 0; j < basePattern.Length && i < max; j++)
                         {
-                            for (int k = 0; k <= n && i < ints.Length; k++)
+                            for (int k = 0; k <= n && i < max; k++)
                             {
-                                debug = $"N:{n}, I:{i}, RI: {j}, R:{basePattern[j]}, A:{saveInts[i] * basePattern[j]}";
                                 newValue += saveInts[i] * basePattern[j];
                                 i++;
                             }
@@ -57,7 +56,7 @@ namespace AdventOfCode2019
         {
             int[] ints = filename.ReadAll().Select(c => (int)(c - 48)).ToArray();
             int[] big = new int[ints.Length * multiplier];
-            for(long m = 0; m < multiplier; m++)
+            for (long m = 0; m < multiplier; m++)
             {
                 ints.CopyTo(big, m * ints.Length);
             }
@@ -68,14 +67,50 @@ namespace AdventOfCode2019
                 offset += ints[6 - i] * (int)Math.Pow(10, i);
             }
 
-            var result = CalculateEFT(big, 100);
+            var result = CalculateEFT2(big, 100, offset);
 
             int firstEight = 0;
             for (int i = 7; i >= 0; i--)
             {
-                firstEight += result[(offset + 7 - i) % result.Length] * (int)Math.Pow(10, i);
+                firstEight += result[(7 - i) % result.Length] * (int)Math.Pow(10, i);
             }
             return firstEight;
+        }
+
+        private static int[] CalculateEFT2(int[] input, int phases, int offset)
+        {
+            int[] a1 = new int[input.Length - offset];
+            int[] a2 = new int[a1.Length];
+            int[] swap;
+            Array.Copy(input, offset, a1, 0, input.Length - offset);
+            a1.CopyTo(a2, 0);
+            int[] basePattern = new int[] { 1, 0, -1, 0 };
+
+            for (int phase = 0; phase < phases; phase++)
+            {
+                swap = a1;
+                a1 = a2;
+                a2 = swap;
+                int max = a1.Length + offset;
+                for (int n = offset; n < max; n++)
+                {
+                    int i = n;
+                    long newValue = 0;
+                    while (i < max)
+                    {
+                        for (int j = 0; j < basePattern.Length && i < max; j++)
+                        {
+                            for (int k = 0; k <= n && i < max; k++)
+                            {
+                                newValue += a2[i - offset] * basePattern[j];
+                                i++;
+                            }
+                        }
+                    }
+                    a1[n - offset] = LastDigit((int)newValue);
+                }
+            }
+            return a1;
         }
 
         public static int LastDigit(int num) =>
