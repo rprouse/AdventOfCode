06e6e49dc47e6430a9a44369c22ce1520451a4c8
Rob Prouse <rob@prouse.org>
2019-12-22 22:32:50 -0500

    Checkpointing day 20

--- AdventOfCode2019/Day20/Day20.cs
+++ AdventOfCode2019/Day20/Day20.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using AdventOfCode.Core;
 using System.Drawing;
 using System.Diagnostics.CodeAnalysis;
+using ConcurrentPriorityQueue;
 
 namespace AdventOfCode2019
 {
@@ -36,29 +37,30 @@ namespace AdventOfCode2019
             public bool Visited { get; set; }
         }
 
-        public class Vertex : IEquatable<Vertex>
-        {
-            public string Start { get; set; }
-            public string End { get; set; }
-            public int Cost { get; set; }
+        //public class Vertex : IEquatable<Vertex>
+        //{
+        //    public string Start { get; set; }
+        //    public string End { get; set; }
+        //    public int Cost { get; set; }
 
-            public override string ToString() =>
-                $"{Start} => {End} = {Cost}";
+        //    public override string ToString() =>
+        //        $"{Start} => {End} = {Cost}";
 
-            public override bool Equals(object obj) =>
-                Equals(obj as Vertex);
+        //    public override bool Equals(object obj) =>
+        //        Equals(obj as Vertex);
 
-            public bool Equals([AllowNull] Vertex other) =>
-                other != null &&
-                Start == other.Start &&
-                End == other.End;
+        //    public bool Equals([AllowNull] Vertex other) =>
+        //        other != null &&
+        //        Start == other.Start &&
+        //        End == other.End;
 
-            public override int GetHashCode() => HashCode.Combine(Start, End);
-        }
+        //    public override int GetHashCode() => HashCode.Combine(Start, End);
+        //}
 
         char[,] _maze;
         Dictionary<string, Portal> _nodes = new Dictionary<string, Portal>();
-        List<Vertex> _graph = new List<Vertex>();
+        //List<Vertex> _graph = new List<Vertex>();
+        Graph _graph = new Graph();
         int[] _xEdges = new int[4];
         int[] _yEdges = new int[4];
 
@@ -72,7 +74,7 @@ namespace AdventOfCode2019
 
         public int ShortestDistance()
         {
-            return 0;
+            return _graph.FindShortestPath("AA");
         }
 
         private void LoadMaze(string[] maze)
@@ -133,16 +135,16 @@ namespace AdventOfCode2019
         private void FindNodes()
         {
             // Horizontal edges
-            for(int x = _xEdges[0] + 1; x < _xEdges[3]; x++)
+            for (int x = _xEdges[0] + 1; x < _xEdges[3]; x++)
             {
                 // Top
-                if(_maze[x, _yEdges[0]] == '.')
+                if (_maze[x, _yEdges[0]] == '.')
                 {
-                    string name = $"{_maze[x,_yEdges[0]-2]}{_maze[x, _yEdges[0]-1]}";
+                    string name = $"{_maze[x, _yEdges[0] - 2]}{_maze[x, _yEdges[0] - 1]}";
                     AddNode(name, x, _yEdges[0]);
                 }
 
-                if(x > _xEdges[1] && x < _xEdges[2])
+                if (x > _xEdges[1] && x < _xEdges[2])
                 {
                     // Top middle
                     if (_maze[x, _yEdges[1]] == '.')
@@ -215,6 +217,7 @@ namespace AdventOfCode2019
         private void BuildGraph()
         {
             FindVertexesFrom(_nodes["AA"], _nodes["AA"].Port1);
+            _graph.AddEdge("ZZ");
         }
 
         private void FindVertexesFrom(Portal portal, Point point)
@@ -266,12 +269,13 @@ namespace AdventOfCode2019
                 }
 
                 var p = _nodes.Values.Where(p => p.Port1 == next || p.Port2 == next).FirstOrDefault();
-                if(p != null)
+                if (p != null)
                 {
-                    // Add to the graph, recurse then start going deep, but not if ZZ
-                    _graph.Add(new Vertex { Start = portal.Name, End = p.Name, Cost = walk.Count });
-                    if(!p.Visited)
-                    {                        
+                    // Add to the graph, recurse then start going deep, but not if ZZ                    _
+                    //_graph.Add(new Vertex { Start = portal.Name, End = p.Name, Cost = walk.Count });
+                    _graph.AddEdge(portal.Name, p.Name, walk.Count);
+                    if (!p.Visited)
+                    {
                         FindVertexesFrom(p, p.Port1 == next ? p.Port2 : p.Port1);
                     }
                     next = walk.Pop();
@@ -300,6 +304,86 @@ namespace AdventOfCode2019
                 sb.AppendLine($"{pair.Key}: {pair.Value.Port1.X},{pair.Value.Port1.Y} => {pair.Value.Port2.X},{pair.Value.Port2.Y}");
 
             return sb.ToString();
+        }
+    }
+    public struct Distance
+    {
+        public int Value;
+        public string Node;
+    }
+    public class Graph
+    {
+        private readonly Dictionary<string, List<Edge>> _adj;
+
+        public Graph()
+        {
+            _adj = new Dictionary<string, List<Edge>>();
+        }
+
+        public void AddEdge(string source)
+        {
+            _adj[source] = new List<Edge>();
+        }
+
+        public void AddEdge(string source, string dest, int weight)
+        {
+            List<Edge> verticies;
+            if (!_adj.TryGetValue(source, out verticies))
+            {
+                verticies = new List<Edge>();
+            }
+            verticies.Add(new Edge(dest, weight));
+            _adj[source] = verticies;
+        }
+
+        public int FindShortestPath(string start)
+        {
+            var q = new ConcurrentPriorityQueue<string, int>();
+            q.Enqueue(start, 0);
+            var dist = new Dictionary<string, int>();
+            var path = new Dictionary<string, string>();
+            var s = new Stack<string>();
+            foreach (var n in _adj.Keys)
+            {
+                dist[n] = int.MaxValue;
+            }
+            dist[start] = 0;
+            while (q.Count() > 0)
+            {
+                var cur = q.Dequeue();
+                foreach (var edge in _adj[cur])
+                {
+                    var newDist = dist[cur] + edge.Weight;
+                    if (newDist < dist[edge.Node])
+                    {
+                        dist[edge.Node] = newDist;
+                        //it's invalid we should check if it's in Queue, if it is, decrease key
+                        q.Enqueue(edge.Node, newDist);
+                        path[cur] = edge.Node;
+                    }
+                }
+            }
+            return dist["ZZ"] - 1;
+            //Console.WriteLine("dist=[{0}]", string.Join(",", dist));
+            //Console.WriteLine("path=[{0}]", string.Join(",", path));
+            //s.Push(start);
+            //for (var cur = path[start]; cur != null; cur = path.ContainsKey(cur) ? path[cur] : null)
+            //{
+            //    s.Push(cur);
+            //}
+            //return s.ToArray();
+        }
+
+        private class Edge
+        {
+            public readonly string Node;
+            public readonly int Weight;
+
+            public Edge(string node, int weight)
+            {
+                Node = node;
+                Weight = weight;
+            }
         }
     }
 }
