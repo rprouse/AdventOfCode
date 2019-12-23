fd629c39017ac8df1b64f15049f7931f3f413b55
Rob Prouse <rob@prouse.org>
2019-12-22 20:54:25 -0500

    Day 22 part 1

--- AdventOfCode2019/Day22/Day22.cs
+++ AdventOfCode2019/Day22/Day22.cs
@@ -11,14 +11,99 @@ namespace AdventOfCode2019
     {
         public static int PartOne(string filename)
         {
-            string[] lines = filename.ReadAllLines();
-            return 0;
+            var cards = new SpaceCards(10007);
+            cards.Shuffle(filename);
+            for(int i = 0; i < 10007; i++)
+                if(cards.Cards[i] == 2019)
+                    return i;
+            return -1;
         }
 
         public static int PartTwo(string filename)
         {
             string[] lines = filename.ReadAllLines();
             return 0;
+        }
+    }
+
+    public class SpaceCards
+    {
+        int[] _stack;
+
+        public int[] Cards { get; private set; }
+
+        public SpaceCards(int deckSize)
+        {
+            Cards = new int[deckSize];
+            _stack = new int[deckSize];
+            for(int i = 0; i < deckSize; i++)
+            {
+                Cards[i] = i;
+            }
+        }
+
+        public void Shuffle(string filename)
+        {
+            string[] lines = filename.ReadAllLines();
+            foreach(var line in lines)
+            {
+                if(line == "deal into new stack")
+                {
+                    DealIntoNewStack();
+                }
+                else if(line.StartsWith("cut"))
+                {
+                    int n = line.Substring(4).ToInt();
+                    CutCards(n);
+                }
+                else if(line.StartsWith("deal with increment"))
+                {
+                    int n = line.Substring(20).ToInt();
+                    DealWithIncrement(n);
+                }
+                else
+                {
+                    throw new ArgumentException($"Unknown command {line}");
+                }
+            }
+        }
+
+        public void DealIntoNewStack()
+        {
+            for (int i = 0; i < Cards.Length; i++)
+                _stack[i] = Cards[Cards.Length - i - 1];
+
+            SwapStacks();
+        }
+
+        public void CutCards(int n)
+        {
+            if(n >= 0)
+            {
+                Array.Copy(Cards, 0, _stack, Cards.Length - n, n);
+                Array.Copy(Cards, n, _stack, 0, Cards.Length - n);
+            }
+            else
+            {
+                n = Math.Abs(n);
+                Array.Copy(Cards, Cards.Length - n, _stack, 0, n);
+                Array.Copy(Cards, 0, _stack, n, Cards.Length - n);
+            }
+            SwapStacks();
+        }
+
+        public void DealWithIncrement(int n)
+        {
+            for (int i = 0; i < Cards.Length; i++)
+                _stack[i * n % Cards.Length] = Cards[i];
+            SwapStacks();
+        }
+
+        void SwapStacks()
+        {
+            var temp = Cards;
+            Cards = _stack;
+            _stack = temp;
         }
     }
 }
--- AdventOfCode2019/Day22/Day22Tests.cs
+++ AdventOfCode2019/Day22/Day22Tests.cs
@@ -12,7 +12,44 @@ namespace AdventOfCode2019
         [Test]
         public void TestPartOne()
         {
-            Assert.That(Day22.PartOne(PuzzleFile(DAY)), Is.EqualTo(0));
+            Assert.That(Day22.PartOne(PuzzleFile(DAY)), Is.EqualTo(6289));
+        }
+
+        [Test]
+        public void CanDealIntoNewStack()
+        {
+            //var cards = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var expected = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            var cards = new SpaceCards(10);
+            cards.DealIntoNewStack();
+            Assert.That(cards.Cards, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CanCutCards()
+        {
+            var expected = new int[] { 3, 4, 5, 6, 7, 8, 9, 0, 1, 2 };
+            var cards = new SpaceCards(10);
+            cards.CutCards(3);
+            Assert.That(cards.Cards, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CanCutCardsWithNegative()
+        {
+            var expected = new int[] { 6, 7, 8, 9, 0, 1, 2, 3, 4, 5 };
+            var cards = new SpaceCards(10);
+            cards.CutCards(-4);
+            Assert.That(cards.Cards, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CanDealWithIncrement()
+        {
+            var expected = new int[] { 0, 7, 4, 1, 8, 5, 2, 9, 6, 3 };
+            var cards = new SpaceCards(10);
+            cards.DealWithIncrement(3);
+            Assert.That(cards.Cards, Is.EqualTo(expected));
         }
 
         [Test]
@@ -22,9 +59,11 @@ namespace AdventOfCode2019
         }
 
         [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
+        public void CanShuffleCards(string filename, int[] expected)
         {
-            Assert.That(Day22.PartOne(filename), Is.EqualTo(expected));
+            var cards = new SpaceCards(10);
+            cards.Shuffle(filename);
+            Assert.That(cards.Cards, Is.EqualTo(expected));
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
@@ -35,13 +74,15 @@ namespace AdventOfCode2019
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY, "Test1.txt"), new int[] { 0, 3, 6, 9, 2, 5, 8, 1, 4, 7 });
+            yield return new TestCaseData(TestFile(DAY, "Test2.txt"), new int[] { 3, 0, 7, 4, 1, 8, 5, 2, 9, 6 });
+            yield return new TestCaseData(TestFile(DAY, "Test3.txt"), new int[] { 6, 3, 0, 7, 4, 1, 8, 5, 2, 9 });
+            yield return new TestCaseData(TestFile(DAY, "Test4.txt"), new int[] { 9, 2, 5, 8, 1, 4, 7, 0, 3, 6 });
         }
 
         public static IEnumerable TestDataTwo()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
+            //yield return new TestCaseData(TestFile(DAY), 0);
             yield return new TestCaseData(PuzzleFile(DAY), 0);
         }
     }
