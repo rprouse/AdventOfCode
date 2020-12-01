5f7687a3d22b491e92d6c9cc96389b3f240a6abd
Rob Prouse <rob@prouse.org>
2020-11-30 22:40:24 -0500

    Switch from Shouldly to FluentAssertions

--- AdventOfCode2018/Day00/Day00Tests.cs
+++ AdventOfCode2018/Day00/Day00Tests.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.IO;
 using AdventOfCode.Core;
 using NUnit.Framework;
-using Shouldly;
+using FluentAssertions;
 
 namespace AdventOfCode2018
 {
@@ -14,25 +14,25 @@ namespace AdventOfCode2018
         [Test]
         public void TestPartOne()
         {
-            Day00.PartOne(PuzzleFile(DAY)).ShouldBe(0);
+            Day00.PartOne(PuzzleFile(DAY)).Should().Be(0);
         }
 
         [Test]
         public void TestPartTwo()
         {
-            Day00.PartTwo(PuzzleFile(DAY)).ShouldBe(0);
+            Day00.PartTwo(PuzzleFile(DAY)).Should().Be(0);
         }
 
         [TestCaseSource(nameof(TestDataOne))]
         public void TestPartOne(string filename, int expected)
         {
-            Day00.PartOne(filename).ShouldBe(expected);
+            Day00.PartOne(filename).Should().Be(expected);
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
-            Day00.PartTwo(filename).ShouldBe(expected);
+            Day00.PartTwo(filename).Should().Be(expected);
         }
 
         public static IEnumerable TestDataOne()
