f07c066553e3d64da5001f8f17ba8c031793b55e
Rob Prouse <rob@prouse.org>
2020-11-30 22:04:30 -0500

    Solution for 2015 day 2 part 1

--- AdventOfCode2015/Day02/Day02.cs
+++ AdventOfCode2015/Day02/Day02.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,13 +11,27 @@ namespace AdventOfCode2015
         public static int PartOne(string filename)
         {
             string[] lines = filename.ReadAllLines();
-            return 0;
+            return lines.Sum(l => CalculatePaperNeeded(l));
         }
 
         public static int PartTwo(string filename)
         {
             string[] lines = filename.ReadAllLines();
             return 0;
+        }
+
+        public static int CalculatePaperNeeded(string str)
+        {
+            var parts = str.Split('x');
+            return CalculatePaperNeeded(parts[0].ToInt(), parts[1].ToInt(), parts[2].ToInt());
+        }
+
+        public static int CalculatePaperNeeded(int l, int w, int h)
+        {
+            int a = l * w;
+            int b = w * h;
+            int c = h * l;
+            return 2 * a + 2 * b + 2 * c + Math.Min(a, Math.Min(b, c));
         }
     }
 }
--- AdventOfCode2015/Day02/Day02Tests.cs
+++ AdventOfCode2015/Day02/Day02Tests.cs
@@ -9,6 +9,22 @@ namespace AdventOfCode2015
     {
         const int DAY = 02;
 
+        [TestCase(2, 3, 4, 58)]
+        [TestCase(1, 1, 10, 43)]
+        public void CanCalculatePaperDimensions(int l, int w, int h, int expected)
+        {
+            int d = Day02.CalculatePaperNeeded(l, w, h);
+            Assert.That(d, Is.EqualTo(expected));
+        }
+
+        [TestCase("2x3x4", 58)]
+        [TestCase("1x1x10", 43)]
+        public void CanCalculatePaperDimensions(string str, int expected)
+        {
+            int d = Day02.CalculatePaperNeeded(str);
+            Assert.That(d, Is.EqualTo(expected));
+        }
+
         [Test]
         public void TestPartOne()
         {
@@ -21,22 +37,10 @@ namespace AdventOfCode2015
             Assert.That(Day02.PartTwo(PuzzleFile(DAY)), Is.EqualTo(0));
         }
 
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
-        {
-            Assert.That(Day02.PartOne(filename), Is.EqualTo(expected));
-        }
-
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
             Assert.That(Day02.PartTwo(filename), Is.EqualTo(expected));
-        }
-
-        public static IEnumerable TestDataOne()
-        {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
         }
 
         public static IEnumerable TestDataTwo()
