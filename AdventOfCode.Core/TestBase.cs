26d4aca17e77367366bdea2e2dfcbebf5e9dc463
Rob Prouse <rob@prouse.org>
2020-11-30 20:59:40 -0500

    Run tests on Linux

--- AdventOfCode.Core/TestBase.cs
+++ AdventOfCode.Core/TestBase.cs
@@ -1,11 +1,13 @@
-﻿namespace AdventOfCode.Core
+﻿using System.IO;
+
+namespace AdventOfCode.Core
 {
     public class TestBase
     {
         public static string TestFile(int day, string filename = "Test.txt") =>
-            $"Day{day:00}\\{filename}";
+            Path.Combine($"Day{day:00}", filename);
 
         public static string PuzzleFile(int day, string filename = "Data.txt") =>
-            $"Day{day:00}\\{filename}";
+            Path.Combine($"Day{day:00}", filename);
     }
 }
