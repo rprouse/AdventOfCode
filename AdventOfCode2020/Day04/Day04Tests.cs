da22138aa6011772018e5712fd2dfad41a2e726a
Rob Prouse <rob@prouse.org>
2020-12-03 09:26:16 -0500

    Added day 4 and 5

---
+++ AdventOfCode2020/Day04/Day04Tests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.IO;
+using AdventOfCode.Core;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AdventOfCode2020
+{
+    [Parallelizable(ParallelScope.All)]
+    public class Day04Tests : TestBase
+    {
+        const int DAY = 04;
+
+        [Test]
+        public void TestPartOne()
+        {
+            Day04.PartOne(PuzzleFile(DAY)).Should().Be(0);
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Day04.PartTwo(PuzzleFile(DAY)).Should().Be(0);
+        }
+
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, int expected)
+        {
+            Day04.PartOne(filename).Should().Be(expected);
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            Day04.PartTwo(filename).Should().Be(expected);
+        }
+
+        public static IEnumerable TestDataOne()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
