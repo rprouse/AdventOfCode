37ae252b241f47b10cb35f2398f2db0223beaf0a
Rob Prouse <rob@prouse.org>
2020-12-05 09:03:17 -0500

    Day 5 part 1

--- AdventOfCode2020/Day05/Day05.cs
+++ AdventOfCode2020/Day05/Day05.cs
@@ -9,16 +9,48 @@ namespace AdventOfCode2020
 {
     public static class Day05
     {
+        const int ROWS = 127;
+        const int COLS = 7;
+
         public static int PartOne(string filename)
         {
             string[] lines = filename.ReadAllLines();
-            return 0;
+            return lines.Select(l => GetSeatId(l)).Max();
         }
 
         public static int PartTwo(string filename)
         {
             string[] lines = filename.ReadAllLines();
             return 0;
+        }
+
+        static internal int GetSeatId(string pass)
+        {
+            var seat = GetSeat(pass);
+            return seat.row * 8 + seat.col;
+        }
+
+        static internal (int row, int col) GetSeat(string pass)
+        {
+            int row = ROWS;
+            int low = 0;
+            for(int i = 0; i < 7; i++)
+            {
+                if (pass[i] == 'F')
+                    row -= (row - low) / 2 + 1;
+                else
+                    low += (row - low) / 2 + 1;
+            }
+            int col = COLS;
+            low = 0;
+            for (int i = 7; i < 10; i++)
+            {
+                if (pass[i] == 'L')
+                    col -= (col - low) / 2 + 1;
+                else
+                    low += (col - low) / 2 + 1;
+            }
+            return (row, col);
         }
     }
 }
--- AdventOfCode2020/Day05/Day05Tests.cs
+++ AdventOfCode2020/Day05/Day05Tests.cs
@@ -12,10 +12,31 @@ namespace AdventOfCode2020
     {
         const int DAY = 05;
 
+        [TestCase("FBFBBFFRLR", 44, 5)]
+        [TestCase("BFFFBBFRRR", 70, 7)]
+        [TestCase("FFFBBBFRRR", 14, 7)]
+        [TestCase("BBFFBBFRLL", 102, 4)]
+        public void CanParseBoardingPass(string pass, int row, int col)
+        {
+            var seat = Day05.GetSeat(pass);
+            seat.row.Should().Be(row);
+            seat.col.Should().Be(col);
+        }
+
+        [TestCase("FBFBBFFRLR", 357)]
+        [TestCase("BFFFBBFRRR", 567)]
+        [TestCase("FFFBBBFRRR", 119)]
+        [TestCase("BBFFBBFRLL", 820)]
+        public void CanGetSeatId(string pass, int expected)
+        {
+            var id = Day05.GetSeatId(pass);
+            id.Should().Be(expected);
+        }
+
         [Test]
         public void TestPartOne()
         {
-            Day05.PartOne(PuzzleFile(DAY)).Should().Be(0);
+            Day05.PartOne(PuzzleFile(DAY)).Should().Be(828);
         }
 
         [Test]
@@ -24,22 +45,10 @@ namespace AdventOfCode2020
             Day05.PartTwo(PuzzleFile(DAY)).Should().Be(0);
         }
 
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
-        {
-            Day05.PartOne(filename).Should().Be(expected);
-        }
-
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
             Day05.PartTwo(filename).Should().Be(expected);
-        }
-
-        public static IEnumerable TestDataOne()
-        {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
         }
 
         public static IEnumerable TestDataTwo()
