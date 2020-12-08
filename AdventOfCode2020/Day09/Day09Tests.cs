1d9ea5bfd1bf1d06f6ffdd1d334a5e14a82efd10
Rob Prouse <rob@prouse.org>
2020-12-08 09:37:15 -0500

    Getting ready for day 9

---
+++ AdventOfCode2020/Day09/Day09Tests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.IO;
+using AdventOfCode.Core;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AdventOfCode2020
+{
+    [Parallelizable(ParallelScope.All)]
+    public class Day09Tests : TestBase
+    {
+        const int DAY = 09;
+
+        [Test]
+        public void TestPartOne()
+        {
+            Day09.PartOne(PuzzleFile(DAY)).Should().Be(0);
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Day09.PartTwo(PuzzleFile(DAY)).Should().Be(0);
+        }
+
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, int expected)
+        {
+            Day09.PartOne(filename).Should().Be(expected);
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            Day09.PartTwo(filename).Should().Be(expected);
+        }
+
+        public static IEnumerable TestDataOne()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
