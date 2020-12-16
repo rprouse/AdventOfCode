ad80e94f27175b01407a9c90ce436765414772dd
Rob Prouse <rob@prouse.org>
2020-12-16 08:38:36 -0500

    Day 16 part 1

--- AdventOfCode2020/Day16/Day16.cs
+++ AdventOfCode2020/Day16/Day16.cs
@@ -12,7 +12,26 @@ namespace AdventOfCode2020
         public static int PartOne(string filename)
         {
             string[] lines = filename.ReadAllLines();
-            return 0;
+            int i = 0;
+            List<Rule> rules = new List<Rule>();
+            while (lines[i] != "your ticket:")
+                rules.Add(new Rule(lines[i++]));
+
+            i++;
+            int[] mine = lines[i++].Split(',').Select(s => s.ToInt()).ToArray();
+
+            int sum = 0;
+            i++;
+            while(i < lines.Length)
+            {
+                int[] ticket = lines[i++].Split(',').Select(s => s.ToInt()).ToArray();
+                foreach(int val in ticket)
+                {
+                    if (!rules.Any(r => r.Valid(val)))
+                        sum += val;
+                }
+            }
+            return sum;
         }
 
         public static int PartTwo(string filename)
@@ -20,5 +39,32 @@ namespace AdventOfCode2020
             string[] lines = filename.ReadAllLines();
             return 0;
         }
+    }
+
+    public class Rule
+    {
+        public string Name { get; set; }
+        public int LowOne { get; set; }
+        public int HighOne { get; set; }
+        public int LowTwo { get; set; }
+        public int HighTwo { get; set; }
+
+        public Rule(string line)
+        {
+            string[] parts = line.Split(": ");
+            Name = parts[0];
+
+            string[] ranges = parts[1].Split(" or ");
+            string[] range = ranges[0].Split("-");
+            LowOne = range[0].ToInt();
+            HighOne = range[1].ToInt();
+
+            range = ranges[1].Split("-");
+            LowTwo = range[0].ToInt();
+            HighTwo = range[1].ToInt();
+        }
+
+        public bool Valid(int n) =>
+            (n >= LowOne && n <= HighOne) || (n >= LowTwo && n <= HighTwo); 
     }
 }
--- AdventOfCode2020/Day16/Day16Tests.cs
+++ AdventOfCode2020/Day16/Day16Tests.cs
@@ -13,12 +13,6 @@ namespace AdventOfCode2020
         const int DAY = 16;
 
         [Test]
-        public void TestPartOne()
-        {
-            Day16.PartOne(PuzzleFile(DAY)).Should().Be(0);
-        }
-
-        [Test]
         public void TestPartTwo()
         {
             Day16.PartTwo(PuzzleFile(DAY)).Should().Be(0);
@@ -38,8 +32,8 @@ namespace AdventOfCode2020
 
         public static IEnumerable TestDataOne()
         {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
+            yield return new TestCaseData(TestFile(DAY), 71);
+            yield return new TestCaseData(PuzzleFile(DAY), 26869);
         }
 
         public static IEnumerable TestDataTwo()
