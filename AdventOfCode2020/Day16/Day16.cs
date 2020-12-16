3449291ef2d7977c6b5d6872bcaf84725e55511d
Rob Prouse <rob@prouse.org>
2020-12-16 11:40:29 -0500

    Minor day 16 refactor

--- AdventOfCode2020/Day16/Day16.cs
+++ AdventOfCode2020/Day16/Day16.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Text;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
 
@@ -100,42 +97,5 @@ namespace AdventOfCode2020
 
         internal static int[] GetTicket(string line) =>
             line.Split(',').Select(s => s.ToInt()).ToArray();
-    }
-
-    public class Rule
-    {
-        public string Name { get; set; }
-        public int LowOne { get; set; }
-        public int HighOne { get; set; }
-        public int LowTwo { get; set; }
-        public int HighTwo { get; set; }
-
-        public Rule(string line)
-        {
-            string[] parts = line.Split(": ");
-            Name = parts[0];
-
-            string[] ranges = parts[1].Split(" or ");
-            string[] range = ranges[0].Split("-");
-            LowOne = range[0].ToInt();
-            HighOne = range[1].ToInt();
-
-            range = ranges[1].Split("-");
-            LowTwo = range[0].ToInt();
-            HighTwo = range[1].ToInt();
-        }
-
-        public bool Valid(int n) =>
-            (n >= LowOne && n <= HighOne) || (n >= LowTwo && n <= HighTwo);
-
-        public override bool Equals(object obj)
-        {
-            var other = obj as Rule;
-            return other != null ? other.Name == Name : false;
-        }
-
-        public override int GetHashCode() => Name.GetHashCode();
-
-        public override string ToString() => Name;
     }
 }
---
+++ AdventOfCode2020/Day16/Rule.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode2020
+{
+    public class Rule
+    {
+        public string Name { get; set; }
+        public int LowOne { get; set; }
+        public int HighOne { get; set; }
+        public int LowTwo { get; set; }
+        public int HighTwo { get; set; }
+
+        public Rule(string line)
+        {
+            string[] parts = line.Split(": ");
+            Name = parts[0];
+
+            string[] ranges = parts[1].Split(" or ");
+            string[] range = ranges[0].Split("-");
+            LowOne = range[0].ToInt();
+            HighOne = range[1].ToInt();
+
+            range = ranges[1].Split("-");
+            LowTwo = range[0].ToInt();
+            HighTwo = range[1].ToInt();
+        }
+
+        public bool Valid(int n) =>
+            (n >= LowOne && n <= HighOne) || (n >= LowTwo && n <= HighTwo);
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Rule;
+            return other != null ? other.Name == Name : false;
+        }
+
+        public override int GetHashCode() => Name.GetHashCode();
+
+        public override string ToString() => Name;
+    }
+}
