f40141251334bdbf323454b0f62b48f7185efe3e
Rob Prouse <rob@prouse.org>
2020-12-18 09:28:32 -0500

    Solution to day 18 part 1

--- AdventOfCode2020/Day18/Day18.cs
+++ AdventOfCode2020/Day18/Day18.cs
@@ -9,16 +9,64 @@ namespace AdventOfCode2020
 {
     public static class Day18
     {
-        public static int PartOne(string filename)
-        {
-            string[] lines = filename.ReadAllLines();
-            return 0;
-        }
+        public static long PartOne(string filename) =>
+            filename.ReadAllLines()
+                    .Select(l => Evaluate(l))
+                    .Sum();
 
         public static int PartTwo(string filename)
         {
             string[] lines = filename.ReadAllLines();
             return 0;
         }
+
+        public static long Evaluate(string equation)
+        {
+            while(ContainsParens(equation))
+            {
+                (int start, int end) = FindInnermostParens(equation);
+                string inner = GetInnerEquation(equation, start, end);
+                long result = EvaluateWithoutParens(inner);
+                equation = SubsituteInnerWithResult(equation, result, start, end);
+            }
+            return EvaluateWithoutParens(equation);
+        }
+
+        public static (int start, int end) FindInnermostParens(string equation)
+        {
+            int start = 0;
+            int end = 1;
+            int nextStart = 0;
+
+            while (nextStart != -1 && nextStart < end)
+            {
+                start = equation.IndexOf('(', start + 1);
+                end = equation.IndexOf(')', start + 1);
+                nextStart = equation.IndexOf('(', start + 1);
+            }
+            return (start, end);
+        }
+
+        public static string GetInnerEquation(string equation, int start, int end) =>
+            equation[(start + 1)..(end)];
+
+        public static bool ContainsParens(string equation) =>
+            equation.Any(c => c == '(');
+
+        public static long EvaluateWithoutParens(string equation)
+        {
+            var parts = equation.Split(' ');
+            long val = parts[0].ToLong();
+            for(int i = 1; i < parts.Length; i += 2)
+            {
+                if (parts[i] == "+") val += parts[i + 1].ToLong();
+                else if (parts[i] == "*") val *= parts[i + 1].ToLong();
+                else throw new InvalidOperationException($"Unexpected operator {parts[i]}");
+            }
+            return val;
+        }
+
+        public static string SubsituteInnerWithResult(string equation, long result, int start, int end) =>
+            equation[0..start] + result.ToString() + equation[(end+1)..];
     }
 }
--- AdventOfCode2020/Day18/Day18Tests.cs
+++ AdventOfCode2020/Day18/Day18Tests.cs
@@ -15,7 +15,7 @@ namespace AdventOfCode2020
         [Test]
         public void TestPartOne()
         {
-            Day18.PartOne(PuzzleFile(DAY)).Should().Be(0);
+            Day18.PartOne(PuzzleFile(DAY)).Should().Be(21022630974613L);
         }
 
         [Test]
@@ -24,22 +24,64 @@ namespace AdventOfCode2020
             Day18.PartTwo(PuzzleFile(DAY)).Should().Be(0);
         }
 
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
+        [TestCase("1 + 2 * 3 + 4 * 5 + 6", 71)]
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+        [TestCase("2 * 3 + (4 * 5)", 26)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+        public void CanEvaluateLine(string equation, long expected)
         {
-            Day18.PartOne(filename).Should().Be(expected);
+            Day18.Evaluate(equation).Should().Be(expected);
+        }
+
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))", 4, 10)]
+        [TestCase("2 * 3 + (4 * 5)", 8, 14)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 4, 26)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 29, 39)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 1, 11)]
+        public void CanFindInnermostParens(string equation, int exStart, int exEnd)
+        {
+            (int start, int end) = Day18.FindInnermostParens(equation);
+            start.Should().Be(exStart);
+            end.Should().Be(exEnd);
+        }
+
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))", 4, 10, "2 * 3")]
+        [TestCase("2 * 3 + (4 * 5)", 8, 14, "4 * 5")]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 4, 26, "8 * 3 + 9 + 3 * 4 * 3")]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 29, 39, "8 + 6 * 4")]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 1, 11, "2 + 4 * 9")]
+        public void CanGetInnerEquation(string equation, int start, int end, string expected)
+        {
+            Day18.GetInnerEquation(equation, start, end).Should().Be(expected);
+        }
+
+        [TestCase("2 * 3", 6)]
+        [TestCase("4 * 5", 20)]
+        [TestCase("8 * 3 + 9 + 3 * 4 * 3", 432)]
+        [TestCase("8 + 6 * 4", 56)]
+        [TestCase("2 + 4 * 9", 54)]
+        [TestCase("1 + 2 * 3 + 4 * 5 + 6", 71)]
+        public void CanEvaluateWithoutParens(string equation, long expected)
+        {
+            Day18.EvaluateWithoutParens(equation).Should().Be(expected);
+        }
+
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))", 6, 4, 10, "1 + 6 + (4 * (5 + 6))")]
+        [TestCase("2 * 3 + (4 * 5)", 20, 8, 14, "2 * 3 + 20")]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 432, 4, 26, "5 + 432")]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 56, 29, 39, "5 * 9 * (7 * 3 * 3 + 9 * 3 + 56)")]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 54, 1, 11, "(54 * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")]
+        public void CanSubsituteInnerWithResult(string equation, long result, int start, int end, string expected)
+        {
+            Day18.SubsituteInnerWithResult(equation, result, start, end).Should().Be(expected);
         }
 
         [TestCaseSource(nameof(TestDataTwo))]
         public void TestPartTwo(string filename, int expected)
         {
             Day18.PartTwo(filename).Should().Be(expected);
-        }
-
-        public static IEnumerable TestDataOne()
-        {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
         }
 
         public static IEnumerable TestDataTwo()
