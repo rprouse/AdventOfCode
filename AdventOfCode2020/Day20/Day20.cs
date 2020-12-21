0dc17e0e7685a9c63b8fb90d093637ade4d45de1
Rob Prouse <rob@prouse.org>
2020-12-20 19:30:36 -0500

    Ready to finally start day 20

---
+++ AdventOfCode2020/Day20/Day20.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Core;
+
+namespace AdventOfCode2020
+{
+    public static class Day20
+    {
+        public static int PartOne(string filename)
+        {
+            string[] lines = filename.ReadAllLines();
+            return 0;
+        }
+
+        public static int PartTwo(string filename)
+        {
+            string[] lines = filename.ReadAllLines();
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2020/Day20/Day20Tests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.IO;
+using AdventOfCode.Core;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AdventOfCode2020
+{
+    [Parallelizable(ParallelScope.All)]
+    public class Day20Tests : TestBase
+    {
+        const int DAY = 20;
+
+        [Test]
+        public void TestPartOne()
+        {
+            Day20.PartOne(PuzzleFile(DAY)).Should().Be(0);
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Day20.PartTwo(PuzzleFile(DAY)).Should().Be(0);
+        }
+
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, int expected)
+        {
+            Day20.PartOne(filename).Should().Be(expected);
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            Day20.PartTwo(filename).Should().Be(expected);
+        }
+
+        public static IEnumerable TestDataOne()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
