6c4483aa7e484d99657f9b518665a1bb6c97a375
Rob Prouse <rob@prouse.org>
2020-12-21 19:23:15 -0500

    Rewrote Day 19 part 1 using RegEx

--- AdventOfCode2020/Day19/Day19.cs
+++ AdventOfCode2020/Day19/Day19.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020
 {
@@ -11,14 +12,45 @@ namespace AdventOfCode2020
     {
         public static int PartOne(string filename)
         {
-            (var rules, var messages) = Parse(filename);
+            (var rules, var messages) = ParseRegex(filename);
+            var regex = new Regex($"^{rules[0].GetRegex()}$", RegexOptions.Compiled | RegexOptions.Singleline);
+
             int m = 0;
             foreach(string message in messages)
             {
-                if (rules[0].Matches(message))
+                if(regex.Match(message).Success)
                     m++;
             }
             return m;
+        }
+
+        private static (Dictionary<int, Day19Regex> rules, List<string> messages) ParseRegex(string filename)
+        {
+            string[] lines = filename.ReadAllLines();
+
+            Dictionary<int, Day19Regex> rules = new Dictionary<int, Day19Regex>();
+            List<string> messages = new List<string>();
+            foreach (string line in lines)
+            {
+                if (line[0] == 'a' || line[0] == 'b')
+                    messages.Add(line);
+                else
+                {
+                    var rule = new Day19Regex(line);
+                    rules.Add(rule.Number, rule);
+                }
+            }
+            foreach (var rule in rules.Values)
+            {
+                rule.Attach(rules);
+            }
+            do
+            {
+                foreach (var rule in rules.Values)
+                    rule.GetRegex();
+
+            } while (rules.Values.Any(r => r.GetRegex() == null)) ;
+            return (rules, messages);
         }
 
         private static (Dictionary<int, Day19Rule> rules, List<string> messages) Parse(string filename)
@@ -55,6 +87,72 @@ namespace AdventOfCode2020
             }
             return m;
         }
+    }
+
+    public class Day19Regex
+    {
+        public int Number { get; }
+
+        string _regex;
+        readonly string _rule;
+        readonly string _ruleA;
+        readonly string _ruleB;
+
+        Day19Regex[] _rulesA;
+        Day19Regex[] _rulesB;
+
+        public Day19Regex(string rule)
+        {
+            _rule = rule;
+            var parts = rule.Split(": ");
+            Number = parts[0].ToInt();
+            if (parts[1][0] == '"')
+            {
+                _regex = $"(?:{parts[1][1]})";
+            }
+            else
+            {
+                var subRules = parts[1].Split(" | ");
+                _ruleA = subRules[0];
+                _ruleB = subRules.Length == 2 ? subRules[1] : null;
+            }
+        }
+
+        public void Attach(Dictionary<int, Day19Regex> rules)
+        {
+            if (_regex != null)
+                return;
+
+            _rulesA = _ruleA.Split(' ').Select(s => rules[s.ToInt()]).ToArray();
+
+            if (_ruleB == null) return;
+
+            _rulesB = _ruleB.Split(' ').Select(s => rules[s.ToInt()]).ToArray();
+        }
+
+        public string GetRegex()
+        {
+            if (_regex != null)
+                return _regex;
+
+            if (_rulesA.Any(r => r.GetRegex() == null))
+                return null;
+
+            if (_rulesB != null && _rulesB.Any(r => r.GetRegex() == null))
+                return null;
+
+            if(_rulesB != null)
+            {
+                _regex = $"(?:(?:{string.Join("", _rulesA.Select(r => r.GetRegex()))})|(?:{string.Join("", _rulesB.Select(r => r.GetRegex()))}))";
+            }
+            else
+            {
+                _regex = $"(?:{string.Join("", _rulesA.Select(r => r.GetRegex()))})";
+            }
+            return _regex;
+        }
+
+        public override string ToString() => $"{Number}: {_regex}";
     }
 
     public class Day19Rule
