97a6fe2e3a54359a24bd5f575ae0c21846d2b25f
Rob Prouse <rob@prouse.org>
2021-11-23 20:05:51 -0500

    Prep for 2015 day 13

---
+++ AdventOfCode2015/Day13/Day13.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Core;
+
+namespace AdventOfCode2015
+{
+    public static class Day13
+    {
+        public static int PartOne(string filename)
+        {
+            string[] lines = filename.ReadAllLines();
+            return 0;
+        }
+
+        public static int PartTwo(string filename)
+        {
+            string[] lines = filename.ReadAllLines();
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2015/Day13/Day13Tests.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.IO;
+using AdventOfCode.Core;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AdventOfCode2015
+{
+    public class Day13Tests : TestBase
+    {
+        const int DAY = 13;
+
+        [Test]
+        public void TestPartOne()
+        {
+            Day13.PartOne(PuzzleFile(DAY)).Should().Be(0);
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Day13.PartTwo(PuzzleFile(DAY)).Should().Be(0);
+        }
+
+        [TestCaseSource(nameof(TestDataOne))]
+        public void TestPartOne(string filename, int expected)
+        {
+            Day13.PartOne(filename).Should().Be(expected);
+        }
+
+        [TestCaseSource(nameof(TestDataTwo))]
+        public void TestPartTwo(string filename, int expected)
+        {
+            Day13.PartTwo(filename).Should().Be(expected);
+        }
+
+        public static IEnumerable TestDataOne()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+
+        public static IEnumerable TestDataTwo()
+        {
+            yield return new TestCaseData(TestFile(DAY), 0);
+            yield return new TestCaseData(PuzzleFile(DAY), 0);
+        }
+    }
+}
