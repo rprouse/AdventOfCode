f080e9ab1296a3a77f480ee388f4f774aaaa8657
Rob Prouse <rob@prouse.org>
2021-11-23 20:04:56 -0500

    Solves 2015 day 12 part 2

--- AdventOfCode2015/Day12/Day12.cs
+++ AdventOfCode2015/Day12/Day12.cs
@@ -39,10 +39,37 @@ namespace AdventOfCode2015
             return sum;
         }
 
-        public static int PartTwo(string filename)
+        public static int PartTwo(string filename) =>
+            SumNumbersMinusRed(filename.ReadAll());
+
+        internal static int SumNumbersMinusRed(string json) =>
+            SumNumbersMinusRed(JsonNode.Parse(json));
+
+        private static int SumNumbersMinusRed(JsonNode node)
         {
-            string[] lines = filename.ReadAllLines();
-            return 0;
+            int sum = 0;
+            JsonValue val = node as JsonValue;
+            if (val != null && val.TryGetValue<int>(out int n))
+            {
+                sum += n;
+            }
+            JsonObject obj = node as JsonObject;
+            if (obj != null)
+            {
+                foreach (var item in obj)
+                {
+                    JsonValue itemVal = item.Value as JsonValue;
+                    if (itemVal != null && itemVal.ToString() == "red")
+                        return 0;
+                    sum += SumNumbersMinusRed(item.Value);
+                }
+            }
+            JsonArray array = node as JsonArray;
+            if (array != null)
+            {
+                sum += array.Sum(n => SumNumbersMinusRed(n));
+            }
+            return sum;
         }
     }
 }
--- AdventOfCode2015/Day12/Day12Tests.cs
+++ AdventOfCode2015/Day12/Day12Tests.cs
@@ -30,10 +30,19 @@ namespace AdventOfCode2015
             Day12.PartOne(PuzzleFile(DAY)).Should().Be(111754);
         }
 
+        [TestCase("[1,2,3]", 6)]
+        [TestCase("[1,{\"c\":\"red\",\"b\":2},3]", 4)]
+        [TestCase("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}", 0)]
+        [TestCase("[1,\"red\",5]", 6)]
+        public void TestSumNumbersMinusRed(string json, int expected)
+        {
+            Day12.SumNumbersMinusRed(json).Should().Be(expected);
+        }
+
         [Test]
         public void TestPartTwo()
         {
-            Day12.PartTwo(PuzzleFile(DAY)).Should().Be(0);
+            Day12.PartTwo(PuzzleFile(DAY)).Should().Be(65402);
         }
     }
 }
