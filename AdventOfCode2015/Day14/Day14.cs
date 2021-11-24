f83b0bc81d5c5bce651608bf83df5dedf7b7e09f
Rob Prouse <rob@prouse.org>
2021-11-23 21:00:15 -0500

    Solves day 14 part 1 of 2015

--- AdventOfCode2015/Day14/Day14.cs
+++ AdventOfCode2015/Day14/Day14.cs
@@ -7,10 +7,33 @@ namespace AdventOfCode2015
 {
     public static class Day14
     {
-        public static int PartOne(string filename)
+        public static int PartOne(string filename) =>
+            filename.ReadAllLines()
+                .Max(x => RaceForSeconds(x,2503));
+
+        internal static int RaceForSeconds(string text, int sec)
         {
-            string[] lines = filename.ReadAllLines();
-            return 0;
+            string[] parts = text.Split(' ');
+            int speed = parts[3].ToInt();
+            int fly = parts[6].ToInt();
+            int rest = parts[13].ToInt();
+
+            int totalDist = 0;
+            int totalSeconds = 0;
+            while(true)
+            {
+                if (totalSeconds + fly >= sec)
+                {
+                    totalDist += (sec - totalSeconds) * speed;
+                    return totalDist;
+                }
+                totalDist += fly * speed;
+                totalSeconds += fly;
+
+                if (totalSeconds + rest >= sec)
+                    return totalDist;
+                totalSeconds += rest;
+            }
         }
 
         public static int PartTwo(string filename)
--- AdventOfCode2015/Day14/Day14Tests.cs
+++ AdventOfCode2015/Day14/Day14Tests.cs
@@ -11,40 +11,23 @@ namespace AdventOfCode2015
     {
         const int DAY = 14;
 
+        [TestCase("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.", 1120)]
+        [TestCase("Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.", 1056)]
+        public void TestDistanceRaced(string text, int expected)
+        {
+            Day14.RaceForSeconds(text, 1000).Should().Be(expected);
+        }
+
         [Test]
         public void TestPartOne()
         {
-            Day14.PartOne(PuzzleFile(DAY)).Should().Be(0);
+            Day14.PartOne(PuzzleFile(DAY)).Should().Be(2655);
         }
 
         [Test]
         public void TestPartTwo()
         {
             Day14.PartTwo(PuzzleFile(DAY)).Should().Be(0);
-        }
-
-        [TestCaseSource(nameof(TestDataOne))]
-        public void TestPartOne(string filename, int expected)
-        {
-            Day14.PartOne(filename).Should().Be(expected);
-        }
-
-        [TestCaseSource(nameof(TestDataTwo))]
-        public void TestPartTwo(string filename, int expected)
-        {
-            Day14.PartTwo(filename).Should().Be(expected);
-        }
-
-        public static IEnumerable TestDataOne()
-        {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
-        }
-
-        public static IEnumerable TestDataTwo()
-        {
-            yield return new TestCaseData(TestFile(DAY), 0);
-            yield return new TestCaseData(PuzzleFile(DAY), 0);
         }
     }
 }
