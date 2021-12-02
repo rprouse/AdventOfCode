f0c96cbf2a2fea82139cc8ba8dc3a22a34ba84fe
Rob Prouse <rob@prouse.org>
2021-12-01 21:48:03 -0500

    Cleaning up program entry

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -5,18 +5,9 @@ using NUnit.Framework;
 
 [assembly: Parallelizable(ParallelScope.Fixtures)]
 
-namespace AdventOfCode2021
-{
-    public static class ProgramEntry
-    {
-        public static async Task Main()
-        {
-            //Day24.PartTwo(TestBase.TestFile(24), true);
+//Day24.PartTwo(TestBase.TestFile(24), true);
 
-            Console.ResetColor();
-            Console.ReadLine();
+Console.ResetColor();
+Console.ReadLine();
 
-            await Task.FromResult(false);
-        }
-    }
-}
+await Task.FromResult(false);
