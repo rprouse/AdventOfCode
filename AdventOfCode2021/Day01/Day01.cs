324ac394adefb210b4aad81d0a5ed35cbcf2658b
Rob Prouse <rob@prouse.org>
2021-12-01 21:28:00 -0500

    Cleaning up day 1

--- AdventOfCode2021/Day01/Day01.cs
+++ AdventOfCode2021/Day01/Day01.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Text;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
 
@@ -12,25 +8,18 @@ public static class Day01
     public static int PartOne(string filename)
     {
         int[] depths = filename.GetInts();
-        int count = 0;
-        for (int i = 0; i < depths.Length - 1; i++)
-        {
-            if (depths[i] < depths[i + 1]) count++;
-        }
-        return count;
+        return Enumerable
+            .Range(0, depths.Length - 1)
+            .Count(i => depths[i] < depths[i + 1]);
     }
 
     public static int PartTwo(string filename)
     {
         int[] depths = filename.GetInts();
-        int count = 0;
-        for (int i = 0; i < depths.Length - 3; i++)
-        {
-            int a = depths[i] + depths[i + 1] + depths[i + 2];
-            int b = depths[i + 1] + depths[i + 2] + depths[i + 3];
-            if (a < b) count++;
-        }
-        return count;
+        return Enumerable
+            .Range(0, depths.Length - 3)
+            .Count(i => depths[i] + depths[i + 1] + depths[i + 2] < 
+                        depths[i + 1] + depths[i + 2] + depths[i + 3]);
     }
 }
 
