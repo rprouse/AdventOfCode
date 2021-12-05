b09bec09904ccb5ad66a23fa11e1eff7c09d2aa0
Rob Prouse <rob@prouse.org>
2021-12-05 08:38:43 -0500

    Day 5 part 1

--- AdventOfCode2021/Day05/Day05.cs
+++ AdventOfCode2021/Day05/Day05.cs
@@ -9,10 +9,60 @@ namespace AdventOfCode2021;
 
 public static class Day05
 {
+    public class Direction
+    {
+        public Direction(string line)
+        {
+            var parts = line.Split("->");
+            var start = parts[0].Split(',');
+            var end = parts[1].Split(',');
+            X1 = start[0].ToInt();
+            Y1 = start[1].ToInt();
+            X2 = end[0].ToInt();
+            Y2 = end[1].ToInt();
+        }
+
+        public int X1 { get; init; }
+        public int Y1 { get; init; }
+        public int X2 { get; init; }
+        public int Y2 { get; init; }
+
+        public bool HorizontalOrVertical => X1 == X2 || Y1 == Y2;
+        public int MinX => Math.Min(X1, X2);
+        public int MaxX => Math.Max(X1, X2);
+        public int MinY => Math.Min(Y1, Y2);
+        public int MaxY => Math.Max(Y1, Y2);
+    }
+
     public static int PartOne(string filename)
     {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        var directions = filename
+            .ReadAllLines()
+            .Select(line => new Direction(line))
+            .Where(d => d.HorizontalOrVertical)
+            .ToList();
+        var maxX = directions.Max(d => d.MaxX) + 1;
+        var maxY = directions.Max(d => d.MaxY) + 1;
+        int[,] board = new int[maxX,maxY];
+        foreach(var d in directions)
+        {
+            for (int x = d.MinX; x <= d.MaxX; x++)
+            {
+                for (int y = d.MinY; y <= d.MaxY; y++)
+                {
+                    board[x, y]++;
+                }
+            }
+        }
+        int count = 0;
+        for (int y = 0; y < maxY; y++)
+        {
+            for (int x = 0; x < maxX; x++)
+            {
+                if(board[x,y] >= 2) count++;
+            }
+        }
+        return count;
     }
 
     public static int PartTwo(string filename)
--- AdventOfCode2021/Day05/Day05Tests.cs
+++ AdventOfCode2021/Day05/Day05Tests.cs
@@ -12,30 +12,6 @@ public class Day05Tests : TestBase
 {
     const int DAY = 05;
 
-    [Test]
-    public void TestPartOne()
-    {
-        Day05.PartOne(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [Test]
-    public void TestPartTwo()
-    {
-        Day05.PartTwo(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [TestCase("", 0)]
-    public void TestCasePartOne(string text, int expected)
-    {
-        Day05.PartOne(text).Should().Be(expected);
-    }
-
-    [TestCase("", 0)]
-    public void TestCasePartTwo(string text, int expected)
-    {
-        Day05.PartTwo(text).Should().Be(expected);
-    }
-
     [TestCaseSource(nameof(TestDataOne))]
     public void TestPartOne(string filename, int expected)
     {
@@ -50,8 +26,8 @@ public class Day05Tests : TestBase
 
     public static IEnumerable TestDataOne()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY), 5);
+        yield return new TestCaseData(PuzzleFile(DAY), 7436);
     }
 
     public static IEnumerable TestDataTwo()
