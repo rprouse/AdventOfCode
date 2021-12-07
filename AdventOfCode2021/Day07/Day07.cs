1d4ba37c38611c1443d013c1f8f6f6ee2e2aeb56
Rob Prouse <r.prouse@valsoftcorp.com>
2021-12-07 08:18:50 -0500

    Day 7 part 1

--- AdventOfCode2021/Day07/Day07.cs
+++ AdventOfCode2021/Day07/Day07.cs
@@ -11,8 +11,17 @@ public static class Day07
 {
     public static int PartOne(string filename)
     {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        int[] crabs = filename.SplitInts();
+        int min = crabs.Min();
+        int max = crabs.Max();
+        int fuel = int.MaxValue;
+        for (int i = min; i <= max; i++)
+        {
+            int sum = crabs.Select(x => Math.Abs(i-x)).Sum();
+            if (sum < fuel)
+                fuel = sum;
+        }
+        return fuel;
     }
 
     public static int PartTwo(string filename)
--- AdventOfCode2021/Day07/Day07Tests.cs
+++ AdventOfCode2021/Day07/Day07Tests.cs
@@ -13,21 +13,9 @@ public class Day07Tests : TestBase
     const int DAY = 07;
 
     [Test]
-    public void TestPartOne()
-    {
-        Day07.PartOne(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [Test]
     public void TestPartTwo()
     {
         Day07.PartTwo(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartOne(string text, int expected)
-    {
-        Day07.PartOne(text).Should().Be(expected);
     }
 
     [TestCase("", 0, Ignore = "If Needed")]
@@ -50,8 +38,8 @@ public class Day07Tests : TestBase
 
     public static IEnumerable TestDataOne()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY), 37);
+        yield return new TestCaseData(PuzzleFile(DAY), 344138);
     }
 
     public static IEnumerable TestDataTwo()
