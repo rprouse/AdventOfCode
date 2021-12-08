dd6cb8c4517263f92345e6ef516f75330a88879c
Rob Prouse <r.prouse@valsoftcorp.com>
2021-12-08 11:15:51 -0500

    Day 8 part 2

--- AdventOfCode2021/Day08/Day08.cs
+++ AdventOfCode2021/Day08/Day08.cs
@@ -7,6 +7,23 @@ using AdventOfCode.Core;
 
 namespace AdventOfCode2021;
 
+//   0:      1:      2:      3:      4:
+// aaaa    ....    aaaa    aaaa    ....
+//b    c  .    c  .    c  .    c  b    c
+//b    c  .    c  .    c  .    c  b    c
+// ....    ....    dddd    dddd    dddd
+//e    f  .    f  e    .  .    f  .    f
+//e    f  .    f  e    .  .    f  .    f
+// gggg    ....    gggg    gggg    ....
+
+//  5:      6:      7:      8:      9:
+// aaaa    aaaa    aaaa    aaaa    aaaa
+//b    .  b    .  .    c  b    c  b    c
+//b    .  b    .  .    c  b    c  b    c
+// dddd    dddd    ....    dddd    dddd
+//.    f  e    f  .    f  e    f  .    f
+//.    f  e    f  .    f  e    f  .    f
+// gggg    gggg    ....    gggg    gggg
 public static class Day08
 {
     public static int PartOne(string filename)
@@ -15,17 +32,99 @@ public static class Day08
         int count = 0;
         foreach (string line in lines)
         {
-            string[] parts = line.Split(" | ");
-            string[] input = parts[0].Split(' ');
-            string[] output = parts[1].Split(' ');
-            count += output.Count(o => o.Length == 2 || o.Length == 4 || o.Length == 3 || o.Length == 7);
+            (_, string[] outputs) = ParseLine(line);
+            count += outputs.Count(o => o.Length == 2 ||
+                o.Length == 4 ||
+                o.Length == 3 ||
+                o.Length == 7);
         }
         return count;
     }
 
-    public static int PartTwo(string filename)
+    public static int PartTwo(string filename) =>
+        filename.ReadAllLines()
+            .Sum(l =>
+            {
+                (string[] inputs, string[] outputs) = ParseLine(l);
+                return DetermineOutput(inputs, outputs);
+            });
+
+    internal static (string[] inputs, string[] outputs) ParseLine(string line)
     {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        string[] parts = line.Split(" | ");
+        string[] input = parts[0].Split(' ');
+        string[] output = parts[1].Split(' ');
+        return (input, output);
+    }
+
+    internal static int DetermineOutput(string[] inputs, string[] outputs)
+    {
+        var mappings = GetMappings(inputs).ToList();
+
+        int result = 0;
+        for(int i = 0; i < outputs.Length; i++)
+        {
+            string digit = outputs[3 - i];
+            string map = mappings.Single(m => digit.Length == m.Length && m.All(c => digit.Contains(c)));
+            int value = mappings.IndexOf(map);
+            int power = (int)Math.Pow(10, i);
+            result +=  value * power;
+        }
+        return result;
+    }
+
+    internal static string[] GetMappings(string[] inputs)
+    {
+        string[] mappings = new string[10];
+
+        // Ones have 2 segments c and f
+        mappings[1] = inputs.Single(i => i.Length == 2);
+
+        // Sevens have 3 segments a, c and f
+        mappings[7] = inputs.Single(i => i.Length == 3);
+
+        // Fours have 4 segments b, c, d and f
+        mappings[4] = inputs.First(i => i.Length == 4);
+
+        // Eights have 7 segments a, b, c, d, e, f and g
+        mappings[8] = inputs.First(i => i.Length == 7);
+
+        // Threes have 5 segments a, c, d, f and g
+        // and are common with seven
+        mappings[3] = inputs.First(i => i.Length == 5 &&
+            mappings[7].All(c => i.Contains(c)));
+
+        // Sixes have 6 segments a, b, d, e, f and g
+        // and are not common with seven
+        mappings[6] = inputs.First(i => i.Length == 6 &&
+            !mappings[7].All(c => i.Contains(c)));
+
+        // Fives have 5 segments a, b, d, f and g,
+        // are not 3
+        // and have all segments in common with 6
+        mappings[5] = inputs.First(i => i.Length == 5 &&
+            i != mappings[3] &&
+            i.All(c => mappings[6].Contains(c)));
+
+        // Twos have 5 segments a, c, d, e and g
+        // and are not 3 or 5
+        mappings[2] = inputs.First(i => i.Length == 5 &&
+            i != mappings[3] &&
+            i != mappings[5]);
+
+        // Nines have 6 segments a, b, c, d, f and g
+        // and are not 6
+        // and have segments in common with 4
+        mappings[9] = inputs.First(i => i.Length == 6 &&
+            i != mappings[6] &&
+            mappings[4].All(c => i.Contains(c)));
+
+        // Zeros have 6 segments a, b, c, e, f and g
+        // and are not 6 or 9
+        mappings[0] = inputs.First(i => i.Length == 6 &&
+            i != mappings[6] &&
+            i != mappings[9]);
+
+        return mappings;
     }
 }
--- AdventOfCode2021/Day08/Day08Tests.cs
+++ AdventOfCode2021/Day08/Day08Tests.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections;
-using System.IO;
 using AdventOfCode.Core;
-using NUnit.Framework;
 using FluentAssertions;
+using NUnit.Framework;
 
 namespace AdventOfCode2021;
 
@@ -13,21 +11,36 @@ public class Day08Tests : TestBase
     const int DAY = 08;
 
     [Test]
-    public void TestPartTwo()
+    public void TestGetMappings()
     {
-        Day08.PartTwo(PuzzleFile(DAY)).Should().Be(0);
+        string[] mappings = Day08.GetMappings(new[] { "acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab" });
+        mappings[0].Should().Be("cagedb");
+        mappings[1].Should().Be("ab");
+        mappings[2].Should().Be("gcdfa");
+        mappings[3].Should().Be("fbcad");
+        mappings[4].Should().Be("eafb");
+        mappings[5].Should().Be("cdfbe");
+        mappings[6].Should().Be("cdfgeb");
+        mappings[7].Should().Be("dab");
+        mappings[8].Should().Be("acedgfb");
+        mappings[9].Should().Be("cefabd");
     }
 
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartOne(string text, int expected)
+    [TestCase("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf", 5353)]
+    [TestCase("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", 8394)]
+    [TestCase("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", 9781)]
+    [TestCase("fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", 1197)]
+    [TestCase("fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", 9361)]
+    [TestCase("aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", 4873)]
+    [TestCase("fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", 8418)]
+    [TestCase("dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", 4548)]
+    [TestCase("bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", 1625)]
+    [TestCase("egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", 8717)]
+    [TestCase("gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", 4315)]
+    public void TestDetermineOutput(string line, int expected)
     {
-        Day08.PartOne(text).Should().Be(expected);
-    }
-
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartTwo(string text, int expected)
-    {
-        Day08.PartTwo(text).Should().Be(expected);
+        (string[] inputs, string[] outputs) = Day08.ParseLine(line);
+        Day08.DetermineOutput(inputs, outputs).Should().Be(expected);
     }
 
     [TestCaseSource(nameof(TestDataOne))]
@@ -50,7 +63,7 @@ public class Day08Tests : TestBase
 
     public static IEnumerable TestDataTwo()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY), 61229);
+        yield return new TestCaseData(PuzzleFile(DAY), 1048410);
     }
 }
