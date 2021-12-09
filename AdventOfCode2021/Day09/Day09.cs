ceeb7d4b699f6efddda5a4bac321b66613653431
Rob Prouse <r.prouse@valsoftcorp.com>
2021-12-09 08:54:00 -0500

    Refactor day 9 part 1

--- AdventOfCode2021/Day09/Day09.cs
+++ AdventOfCode2021/Day09/Day09.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
+using System.Drawing;
 
 namespace AdventOfCode2021;
 
@@ -11,32 +12,35 @@ public static class Day09
 {
     public static int PartOne(string filename)
     {
-        var map = filename.ReadAllLines()
+        var map = GetMap(filename);
+        var lowPoints = GetLowPoints(map);
+        return lowPoints.Sum(p => map[p.Y][p.X] + 1);
+    }
+
+    private static int[][] GetMap(string filename)
+    {
+        return filename.ReadAllLines()
             .Select(line => line.Select(c => c.ToString().ToInt()).ToArray())
             .ToArray();
+    }
 
-        int risk = 0;
-        for(int y = 0; y < map.Length; y++)
+    private static IList<Point> GetLowPoints(int[][] map)
+    {
+        var lowPoints = new List<Point>();
+        for (int y = 0; y < map.Length; y++)
         {
-            for(int x = 0; x < map[y].Length; x++)
+            for (int x = 0; x < map[y].Length; x++)
             {
-                try
+                if ((x == 0 || map[y][x] < map[y][x - 1]) &&
+                    (y == 0 || map[y][x] < map[y - 1][x]) &&
+                    (x == map[y].Length - 1 || map[y][x] < map[y][x + 1]) &&
+                    (y == map.Length - 1 || map[y][x] < map[y + 1][x]))
                 {
-                    if ((x == 0 || map[y][x] < map[y][x - 1]) &&
-                       (y == 0 || map[y][x] < map[y - 1][x]) &&
-                       (x == map[y].Length - 1 || map[y][x] < map[y][x + 1]) &&
-                       (y == map.Length - 1 || map[y][x] < map[y + 1][x]))
-                    {
-                        risk += map[y][x] + 1;
-                    }
-                }
-                catch(Exception)
-                {
-                    Console.WriteLine($"{x},{y} = {map[y].Length},{map.Length}");
+                    lowPoints.Add(new Point(x, y));
                 }
             }
         }
-        return risk;
+        return lowPoints;
     }
 
     public static int PartTwo(string filename)
