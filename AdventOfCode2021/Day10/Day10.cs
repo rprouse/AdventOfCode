e8dfc9491730b3f6c91929d63acb82473c04cad3
Rob Prouse <r.prouse@valsoftcorp.com>
2021-12-10 18:11:15 -0500

    Not the correct solution to day 10

--- AdventOfCode2021/Day10/Day10.cs
+++ AdventOfCode2021/Day10/Day10.cs
@@ -9,13 +9,66 @@ namespace AdventOfCode2021;
 
 public static class Day10
 {
-    public static int PartOne(string filename)
+    static Dictionary<char, char> _matches = new Dictionary<char, char>
     {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+        { '<', '>' }
+    };
+
+    public static long PartOne(string filename)
+    {
+        long score = 0;
         string[] lines = filename.ReadAllLines();
-        return 0;
+        foreach (string line in lines)
+        {
+            var corrupted = GetCorruptedCharacter(line);
+            score += GetCorruptedScore(corrupted);
+        }
+        return score;
     }
 
-    public static int PartTwo(string filename)
+    internal static char GetCorruptedCharacter(string line)
+    {
+        var buffer = new StringBuilder();
+        bool foundMatch = false;
+        do
+        {
+            foundMatch = false;
+            buffer.Clear();
+            for (int i = 0; i < line.Length - 1; i++)
+            {
+                if (_matches.ContainsKey(line[i]) &&
+                   _matches[line[i]] == line[i + 1])
+                {
+                    foundMatch = true;
+                    i += 1;
+                }
+                else
+                {
+                    buffer.Append(line[i]);
+                }
+            }
+            line = buffer.ToString();
+        }
+        while (foundMatch);
+        char corrupted = line.FirstOrDefault(c => _matches.Values.Contains(c));
+        Console.WriteLine($"{(corrupted == 0 ? ' ' : corrupted)} {line}");
+        return corrupted;
+    }
+
+    internal static int GetCorruptedScore(char corrupted) =>
+        corrupted switch
+        {
+            ')' => 3,
+            ']' => 57,
+            '}' => 1197,
+            '>' => 25137,
+            _ => 0
+        };
+
+    public static long PartTwo(string filename)
     {
         string[] lines = filename.ReadAllLines();
         return 0;
--- AdventOfCode2021/Day10/Day10Tests.cs
+++ AdventOfCode2021/Day10/Day10Tests.cs
@@ -25,33 +25,33 @@ public class Day10Tests : TestBase
     }
 
     [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartOne(string text, int expected)
+    public void TestCasePartOne(string text, long expected)
     {
         Day10.PartOne(text).Should().Be(expected);
     }
 
     [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartTwo(string text, int expected)
+    public void TestCasePartTwo(string text, long expected)
     {
         Day10.PartTwo(text).Should().Be(expected);
     }
 
     [TestCaseSource(nameof(TestDataOne))]
-    public void TestPartOne(string filename, int expected)
+    public void TestPartOne(string filename, long expected)
     {
         Day10.PartOne(filename).Should().Be(expected);
     }
 
     [TestCaseSource(nameof(TestDataTwo))]
-    public void TestPartTwo(string filename, int expected)
+    public void TestPartTwo(string filename, long expected)
     {
         Day10.PartTwo(filename).Should().Be(expected);
     }
 
     public static IEnumerable TestDataOne()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY), 26397);
+        yield return new TestCaseData(PuzzleFile(DAY), 210021);
     }
 
     public static IEnumerable TestDataTwo()
