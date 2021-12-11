45833f66d25218d3b7a41372c2c386ff33cfaf86
Rob Prouse <rob@prouse.org>
2021-12-11 16:33:42 -0500

    Refactor day 11

--- AdventOfCode2021/Day11/Day11.cs
+++ AdventOfCode2021/Day11/Day11.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Text;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
 
@@ -15,56 +11,10 @@ public static class Day11
         int[][] map = GetMap(filename);
         foreach (var _ in Enumerable.Range(0, 100))
         {
-            // Increment all by one
-            foreach (var row in map)
-            {
-                for (int i = 0; i < row.Length; i++)
-                    row[i]++;
-            }
-            // Flash
-            bool found = true;
-            while (found)
-            {
-                found = false;
-                for (int y = 0; y < map.Length; y++)
-                {
-                    for (int x = 0; x < map[y].Length; x++)
-                    {
-                        if (map[y][x] > 9)
-                        {
-                            found = true;
-                            flashes++;
-                            map[y][x] = 0;
-
-                            if (y > 0 && map[y - 1][x] != 0) map[y - 1][x]++;
-                            if (x > 0 && map[y][x - 1] != 0) map[y][x - 1]++;
-                            if (y < 9 && map[y + 1][x] != 0) map[y + 1][x]++;
-                            if (x < 9 && map[y][x + 1] != 0) map[y][x + 1]++;
-
-                            if (y > 0 && x > 0 && map[y - 1][x - 1] != 0) map[y - 1][x - 1]++;
-                            if (y < 9 && x > 0 && map[y + 1][x - 1] != 0) map[y + 1][x - 1]++;
-                            if (y > 0 && x < 9 && map[y - 1][x + 1] != 0) map[y - 1][x + 1]++;
-                            if (y < 9 && x < 9 && map[y + 1][x + 1] != 0) map[y + 1][x + 1]++;
-                        }
-                    }
-                }
-            }
-            //OutputMap(map);
+            IncrementOctopuses(map);
+            flashes += FlashOctopuses(map);
         }
         return flashes;
-    }
-
-    static void OutputMap(int[][] map)
-    {
-        for (int y = 0; y < map.Length; y++)
-        {
-            for (int x = 0; x < map[y].Length; x++)
-            {
-                Console.Write(map[y][x]);
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine();
     }
 
     public static int PartTwo(string filename)
@@ -73,48 +23,58 @@ public static class Day11
         int[][] map = GetMap(filename);
         do
         {
-            // Increment all by one
-            foreach (var row in map)
-            {
-                for (int i = 0; i < row.Length; i++)
-                    row[i]++;
-            }
-            // Flash
-            bool found = true;
-            while (found)
-            {
-                found = false;
-                for (int y = 0; y < map.Length; y++)
-                {
-                    for (int x = 0; x < map[y].Length; x++)
-                    {
-                        if (map[y][x] > 9)
-                        {
-                            found = true;
-                            map[y][x] = 0;
-
-                            if (y > 0 && map[y - 1][x] != 0) map[y - 1][x]++;
-                            if (x > 0 && map[y][x - 1] != 0) map[y][x - 1]++;
-                            if (y < 9 && map[y + 1][x] != 0) map[y + 1][x]++;
-                            if (x < 9 && map[y][x + 1] != 0) map[y][x + 1]++;
-
-                            if (y > 0 && x > 0 && map[y - 1][x - 1] != 0) map[y - 1][x - 1]++;
-                            if (y < 9 && x > 0 && map[y + 1][x - 1] != 0) map[y + 1][x - 1]++;
-                            if (y > 0 && x < 9 && map[y - 1][x + 1] != 0) map[y - 1][x + 1]++;
-                            if (y < 9 && x < 9 && map[y + 1][x + 1] != 0) map[y + 1][x + 1]++;
-                        }
-                    }
-                }
-            }
+            IncrementOctopuses(map);
+            FlashOctopuses(map);
             steps++;
-            //OutputMap(map);
         }
         while (!map.All(r => r.All(c => c == 0)));
         return steps;
     }
 
-    internal static int[][] GetMap(string filename) =>
+    private static int[][] GetMap(string filename) =>
         filename.ReadAllLines()
             .Select(line => line.Select(c => c.ToString().ToInt()).ToArray())
             .ToArray();
+
+    private static void IncrementOctopuses(int[][] map)
+    {
+        foreach (var row in map)
+        {
+            for (int i = 0; i < row.Length; i++)
+                row[i]++;
+        }
+    }
+
+    private static int FlashOctopuses(int[][] map)
+    {
+        int flashes = 0;
+        bool found = true;
+        while (found)
+        {
+            found = false;
+            for (int y = 0; y < map.Length; y++)
+            {
+                for (int x = 0; x < map[y].Length; x++)
+                {
+                    if (map[y][x] > 9)
+                    {
+                        flashes++;
+                        found = true;
+                        map[y][x] = 0;
+
+                        if (y > 0 && map[y - 1][x] != 0) map[y - 1][x]++;
+                        if (x > 0 && map[y][x - 1] != 0) map[y][x - 1]++;
+                        if (y < 9 && map[y + 1][x] != 0) map[y + 1][x]++;
+                        if (x < 9 && map[y][x + 1] != 0) map[y][x + 1]++;
+
+                        if (y > 0 && x > 0 && map[y - 1][x - 1] != 0) map[y - 1][x - 1]++;
+                        if (y < 9 && x > 0 && map[y + 1][x - 1] != 0) map[y + 1][x - 1]++;
+                        if (y > 0 && x < 9 && map[y - 1][x + 1] != 0) map[y - 1][x + 1]++;
+                        if (y < 9 && x < 9 && map[y + 1][x + 1] != 0) map[y + 1][x + 1]++;
+                    }
+                }
+            }
+        }
+        return flashes;
+    }
 }
