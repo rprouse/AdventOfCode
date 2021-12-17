2186dc96b235ef3a173dc44e1e7de64e908d8141
Rob Prouse <rob@prouse.org>
2021-12-16 23:18:28 -0500

    working on Dijkstra

--- AdventOfCode2021/Day15/Day15.cs
+++ AdventOfCode2021/Day15/Day15.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
+using System.Drawing;
 
 namespace AdventOfCode2021;
 
@@ -17,7 +18,7 @@ public static class Day15
 
     public static int PartTwo(string filename)
     {
-        int[,] map = ParseMap(filename); 
+        int[,] map = ParseMap(filename);
         return 0;
     }
 
@@ -33,5 +34,36 @@ public static class Day15
             }
         }
         return map;
+    }
+
+    internal static int Dijkstra(int[,] graph, Point start, Point end)
+    {
+        List<Point> queue = new List<Point>();
+        Dictionary<Point, int> dist = new Dictionary<Point, int>();
+        Point[,] prev = new Point[graph.GetLength(0), graph.GetLength(1)];
+
+        for (int y = 0; y < graph.GetLength(1); y++)
+        {
+            for(int x = 0; x <= graph.GetLength(0); x++)
+            {
+                Point p = new Point(x, y);
+                dist[p] = int.MaxValue;
+                queue.Add(p);
+            }
+        }
+        dist[start] = 0;
+
+        while(queue.Count > 0)
+        {
+            int min = queue.Min(p => dist[p]);
+            Point u = queue.First(p => dist[p] == min);
+
+            queue.Remove(u);
+
+            // foreach neighbor still in queue
+
+        }
+
+        return 0;
     }
 }
