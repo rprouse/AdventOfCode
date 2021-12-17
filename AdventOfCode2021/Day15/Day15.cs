3c405f5a8741d62ef3996082a92dd9a75050bbed
Rob Prouse <rob@prouse.org>
2021-12-16 20:03:23 -0500

    data for day 15

--- AdventOfCode2021/Day15/Day15.cs
+++ AdventOfCode2021/Day15/Day15.cs
@@ -11,13 +11,27 @@ public static class Day15
 {
     public static int PartOne(string filename)
     {
-        string[] lines = filename.ReadAllLines();
+        int[,] map = ParseMap(filename);
         return 0;
     }
 
     public static int PartTwo(string filename)
     {
-        string[] lines = filename.ReadAllLines();
+        int[,] map = ParseMap(filename);
         return 0;
+    }
+
+    internal static int[,] ParseMap(string filename)
+    {
+        string[] lines = filename.ReadAllLines();
+        int[,] map = new int[lines[0].Length, lines.Length];
+        for (int y = 0; y < lines.Length; y++)
+        {
+            for (int x = 0; x < lines[0].Length; x++)
+            {
+                map[x, y] = lines[y][x] - '0';
+            }
+        }
+        return map;
     }
 }
