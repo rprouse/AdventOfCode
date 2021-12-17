360732e0ef5bf7374d4d64aa5a7bfaa09aca09e5
Rob Prouse <rob@prouse.org>
2021-12-17 07:28:38 -0500

    Data for day 17

--- AdventOfCode2021/Day17/Day17Tests.cs
+++ AdventOfCode2021/Day17/Day17Tests.cs
@@ -12,51 +12,17 @@ public class Day17Tests : TestBase
 {
     const int DAY = 17;
 
-    [Test]
-    public void TestPartOne()
+    [TestCase("target area: x=269..292, y=-68..-44", 0)]
+    [TestCase("target area: x=269..292, y=-68..-44", 0)]
+    public void TestCasePartOne(string targetArea, int expected)
     {
-        Day17.PartOne(PuzzleFile(DAY)).Should().Be(0);
+        Day17.PartOne(targetArea).Should().Be(expected);
     }
 
-    [Test]
-    public void TestPartTwo()
+    [TestCase("target area: x=269..292, y=-68..-44", 0)]
+    [TestCase("target area: x=269..292, y=-68..-44", 0)]
+    public void TestCasePartTwo(string targetArea, int expected)
     {
-        Day17.PartTwo(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartOne(string text, int expected)
-    {
-        Day17.PartOne(text).Should().Be(expected);
-    }
-
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartTwo(string text, int expected)
-    {
-        Day17.PartTwo(text).Should().Be(expected);
-    }
-
-    [TestCaseSource(nameof(TestDataOne))]
-    public void TestPartOne(string filename, int expected)
-    {
-        Day17.PartOne(filename).Should().Be(expected);
-    }
-
-    [TestCaseSource(nameof(TestDataTwo))]
-    public void TestPartTwo(string filename, int expected)
-    {
-        Day17.PartTwo(filename).Should().Be(expected);
-    }
-
-    public static IEnumerable TestDataOne()
-    {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
-    }
-
-    public static IEnumerable TestDataTwo()
-    {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
+        Day17.PartTwo(targetArea).Should().Be(expected);
     }
 }
