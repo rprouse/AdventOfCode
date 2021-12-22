a50aba5a8ab020d07194ef91dbad620c8c64ffce
Rob Prouse <rob@prouse.org>
2021-12-22 03:02:52 -0500

    Day 20 part 1

--- AdventOfCode2021/Day20/Day20.cs
+++ AdventOfCode2021/Day20/Day20.cs
@@ -9,10 +9,77 @@ namespace AdventOfCode2021;
 
 public static class Day20
 {
-    public static int PartOne(string filename)
+    public static int PartOne(string filename, int passes)
     {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        string[] lines = filename.ReadAllLinesIncludingBlank();
+        int[] enhance = lines[0].Select(c => c == '#' ? 1 : 0).ToArray();
+        int[][] image = lines
+            .Skip(2)
+            .Select(line => line.Select(c => c == '#' ? 1 : 0).ToArray())
+            .ToArray();
+
+        //ViewImage(image);
+
+        for (int i = 0; i < passes; i++)
+        {
+            int[][] newImage = new int[image.Length + 2][];
+            for (int y = 0; y < newImage.Length; y++)
+            {
+                newImage[y] = new int[image[0].Length + 2];
+                if ( y > 0 && y < newImage.Length - 1)
+                {
+                    for(int x = 0; x < image[0].Length; x++)
+                    {
+                        newImage[y][x + 1] = image[y - 1][x];
+                    }
+                }
+            }
+            image = newImage; 
+            newImage = new int[image.Length][];
+            for (int y = 0; y < newImage.Length; y++)
+            {
+                newImage[y] = new int[image[y].Length];
+                for (int x = 0; x < newImage[y].Length; x++)
+                {
+                    int index = 0;
+                    if (x > 0 && y > 0) index += image[y - 1][x - 1];
+                    index = index << 1;
+                    if (y > 0) index += image[y - 1][x];
+                    index = index << 1;
+                    if (x < image[y].Length - 1 && y > 0) index += image[y - 1][x + 1];
+                    index = index << 1;
+                    if (x > 0) index += image[y][x - 1];
+                    index = index << 1;
+                    index += image[y][x];
+                    index = index << 1;
+                    if (x < image[y].Length - 1) index += image[y][x + 1];
+                    index = index << 1;
+                    if (x > 0 && y < image.Length - 1) index += image[y + 1][x - 1];
+                    index = index << 1;
+                    if (y < image.Length - 1) index += image[y + 1][x];
+                    index = index << 1;
+                    if (x < image[y].Length - 1 && y < image.Length - 1) index += image[y + 1][x + 1];
+
+                    newImage[y][x] = enhance[index];
+                }
+            }
+            image = newImage;
+            //ViewImage(image);
+        }
+
+        return image.Sum(l => l.Count(c => c == 1));
+    }
+
+    static void ViewImage(int[][] image)
+    {
+        for (int y = 0; y < image.Length; y++)
+        {
+            for (int x = 0; x < image[y].Length; x++)
+            {
+                Console.Write(image[y][x] == 1 ? '#' : '.');
+            }
+            Console.WriteLine();
+        }
     }
 
     public static int PartTwo(string filename)
--- AdventOfCode2021/Day20/Day20Tests.cs
+++ AdventOfCode2021/Day20/Day20Tests.cs
@@ -12,34 +12,10 @@ public class Day20Tests : TestBase
 {
     const int DAY = 20;
 
-    [Test]
-    public void TestPartOne()
-    {
-        Day20.PartOne(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [Test]
-    public void TestPartTwo()
-    {
-        Day20.PartTwo(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartOne(string text, int expected)
-    {
-        Day20.PartOne(text).Should().Be(expected);
-    }
-
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartTwo(string text, int expected)
-    {
-        Day20.PartTwo(text).Should().Be(expected);
-    }
-
     [TestCaseSource(nameof(TestDataOne))]
     public void TestPartOne(string filename, int expected)
     {
-        Day20.PartOne(filename).Should().Be(expected);
+        Day20.PartOne(filename, 2).Should().Be(expected);
     }
 
     [TestCaseSource(nameof(TestDataTwo))]
@@ -50,8 +26,8 @@ public class Day20Tests : TestBase
 
     public static IEnumerable TestDataOne()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY), 35);
+        yield return new TestCaseData(PuzzleFile(DAY), 5960);
     }
 
     public static IEnumerable TestDataTwo()
