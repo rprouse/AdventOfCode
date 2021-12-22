049786e070ff3e31ac63979d4d3b8acde72ea975
Rob Prouse <rob@prouse.org>
2021-12-22 02:07:57 -0500

    Day 21 part 1

--- AdventOfCode2021/Day21/Day21.cs
+++ AdventOfCode2021/Day21/Day21.cs
@@ -9,10 +9,29 @@ namespace AdventOfCode2021;
 
 public static class Day21
 {
-    public static int PartOne(string filename)
+    public static int PartOne(int p1, int p2)
     {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        int rolls = 0;
+        int dice = 0;
+        int[] score = new int[2];
+        int[] player = new int[2];
+        player[0] = p1;
+        player[1] = p2;
+        while (true)
+        {
+            for (int p = 0; p < 2; p++)
+            {
+                player[p] += dice * 3 + 6;
+                player[p] = (player[p] - 1) % 10 + 1;
+                score[p] += player[p];
+                rolls += 3;
+                dice += 3;
+                if (score[p] >= 1000)
+                {
+                    return rolls * score[(p + 1) % 2];
+                }
+            }
+        }
     }
 
     public static int PartTwo(string filename)
--- AdventOfCode2021/Day21/Day21Tests.cs
+++ AdventOfCode2021/Day21/Day21Tests.cs
@@ -12,51 +12,16 @@ public class Day21Tests : TestBase
 {
     const int DAY = 21;
 
-    [Test]
-    public void TestPartOne()
+    [TestCase(4, 8, 739785)]
+    [TestCase(6, 9, 925605)]
+    public void TestCasePartOne(int p1, int p2, int expected)
     {
-        Day21.PartOne(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [Test]
-    public void TestPartTwo()
-    {
-        Day21.PartTwo(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartOne(string text, int expected)
-    {
-        Day21.PartOne(text).Should().Be(expected);
+        Day21.PartOne(p1, p2).Should().Be(expected);
     }
 
     [TestCase("", 0, Ignore = "If Needed")]
     public void TestCasePartTwo(string text, int expected)
     {
         Day21.PartTwo(text).Should().Be(expected);
-    }
-
-    [TestCaseSource(nameof(TestDataOne))]
-    public void TestPartOne(string filename, int expected)
-    {
-        Day21.PartOne(filename).Should().Be(expected);
-    }
-
-    [TestCaseSource(nameof(TestDataTwo))]
-    public void TestPartTwo(string filename, int expected)
-    {
-        Day21.PartTwo(filename).Should().Be(expected);
-    }
-
-    public static IEnumerable TestDataOne()
-    {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
-    }
-
-    public static IEnumerable TestDataTwo()
-    {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
     }
 }
