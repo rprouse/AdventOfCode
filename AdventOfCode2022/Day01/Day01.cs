a0adb6f5467efe024645b1ad8aa5acfe912072c1
Rob Prouse <r.prouse@valsoftcorp.com>
2022-11-25 10:23:50 -0500

    Ready for day 1

---
+++ AdventOfCode2022/Day01/Day01.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Core;
+
+namespace AdventOfCode2022;
+
+public static class Day01
+{
+    public static int PartOne(string filename)
+    {
+        string[] lines = filename.ReadAllLines();
+        return 0;
+    }
+
+    public static int PartTwo(string filename)
+    {
+        string[] lines = filename.ReadAllLines();
+        return 0;
+    }
+}
