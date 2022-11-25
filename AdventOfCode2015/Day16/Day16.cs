0bebbdd141d559bdd2d35c4d9fb46ef5e8327be3
Rob Prouse <r.prouse@valsoftcorp.com>
2022-11-25 14:25:14 -0500

    2015 day 16 part 1

--- AdventOfCode2015/Day16/Day16.cs
+++ AdventOfCode2015/Day16/Day16.cs
@@ -7,11 +7,85 @@ namespace AdventOfCode2015;
 
 public static class Day16
 {
-    public static int PartOne(string filename)
+    public class MFCSAM
     {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        public MFCSAM(string line)
+        {
+            var p1 = line.Split(": ", 2);
+            Number = p1[0].Substring(4).ToInt();
+            foreach(string part in p1[1].Split(", ")) 
+            {
+                var p2 = part.Split(": ");
+                switch(p2[0])
+                {
+                    case "children":
+                        Children = p2[1].ToInt();
+                        break;
+                    case "cats":
+                        Cats = p2[1].ToInt();
+                        break;
+                    case "samoyeds":
+                        Samoyeds = p2[1].ToInt();
+                        break;
+                    case "pomeranians":
+                        Pomeranians = p2[1].ToInt();
+                        break;
+                    case "akitas":
+                        Akitas = p2[1].ToInt();
+                        break;
+                    case "vizslas":
+                        Vizslas = p2[1].ToInt();
+                        break;
+                    case "goldfish":
+                        Goldfish = p2[1].ToInt();
+                        break;
+                    case "trees":
+                        Trees = p2[1].ToInt();
+                        break;
+                    case "cars":
+                        Cars = p2[1].ToInt();
+                        break;
+                    case "perfumes":
+                        Perfumes = p2[1].ToInt();
+                        break;
+                }
+            }
+        }
+
+        public bool Matches()
+        {
+            if (Children.HasValue && Children != 3) return false;
+            if (Cats.HasValue && Cats != 7) return false;
+            if (Samoyeds.HasValue && Samoyeds != 2) return false;
+            if (Pomeranians.HasValue && Pomeranians != 3) return false;
+            if (Akitas.HasValue && Akitas != 0) return false;
+            if (Vizslas.HasValue && Vizslas != 0) return false;
+            if (Goldfish.HasValue && Goldfish != 5) return false;
+            if (Trees.HasValue && Trees != 3) return false;
+            if (Cars.HasValue && Cars != 2) return false;
+            if (Perfumes.HasValue && Perfumes != 1) return false;
+            return true;
+        }
+
+        public int Number { get; }
+        public int? Children { get; }
+        public int? Cats { get; }
+        public int? Samoyeds { get; }
+        public int? Pomeranians { get; }
+        public int? Akitas { get; }
+        public int? Vizslas { get; }
+        public int? Goldfish { get; }
+        public int? Trees { get; }
+        public int? Cars { get; }
+        public int? Perfumes { get; }
     }
+
+    public static int PartOne(string filename) =>
+        filename.ReadAllLines()
+            .Select(l => new MFCSAM(l))
+            .Where(a => a.Matches())
+            .First()
+            .Number;
 
     public static int PartTwo(string filename)
     {
--- AdventOfCode2015/Day16/Day16Tests.cs
+++ AdventOfCode2015/Day16/Day16Tests.cs
@@ -14,36 +14,12 @@ public class Day16Tests : TestBase
     [Test]
     public void TestPartOne()
     {
-        Day16.PartOne(PuzzleFile(DAY)).Should().Be(0);
+        Day16.PartOne(PuzzleFile(DAY)).Should().Be(103);
     }
 
     [Test]
     public void TestPartTwo()
     {
         Day16.PartTwo(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [TestCaseSource(nameof(TestDataOne))]
-    public void TestPartOne(string filename, int expected)
-    {
-        Day16.PartOne(filename).Should().Be(expected);
-    }
-
-    [TestCaseSource(nameof(TestDataTwo))]
-    public void TestPartTwo(string filename, int expected)
-    {
-        Day16.PartTwo(filename).Should().Be(expected);
-    }
-
-    public static IEnumerable TestDataOne()
-    {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
-    }
-
-    public static IEnumerable TestDataTwo()
-    {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
     }
 }
