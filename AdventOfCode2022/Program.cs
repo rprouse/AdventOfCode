a695a4e3738c44756427b8d6363263df3c66b018
Rob Prouse <r.prouse@valsoftcorp.com>
2022-11-25 10:22:37 -0500

    Setup new project for 2022

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+using AdventOfCode.Core;
+using AdventOfCode2022;
+using NUnit.Framework;
+
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
+//int result = Day15.PartTwo(TestBase.PuzzleFile(15));
+//Console.WriteLine(result);
+
+Console.ResetColor();
+Console.ReadLine();
+
+await Task.FromResult(false);
