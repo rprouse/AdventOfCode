706d65b12a3a05f924c3ed8351917cf6c6c586bc
Rob Prouse <rob@prouse.org>
2022-11-27 14:58:04 -0500

    Ready for 2015 day 19

---
+++ AdventOfCode2015/Day19/Day19Tests.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.IO;
+using AdventOfCode.Core;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AdventOfCode2015;
+
+public class Day19Tests : TestBase
+{
+    const int DAY = 19;
+
+    [Test]
+    public void TestPartOne()
+    {
+        Day19.PartOne(PuzzleFile(DAY)).Should().Be(0);
+    }
+
+    [Test]
+    public void TestPartTwo()
+    {
+        Day19.PartTwo(PuzzleFile(DAY)).Should().Be(0);
+    }
+
+    [TestCaseSource(nameof(TestDataOne))]
+    public void TestPartOne(string filename, int expected)
+    {
+        Day19.PartOne(filename).Should().Be(expected);
+    }
+
+    [TestCaseSource(nameof(TestDataTwo))]
+    public void TestPartTwo(string filename, int expected)
+    {
+        Day19.PartTwo(filename).Should().Be(expected);
+    }
+
+    public static IEnumerable TestDataOne()
+    {
+        yield return new TestCaseData(TestFile(DAY), 0);
+        yield return new TestCaseData(PuzzleFile(DAY), 0);
+    }
+
+    public static IEnumerable TestDataTwo()
+    {
+        yield return new TestCaseData(TestFile(DAY), 0);
+        yield return new TestCaseData(PuzzleFile(DAY), 0);
+    }
+}
