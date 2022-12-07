001f96616ed20e939ecff2c0e00a822731dfc893
Rob Prouse <rob@prouse.org>
2022-12-07 07:34:17 -0500

    Checkpointing day 7

--- AdventOfCode2022/Day07/Day07.cs
+++ AdventOfCode2022/Day07/Day07.cs
@@ -11,7 +11,7 @@ public static class Day07
 {
     public static int PartOne(string filename)
     {
-        string[] lines = filename.ReadAllLines();
+        Directory root = PlaybackCommands(filename);
         return 0;
     }
 
@@ -20,4 +20,78 @@ public static class Day07
         string[] lines = filename.ReadAllLines();
         return 0;
     }
+
+    private static Directory PlaybackCommands(string filename)
+    {
+        string[] commands = filename.ReadAllLines();
+        var root = new Directory("/", null);
+        var pwd = root;
+        foreach (string cmd in commands.Skip(1))
+        {
+            if (cmd[0] == '$') // Command
+            {
+                if (cmd == "$ ls") continue;
+                if (cmd == "$ cd ..")
+                {
+                    if (pwd.ParentDirectory == null)
+                        throw new NotSupportedException("No parent directory");
+
+                    pwd = pwd.ParentDirectory;
+                    continue;
+                }
+                var name = cmd.Substring(5);
+                var dir = pwd.Directories.FirstOrDefault(d => d.Name == name);
+                if (dir == null)
+                {
+                    dir = new Directory(name, pwd);
+                    pwd.Directories.Add(dir);
+                }
+                pwd = dir;
+            }
+            else // Output
+            {
+                if (cmd.StartsWith("dir"))
+                {
+                    var name = cmd.Substring(4);
+                    var dir = pwd.Directories.FirstOrDefault(d => d.Name == name);
+                    if (dir == null)
+                    {
+                        dir = new Directory(name, pwd);
+                        pwd.Directories.Add(dir);
+                    }
+                }
+                else
+                {
+                    var parts = cmd.Split(' ');
+                    var file = new File(parts[1], parts[0].ToInt());
+                    pwd.Files.Add(file);
+                }
+            }
+        }
+        return root;
+    }
 }
+
+internal class Directory
+{
+    public Directory(string name, Directory parent)
+    {
+        Name = name;
+        ParentDirectory = parent;
+    }
+
+    public string Name { get; set; }
+    public Directory ParentDirectory { get; set; }
+    public IList<Directory> Directories { get; } = new List<Directory>();
+    public IList<File> Files { get; } = new List<File>();
+
+    public long Size() =>
+        Files.Sum(f => f.Size) + Directories.Sum(d => d.Size());
+
+    public IEnumerable<long> SizeOfDirectoriesLargerThan(long size)
+    {
+        yield return 0;
+    }
+}
+
+internal record File(string Name, long Size);
--- AdventOfCode2022/Day07/Day07Tests.cs
+++ AdventOfCode2022/Day07/Day07Tests.cs
@@ -50,7 +50,7 @@ public class Day07Tests : TestBase
 
     public static IEnumerable TestDataOne()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY), 95437);
         yield return new TestCaseData(PuzzleFile(DAY), 0);
     }
 
