b3950e3d93930333faf4d0844d9590882c1cdbb0
Rob Prouse <r.prouse@valsoftcorp.com>
2022-12-08 10:10:08 -0500

    Day 8 part 2

--- AdventOfCode2022/Day08/Day08.cs
+++ AdventOfCode2022/Day08/Day08.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Text;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using AdventOfCode.Core;
 
@@ -89,6 +86,75 @@ public static class Day08
         return count;
     }
 
+    public static int PartTwo(string filename)
+    {
+        int[,] trees = ParseTrees(filename);
+        var scores = new List<int>();
+        int xMax = trees.GetLength(0);
+        int yMax = trees.GetLength(1);
+        for (int y = 1; y < yMax - 1; y++)
+        {
+            for (int x = 1; x < xMax - 1; x++)
+            {
+                int score = 0;
+                // Left
+                bool found = false;
+                for (int x1 = x - 1; x1 >= 0; x1--)
+                {
+                    if (trees[x1, y] >= trees[x, y])
+                    {
+                        score = x - x1;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) score = x;
+
+                // Right
+                found = false;
+                for (int x1 = x + 1; x1 < xMax; x1++)
+                {
+                    if (trees[x1, y] >= trees[x, y])
+                    {
+                        score *= x1 - x;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) score *= xMax - x - 1;
+
+                // Up
+                found = false;
+                for (int y1 = y - 1; y1 >= 0; y1--)
+                {
+                    if (trees[x, y1] >= trees[x, y])
+                    {
+                        score *= y - y1;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) score *= y;
+
+                // Down
+                found = false;
+                for (int y1 = y + 1; y1 < yMax; y1++)
+                {
+                    if (trees[x, y1] >= trees[x, y])
+                    {
+                        score *= y1 - y;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) score *= yMax - y - 1;
+
+                scores.Add(score);
+            }
+        }
+        return scores.Max();
+    }
+
     public static int[,] ParseTrees(string filename)
     {
         string[] lines = filename.ReadAllLines();
@@ -101,11 +167,5 @@ public static class Day08
             }
         }
         return trees;
-    }
-
-    public static int PartTwo(string filename)
-    {
-        string[] lines = filename.ReadAllLines();
-        return 0;
     }
 }
