2de30459b73be27e300f9b970042a63e91ff7cff
Rob Prouse <rob@prouse.org>
2022-12-09 19:38:10 -0500

    Refactor day 9

--- AdventOfCode2022/Day09/Day09.cs
+++ AdventOfCode2022/Day09/Day09.cs
@@ -1,77 +1,24 @@
 using System;
-using System.Text;
 using System.Collections.Generic;
-using System.IO;
+using System.Drawing;
 using System.Linq;
 using AdventOfCode.Core;
-using System.Drawing;
 
 namespace AdventOfCode2022;
 
 public static class Day09
 {
-    public static int PartOne(string filename)
-    {
-        var visited = new List<Point>();
-        var head = new Point(0, 0);
-        var tail = new Point(0, 0);
-        visited.Add(tail);
-        string[] lines = filename.ReadAllLines();
-        foreach(string line in lines)
-        {
-            char dir = line[0];
-            int steps = line.Substring(2).ToInt();
-            foreach(var _ in Enumerable.Range(0, steps))
-            {
-                switch (dir)
-                {
-                    case 'R':
-                        head.Offset(1, 0);
-                        break;
-                    case 'L':
-                        head.Offset(-1, 0);
-                        break;
-                    case 'U':
-                        head.Offset(0, 1);
-                        break;
-                    case 'D':
-                        head.Offset(0, -1);
-                        break;
-                }
-                if (Math.Abs(head.X - tail.X) <= 1 && Math.Abs(head.Y - tail.Y) <= 1)
-                {
-                    continue;
-                }
-                if (head.X > tail.X)
-                {
-                    tail.Offset(1, 0);
-                }
-                else if (head.X < tail.X)
-                {
-                    tail.Offset(-1, 0);
-                }
-                if (head.Y > tail.Y)
-                {
-                    tail.Offset(0, 1);
-                }
-                else if (head.Y < tail.Y)
-                {
-                    tail.Offset(0, -1);
-                }
-                if (!visited.Contains(tail))
-                {
-                    visited.Add(tail);
-                }
-            }
-        }
-        return visited.Count;
-    }
+    public static int PartOne(string filename) =>
+        CalculateVisited(filename, 2);
 
-    public static int PartTwo(string filename)
-    {
+    public static int PartTwo(string filename) =>
+        CalculateVisited(filename, 10);
+
+    private static int CalculateVisited(string filename, int numPoints)
+    { 
         var visited = new List<Point>();
-        var points = new Point[10];
-        visited.Add(points[9]);
+        var points = new Point[numPoints];
+        visited.Add(points[points.Length - 1]);
         string[] lines = filename.ReadAllLines();
         foreach (string line in lines)
         {
@@ -79,51 +26,66 @@ public static class Day09
             int steps = line.Substring(2).ToInt();
             foreach (var _ in Enumerable.Range(0, steps))
             {
-                switch (dir)
-                {
-                    case 'R':
-                        points[0].Offset(1, 0);
-                        break;
-                    case 'L':
-                        points[0].Offset(-1, 0);
-                        break;
-                    case 'U':
-                        points[0].Offset(0, 1);
-                        break;
-                    case 'D':
-                        points[0].Offset(0, -1);
-                        break;
-                }
-                for (int i = 1; i < 10; i++)
-                {
-                    if (Math.Abs(points[i - 1].X - points[i].X) <= 1 &&
-                        Math.Abs(points[i - 1].Y - points[i].Y) <= 1)
-                    {
-                        continue;
-                    }
-                    if (points[i - 1].X > points[i].X)
-                    {
-                        points[i].Offset(1, 0);
-                    }
-                    else if (points[i - 1].X < points[i].X)
-                    {
-                        points[i].Offset(-1, 0);
-                    }
-                    if (points[i - 1].Y > points[i].Y)
-                    {
-                        points[i].Offset(0, 1);
-                    }
-                    else if (points[i - 1].Y < points[i].Y)
-                    {
-                        points[i].Offset(0, -1);
-                    }
-                }
-                if (!visited.Contains(points[9]))
-                {
-                    visited.Add(points[9]);
-                }
+                MoveHead(points, dir);
+                MoveTails(points);
+
+                if (!visited.Contains(points[points.Length - 1]))
+                    visited.Add(points[points.Length - 1]);
             }
         }
         return visited.Count;
+    }
+
+    private static void MoveHead(Point[] points, char dir)
+    {
+        switch (dir)
+        {
+            case 'R':
+                points[0].Offset(1, 0);
+                break;
+            case 'L':
+                points[0].Offset(-1, 0);
+                break;
+            case 'U':
+                points[0].Offset(0, 1);
+                break;
+            case 'D':
+                points[0].Offset(0, -1);
+                break;
+        }
+    }
+
+    private static void MoveTails(Point[] points)
+    {
+        for (int i = 1; i < points.Length; i++)
+        {
+            if (IsOneAwayFromPreviousPoint(points, i)) continue;
+            MoveTail(points, i);
+        }
+    }
+
+    private static bool IsOneAwayFromPreviousPoint(Point[] points, int i) =>
+        Math.Abs(points[i - 1].X - points[i].X) <= 1 &&
+        Math.Abs(points[i - 1].Y - points[i].Y) <= 1;
+
+
+    private static void MoveTail(Point[] points, int i)
+    {
+        if (points[i - 1].X > points[i].X)
+        {
+            points[i].Offset(1, 0);
+        }
+        else if (points[i - 1].X < points[i].X)
+        {
+            points[i].Offset(-1, 0);
+        }
+        if (points[i - 1].Y > points[i].Y)
+        {
+            points[i].Offset(0, 1);
+        }
+        else if (points[i - 1].Y < points[i].Y)
+        {
+            points[i].Offset(0, -1);
+        }
     }
 }
