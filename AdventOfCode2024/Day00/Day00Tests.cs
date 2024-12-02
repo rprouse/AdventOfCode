234a61ec7862dccca12ae29393fb1b5dfbd4b8ca
Rob Prouse <rob@prouse.org>
2024-12-01 21:39:28 -0500

    Add projects for 2023 and 2024

---
+++ AdventOfCode2024/Day00/Day00Tests.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.IO;
+using AdventOfCode.Core;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AdventOfCode2024;
+
+[Parallelizable(ParallelScope.All)]
+public class Day00Tests : TestBase
+{
+    const int DAY = 00;
+
+    [Test]
+    public void TestPartOne()
+    {
+        Day00.PartOne(PuzzleFile(DAY)).Should().Be(0);
+    }
+
+    [Test]
+    public void TestPartTwo()
+    {
+        Day00.PartTwo(PuzzleFile(DAY)).Should().Be(0);
+    }
+
+    [TestCase("", 0, Ignore = "If Needed")]
+    public void TestCasePartOne(string text, int expected)
+    {
+        Day00.PartOne(text).Should().Be(expected);
+    }
+
+    [TestCase("", 0, Ignore = "If Needed")]
+    public void TestCasePartTwo(string text, int expected)
+    {
+        Day00.PartTwo(text).Should().Be(expected);
+    }
+
+    [TestCaseSource(nameof(TestDataOne))]
+    public void TestPartOne(string filename, int expected)
+    {
+        Day00.PartOne(filename).Should().Be(expected);
+    }
+
+    [TestCaseSource(nameof(TestDataTwo))]
+    public void TestPartTwo(string filename, int expected)
+    {
+        Day00.PartTwo(filename).Should().Be(expected);
+    }
+
+    public static IEnumerable TestDataOne()
+    {
+        yield return new TestCaseData(TestFile(DAY), 0);
+        yield return new TestCaseData(PuzzleFile(DAY), 0);
+    }
+
+    public static IEnumerable TestDataTwo()
+    {
+        yield return new TestCaseData(TestFile(DAY), 0);
+        yield return new TestCaseData(PuzzleFile(DAY), 0);
+    }
+}
