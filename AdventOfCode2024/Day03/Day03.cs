65306cb4079c62b6097f5c3698b4a36c7b11b4f7
Rob Prouse <rob.prouse@aspiresoftware.com>
2024-12-03 16:16:50 -0500

    2024 day 3 part 2

--- AdventOfCode2024/Day03/Day03.cs
+++ AdventOfCode2024/Day03/Day03.cs
@@ -12,6 +12,56 @@ public static class Day03
     {
         string text = string.Join("", filename.ReadAllLines());
 
+        return SumMultiples(text);
+    }
+
+    public static int PartTwo(string filename)
+    {
+        string text = string.Join("", filename.ReadAllLines());
+
+        // Find the do() and don't() conditionals, strip out the don't()s, and return the sum of the do()s
+        bool enabled = true;
+        int current = 0;
+        StringBuilder sb = new ();
+        int pos = text.IndexOf("do");
+        while (pos >= 0)
+        {
+            if (text.Substring(pos, 4) == "do()")
+            {
+                if (enabled)
+                {
+                    sb.Append(text.Substring(current, pos - current));
+                }
+                enabled = true;
+                current = pos + 4;
+                pos += 4;
+            }
+            else if (text.Substring(pos, 7) == "don't()")
+            {
+                if (enabled)
+                {
+                    sb.Append(text.Substring(current, pos - current));
+                }
+                enabled = false;
+                current = pos + 7;
+                pos += 7;
+            }
+            else
+            {
+                pos += 2;
+            }
+            pos = text.IndexOf("do", pos);
+        }
+        if (enabled)
+        {
+            sb.Append(text.Substring(current));
+        }
+
+        return SumMultiples(sb.ToString());
+    }
+
+    private static int SumMultiples(string text)
+    {
         int sum = 0;
         MatchCollection matches = part1_regex.Matches(text);
         foreach (var match in matches)
@@ -21,12 +71,7 @@ public static class Day03
             int b = int.Parse(m.Groups[2].Value);
             sum += a * b;
         }
-        return sum;
-    }
 
-    public static int PartTwo(string filename)
-    {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        return sum;
     }
 }
--- AdventOfCode2024/Day03/Day03Tests.cs
+++ AdventOfCode2024/Day03/Day03Tests.cs
@@ -5,18 +5,6 @@ public class Day03Tests : TestBase
 {
     const int DAY = 03;
 
-    [Test]
-    public void TestPartTwo()
-    {
-        Day03.PartTwo(PuzzleFile(DAY)).Should().Be(0);
-    }
-
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartTwo(string text, int expected)
-    {
-        Day03.PartTwo(text).Should().Be(expected);
-    }
-
     [TestCaseSource(nameof(TestDataOne))]
     public void TestPartOne(string filename, int expected)
     {
@@ -31,13 +19,13 @@ public class Day03Tests : TestBase
 
     public static IEnumerable TestDataOne()
     {
-        yield return new TestCaseData(TestFile(DAY), 161);
+        yield return new TestCaseData(TestFile(DAY, "Test1.txt"), 161);
         yield return new TestCaseData(PuzzleFile(DAY), 173529487);
     }
 
     public static IEnumerable TestDataTwo()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY, "Test2.txt"), 48);
+        yield return new TestCaseData(PuzzleFile(DAY), 99532691);
     }
 }
