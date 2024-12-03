de5e89ab066a8c9883af92c29bd2a4a02057f3c1
Rob Prouse <rob.prouse@aspiresoftware.com>
2024-12-03 16:18:46 -0500

    Ready for day 4

---
+++ AdventOfCode2024/Day04/Day04.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode2024;
+
+public static class Day04
+{
+    public static int PartOne(string filename)
+    {
+        string[] lines = filename.ReadAllLines();
+        return 0;
+    }
+
+    public static int PartTwo(string filename)
+    {
+        string[] lines = filename.ReadAllLines();
+        return 0;
+    }
+}
