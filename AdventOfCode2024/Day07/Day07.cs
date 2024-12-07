356e45a90027c053294c309874e2d80cacef6561
Rob Prouse <rob.prouse@aspiresoftware.com>
2024-12-07 12:07:03 -0500

    Day 7 part 1

--- AdventOfCode2024/Day07/Day07.cs
+++ AdventOfCode2024/Day07/Day07.cs
@@ -2,15 +2,97 @@ namespace AdventOfCode2024;
 
 public static class Day07
 {
-    public static int PartOne(string filename)
+    public static ulong PartOne(string filename) =>
+        ParseCalibrations(filename)
+            .Where(c => c.ValidWithAdditionAndMultiplication())
+            .Select(c => c.Value)
+            .Aggregate(0UL, (total, next) => total + next);
+
+    public static ulong PartTwo(string filename) =>
+        ParseCalibrations(filename)
+            .Where(c => c.ValidWithAdditionMultiplicationAndConcatination())
+            .Select(c => c.Value)
+            .Aggregate(0UL, (total, next) => total + next);
+
+    private static Calibration[] ParseCalibrations(string filename) =>
+        filename.ReadAllLines()
+            .Select(line => new Calibration(line))
+            .ToArray();
+}
+
+public class Calibration
+{
+    public ulong Value { get; set; }
+    public ulong[] Inputs { get; set; }
+
+    public Calibration(string line)
     {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        var parts = line.Split(':', StringSplitOptions.TrimEntries);
+        Value = parts[0].ToUlong();
+        Inputs = parts[1].Split(' ', StringSplitOptions.TrimEntries).Select(p => p.ToUlong()).ToArray();
     }
 
-    public static int PartTwo(string filename)
+    public bool ValidWithAdditionAndMultiplication()
     {
-        string[] lines = filename.ReadAllLines();
-        return 0;
+        ulong result = Inputs[0];
+        int nextInput = 1;
+
+        // Recursion for the win
+        return ValidWithAdditionAndMultiplication(result, nextInput);
+    }
+
+    private bool ValidWithAdditionAndMultiplication(ulong result, int nextInput)
+    {
+        // Short circuit if the result is already greater than the value
+        if (result > Value)
+            return false;
+
+        // If we've reached the end of the inputs, check if the result is the value
+        if (nextInput >= Inputs.Length)
+            return result == Value;
+
+        // Try addition
+        ulong addition = result + Inputs[nextInput];
+        if (ValidWithAdditionAndMultiplication(addition, nextInput + 1))
+            return true;
+
+        // Try multiplication
+        ulong multiplication = result * Inputs[nextInput];
+        if (ValidWithAdditionAndMultiplication(multiplication, nextInput + 1))
+            return true;
+
+        return false;
+    }
+
+    public bool ValidWithAdditionMultiplicationAndConcatination()
+    {
+        ulong result = Inputs[0];
+        int nextInput = 1;
+
+        // Recursion for the win
+        return ValidWithAdditionMultiplicationAndConcatination(result, nextInput);
+    }
+
+    private bool ValidWithAdditionMultiplicationAndConcatination(ulong result, int nextInput)
+    {
+        // Short circuit if the result is already greater than the value
+        if (result > Value)
+            return false;
+
+        // If we've reached the end of the inputs, check if the result is the value
+        if (nextInput >= Inputs.Length)
+            return result == Value;
+
+        // Try addition
+        ulong addition = result + Inputs[nextInput];
+        if (ValidWithAdditionMultiplicationAndConcatination(addition, nextInput + 1))
+            return true;
+
+        // Try multiplication
+        ulong multiplication = result * Inputs[nextInput];
+        if (ValidWithAdditionMultiplicationAndConcatination(multiplication, nextInput + 1))
+            return true;
+
+        return false;
     }
 }
--- AdventOfCode2024/Day07/Day07Tests.cs
+++ AdventOfCode2024/Day07/Day07Tests.cs
@@ -6,9 +6,11 @@ public class Day07Tests : TestBase
     const int DAY = 07;
 
     [Test]
-    public void TestPartOne()
+    public void CanConstructCalibration()
     {
-        Day07.PartOne(PuzzleFile(DAY)).Should().Be(0);
+        var calibration = new Calibration("3267: 81 40 27");
+        calibration.Value.Should().Be(3267);
+        calibration.Inputs.Should().BeEquivalentTo([81, 40, 27]);
     }
 
     [Test]
@@ -17,39 +19,48 @@ public class Day07Tests : TestBase
         Day07.PartTwo(PuzzleFile(DAY)).Should().Be(0);
     }
 
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartOne(string text, int expected)
+    [TestCase("190: 10 19", true)]
+    [TestCase("3267: 81 40 27", true)]
+    [TestCase("83: 17 5", false)]
+    [TestCase("156: 15 6", false)]
+    [TestCase("7290: 6 8 6 15", false)]
+    [TestCase("161011: 16 10 13", false)]
+    [TestCase("192: 17 8 14", false)]
+    [TestCase("21037: 9 7 18 13", false)]
+    [TestCase("292: 11 6 16 20", true)]
+    public void CanDetermineIfValidWithMultiplicationAndDivision(string text, bool expected)
     {
-        Day07.PartOne(text).Should().Be(expected);
+        var calibration = new Calibration(text);
+        calibration.ValidWithAdditionAndMultiplication().Should().Be(expected);
     }
 
-    [TestCase("", 0, Ignore = "If Needed")]
-    public void TestCasePartTwo(string text, int expected)
+    [TestCase("", 0UL, Ignore = "If Needed")]
+    public void TestCasePartTwo(string text, ulong expected)
     {
         Day07.PartTwo(text).Should().Be(expected);
     }
 
     [TestCaseSource(nameof(TestDataOne))]
-    public void TestPartOne(string filename, int expected)
+    public void TestPartOne(string filename, ulong expected)
     {
         Day07.PartOne(filename).Should().Be(expected);
     }
 
     [TestCaseSource(nameof(TestDataTwo))]
-    public void TestPartTwo(string filename, int expected)
+    public void TestPartTwo(string filename, ulong expected)
     {
         Day07.PartTwo(filename).Should().Be(expected);
     }
 
     public static IEnumerable TestDataOne()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
-        yield return new TestCaseData(PuzzleFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY), 3749UL);
+        yield return new TestCaseData(PuzzleFile(DAY), 3245122495150UL);
     }
 
     public static IEnumerable TestDataTwo()
     {
-        yield return new TestCaseData(TestFile(DAY), 0);
+        yield return new TestCaseData(TestFile(DAY), 11387UL);
         yield return new TestCaseData(PuzzleFile(DAY), 0);
     }
 }
